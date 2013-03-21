e7d1444ad49a5b0f3a7222ee433ec55ec8a1e74d
Kristian Flikka <kristian.flikka@gmail.com>
2013-03-21 13:18:52 +0100

    Added tests and Record class. Moved BOOST_ROOT into CMakeLists file

---
+++ opm/parser/eclipse/data/Record.hpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   Record.hpp
+ * Author: kflik
+ *
+ * Created on March 21, 2013, 10:44 AM
+ */
+
+#ifndef RECORD_HPP
+#define	RECORD_HPP
+
+class Record {
+public:
+    Record();
+    Record(const Record& orig);
+    virtual ~Record();
+private:
+
+};
+
+#endif	/* RECORD_HPP */
+
