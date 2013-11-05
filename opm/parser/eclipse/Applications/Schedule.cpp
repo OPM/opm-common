e5b921fe1c36c0a56173b2a9e2fbe541f74e640f
Joakim Hove <joakim.hove@gmail.com>
2013-11-05 18:06:51 +0100

    Added simple datafile parser which focuses on the Schedule section

---
+++ opm/parser/eclipse/Applications/Schedule.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   EclipseDataDoctor.cpp
+ * Author: kflik
+ *
+ * Created on August 20, 2013, 1:19 PM
+ */
+
+#include <iostream>
+#include <opm/parser/eclipse/Parser/Parser.hpp>
+#include <opm/parser/eclipse/Deck/Deck.hpp>
+#include <opm/parser/eclipse/EclipseState/Schedule/Schedule.hpp>
+
+
+int main(int argc, char** argv) {
+    Opm::ParserPtr parser(new Opm::Parser());
+    std::string file = argv[1];
+    Opm::DeckConstPtr deck = parser->parse(file, false);
+    Opm::Schedule sched( deck );
+    
+    std::cout << "Wells: " << sched.numWells() << std::endl;
+    
+
+    return 0;
+}
+
