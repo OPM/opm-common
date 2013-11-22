581ccb21dc565fbec792e214746d32065506139a
Joakim Hove <joakim.hove@gmail.com>
2013-11-22 22:34:44 +0100

    Added new WellSet class - just a thin wrapper around std::map

---
+++ opm/parser/eclipse/EclipseState/Schedule/WellSet.cpp
@@ -0,0 +1,70 @@
+/*
+  Copyright 2013 Statoil ASA.
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <opm/parser/eclipse/EclipseState/Schedule/WellSet.hpp>
+namespace Opm {
+    
+        
+    WellSet::WellSet() {
+    }
+
+    size_t WellSet::size() const {
+        return m_wells.size();
+    }
+
+
+    bool WellSet::hasWell(const std::string& wellName) const {
+        return (m_wells.find(wellName) != m_wells.end());
+    }
+
+
+    WellConstPtr WellSet::getWell(const std::string& wellName) const {
+        if (hasWell(wellName))
+            return m_wells.find(wellName)->second;
+        else
+            throw std::invalid_argument("Does not have this well?!\n");
+    }
+
+    
+    void WellSet::addWell(WellPtr well) {
+        const std::string& wellName = well->name();
+        if (!hasWell(wellName)) 
+            m_wells[wellName] = well;
+        else if (well != getWell(wellName))
+            throw std::invalid_argument("Well has changed - internal fuckup");
+    }
+
+
+    void WellSet::delWell(const std::string& wellName) {
+        if (hasWell(wellName))
+            m_wells.erase( wellName );
+        else
+            throw std::invalid_argument("Does not have this well?");
+    }
+
+
+    WellSet * WellSet::shallowCopy() const {
+        WellSet * copy = new WellSet();
+        
+        for (std::map<std::string , WellPtr>::const_iterator iter=m_wells.begin(); iter != m_wells.end(); ++iter) 
+            copy->addWell( (*iter).second );
+        
+        return copy;
+    }
+}
---
+++ opm/parser/eclipse/EclipseState/Schedule/WellSet.hpp
@@ -0,0 +1,50 @@
+/*
+  Copyright 2013 Statoil ASA.
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef WELLSET_HPP
+#define WELLSET_HPP
+
+#include <opm/parser/eclipse/EclipseState/Schedule/TimeMap.hpp>
+#include <opm/parser/eclipse/EclipseState/Schedule/DynamicState.hpp>
+#include <opm/parser/eclipse/EclipseState/Schedule/Well.hpp>
+
+#include <boost/shared_ptr.hpp>
+
+#include <string>
+#include <map>
+
+namespace Opm {
+    
+    class WellSet {
+    public:
+        WellSet();
+        size_t size() const;
+        bool hasWell(const std::string& wellName) const;
+        WellConstPtr getWell(const std::string& wellName) const;
+        void addWell(WellPtr well);
+        void delWell(const std::string& wellName);
+        WellSet * shallowCopy() const;
+    private:
+        std::map<std::string , WellPtr> m_wells;
+    };
+    
+    typedef boost::shared_ptr<WellSet> WellSetPtr;
+    typedef boost::shared_ptr<const WellSet> WellSetConstPtr;
+}
+
+#endif
