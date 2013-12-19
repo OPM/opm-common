8f94b29547cac367bbf78ee4b869f82fb2eae2f7
Atle Haugan (ITV DIS RD) <atleh@tr-lcdapp02.tr.statoil.no>
2013-12-19 14:05:50 +0100

    Added KeywordDescriber.cpp

---
+++ opm/parser/eclipse/Applications/KeywordDescriber.cpp
@@ -0,0 +1,17 @@
+/*
+ * File:   KeywordDescriber.cpp
+ * Author: atleh
+ *
+ * Created on December 19, 2013, 1:19 PM
+ */
+
+#include <iostream>
+
+
+/*
+ *
+ */
+int main(int argc, char** argv) {
+        std::cout << "Usage: " << argv[0] << " <Filename>" << std::endl;
+        exit(1);
+    }
