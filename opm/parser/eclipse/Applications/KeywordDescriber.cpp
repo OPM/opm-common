a18cf4155390fd2d5cae2be919d450a8b68601b2
Atle Haugan <atleh@statoil.com>
2013-12-20 13:02:33 +0100

    Some cleanup and refactoring

--- opm/parser/eclipse/Applications/KeywordDescriber.cpp
+++ opm/parser/eclipse/Applications/KeywordDescriber.cpp
@@ -19,16 +19,39 @@ void printKeywordInformation(Opm::ParserKeywordConstPtr keyword)
     std::string indent = "   ";
 
     std::cout << keyword->getName() << std::endl;
-    std::cout << indent << "numItems: " << keyword->numItems() << std::endl;
-    std::cout << indent << "hasDimension: " << keyword->hasDimension() << std::endl;
-    std::cout << indent << "hasFixedSize: " << keyword->hasFixedSize() << std::endl;
+    std::cout << indent << "numItems:              " << keyword->numItems() << std::endl;
+    std::cout << indent << "hasDimension:          " << keyword->hasDimension() << std::endl;
+    std::cout << indent << "hasFixedSize:          " << keyword->hasFixedSize() << std::endl;
     if (keyword->hasFixedSize())
-        std::cout << indent << "getFixedSize: " << keyword->getFixedSize() << std::endl;
-    std::cout << indent << "isTableCollection: " << keyword->isTableCollection() << std::endl;
-    std::cout << indent << "getSizeType: " << keyword->getSizeType() << std::endl;
+        std::cout << indent << "getFixedSize:          " << keyword->getFixedSize() << std::endl;
+    std::cout << indent << "isTableCollection:     " << keyword->isTableCollection() << std::endl;
+    std::cout << indent << "getSizeType:           " << keyword->getSizeType() << std::endl;
     std::pair<std::string, std::string> sizeDefinitionPair = keyword->getSizeDefinitionPair();
     std::cout << indent << "getSizeDefinitionPair: '" << sizeDefinitionPair.first << "', '" << sizeDefinitionPair.second << "'" << std::endl;
-    std::cout << indent << "isDataKeyword: " << keyword->isDataKeyword() << std::endl;
+    std::cout << indent << "isDataKeyword:         " << keyword->isDataKeyword() << std::endl;
+}
+
+bool parseCommandLineForAllKeywordsOption(char** argv)
+{
+    bool allKeywords = false;
+    std::string arg(argv[1]);
+    if (arg == "-a")
+        allKeywords = true;
+
+    return allKeywords;
+}
+
+std::list<std::string> createListOfKeywordsToDescribe(char** argv, bool allKeywords, Opm::ParserPtr parser)
+{
+    std::list<std::string> keywords;
+    if (allKeywords) {
+        parser->getKeywords(&keywords);
+    } else {
+        std::string keywordName = argv[1];
+        keywords.push_back(keywordName);
+    }
+
+    return keywords;
 }
 
 int main(int argc, char** argv) {
@@ -37,27 +60,18 @@ int main(int argc, char** argv) {
             exit(1);
         }
 
-        bool allKeywords = false;
-        std::string arg(argv[1]);
-        if (arg == "-a")
-            allKeywords = true;
+        bool allKeywords = parseCommandLineForAllKeywordsOption(argv);
 
         Opm::ParserPtr parser(new Opm::Parser());
-        std::string keywordName = argv[1];
 
         std::list<std::string> keywords;
-        if (allKeywords) {
-            parser->getKeywords(&keywords);
-        } else {
-            keywords.push_back(keywordName);
-        }
+        keywords = createListOfKeywordsToDescribe(argv, allKeywords, parser);
 
         std::list<std::string>::const_iterator iterator;
         for (iterator = keywords.begin(); iterator != keywords.end(); ++iterator) {
             Opm::ParserKeywordConstPtr keyword = parser->getKeyword(*iterator);
             printKeywordInformation(keyword);
         }
-
 
         return 0;
     }
