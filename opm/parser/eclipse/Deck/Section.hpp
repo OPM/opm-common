14507208d50cdc9c9d45be4236ba0af0bf5d627b
atleh <atleh@statoil.com>
2014-03-21 14:39:24 +0100

    Added first version of Section class

---
+++ opm/parser/eclipse/Deck/Section.hpp
@@ -0,0 +1,34 @@
+/*
+  Copyright 2013 Statoil ASA.
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SECTION_HPP
+#define SECTION_HPP
+
+#include <opm/parser/eclipse/Deck/Deck.hpp>
+
+namespace Opm {
+
+    class Section
+    {
+    public:
+        Section(Deck& deck);
+    };
+}
+
+#endif // SECTION_HPP
