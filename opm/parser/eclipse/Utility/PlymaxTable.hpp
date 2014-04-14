406f294026e3f216830aca6aaf4293688edbf801
Andreas Lauser <and@poware.org>
2014-04-14 16:18:37 +0200

    add table utility classes for the PLY{ADS,MAX,ROCK,VISC} and TLMIXPAR keywords
    these are not really used as tables in the current opm-polymer code,
    but the Eclipse RM says they are tables, so we should add these
    classes...

---
+++ opm/parser/eclipse/Utility/PlymaxTable.hpp
@@ -0,0 +1,55 @@
+/*
+  Copyright (C) 2014 by Andreas Lauser
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef OPM_PARSER_PLYMAX_TABLE_HPP
+#define	OPM_PARSER_PLYMAX_TABLE_HPP
+
+#include "SimpleTable.hpp"
+
+namespace Opm {
+    class PlymaxTable : protected SimpleTable {
+        typedef SimpleTable ParentType;
+
+    public:
+        /*!
+         * \brief Read the PLYMAX keyword and provide some convenience
+         *        methods for it.
+         */
+        PlymaxTable(Opm::DeckKeywordConstPtr keyword, int recordIdx = 0)
+            : ParentType(keyword,
+                         std::vector<std::string>{"C_POLYMER", "C_POLYMER_MAX"},
+                         recordIdx,
+                         /*firstEntityOffset=*/0)
+        {}
+
+        int numRows() const
+        { return ParentType::numRows(); };
+
+        int numColumns() const
+        { return ParentType::numColumns(); };
+
+        const std::vector<double> &getPolymerConcentrationColumn() const
+        { return ParentType::getColumn(0); }
+
+        const std::vector<double> &getMaxPolymerConcentrationColumn() const
+        { return ParentType::getColumn(1); }
+    };
+}
+
+#endif	// OPM_PARSER_SIMPLE_TABLE_HPP
+
---
+++ opm/parser/eclipse/Utility/PlyrockTable.hpp
@@ -0,0 +1,78 @@
+/*
+  Copyright (C) 2014 by Andreas Lauser
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef OPM_PARSER_PLYROCK_TABLE_HPP
+#define	OPM_PARSER_PLYROCK_TABLE_HPP
+
+#include "SimpleTable.hpp"
+
+namespace Opm {
+    class PlyrockTable : protected SimpleTable {
+        typedef SimpleTable ParentType;
+
+    public:
+        /*!
+         * \brief Read the PLYROCK keyword and provide some convenience
+         *        methods for it.
+         */
+        PlyrockTable(Opm::DeckKeywordConstPtr keyword, int recordIdx = 0)
+            : ParentType(keyword,
+                         std::vector<std::string>{
+                             "DeadPoreVolume",
+                             "ResidualResistanceFactor",
+                             "RockDensityFactor",
+                             "AdsorbtionIndex",
+                             "MaxAdsorbtion"
+                         },
+                         recordIdx,
+                         /*firstEntityOffset=*/0)
+        {}
+
+        int numRows() const
+        { return ParentType::numRows(); };
+
+        int numColumns() const
+        { return ParentType::numColumns(); };
+
+        const std::vector<double> &getDeadPoreVolumeColumn() const
+        { return ParentType::getColumn(0); }
+
+        const std::vector<double> &getResidualResistanceFactorColumn() const
+        { return ParentType::getColumn(1); }
+
+        const std::vector<double> &getRockDensityFactorColumn() const
+        { return ParentType::getColumn(2); }
+
+        // is column is actually an integer, but this is not yet
+        // supported by opm-parser (yet?) as it would require quite a
+        // few changes in the table support classes (read: it would
+        // probably require a lot of template vodoo) and some in the
+        // JSON-to-C conversion code. In the meantime, the index is
+        // just a double which can be converted to an integer in the
+        // calling code. (Make sure, that you don't interpolate
+        // indices, though!)
+        const std::vector<double> &getAdsorbtionIndexColumn() const
+        { return ParentType::getColumn(3); }
+
+        const std::vector<double> &getMaxAdsorbtionColumn() const
+        { return ParentType::getColumn(4); }
+    };
+}
+
+#endif	// OPM_PARSER_PLYROCK_TABLE_HPP
+
