ce0dc92822e1274254da3f36ff36e7bfe19e0f87
Joakim Hove <joakim.hove@gmail.com>
2014-05-16 15:57:47 +0200

    Added grid properties container.

---
+++ opm/parser/eclipse/EclipseState/Grid/GridProperties.hpp
@@ -0,0 +1,109 @@
+/*
+  Copyright 2014 Statoil ASA.
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef ECLIPSE_GRIDPROPERTIES_HPP_
+#define ECLIPSE_GRIDPROPERTIES_HPP_
+
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+#include <opm/parser/eclipse/EclipseState/Grid/GridProperty.hpp>
+
+/*
+  This class implements a container (std::unordered_map<std::string ,
+  Gridproperty<T>>) of Gridproperties. Usage is as follows:
+
+    1. Instantiate the class; passing the number of grid cells and the
+       supported keywords as a list of strings to the constructor.
+
+    2. Query the container with the supportsKeyword() and hasKeyword()
+       methods. 
+       
+    3. When you ask the container to get a keyword with the
+       getKeyword() method it will automatically create a new
+       GridProperty object if the container does not have this
+       property. 
+*/
+
+
+namespace Opm {
+
+template <typename T>
+class GridProperties {
+public:
+
+    GridProperties(size_t gridVolume , const std::vector<std::pair<std::string, T> > & supportedKeywords) {
+        m_gridVolume = gridVolume;
+        
+        for (auto iter = supportedKeywords.begin(); iter != supportedKeywords.end(); ++iter) 
+            m_supportedKeywords.insert( std::pair<std::string , T>(*iter));
+    }
+
+    
+    bool supportsKeyword(const std::string& keyword) {
+        if (m_supportedKeywords.find( keyword ) == m_supportedKeywords.end())
+            return false;
+        else
+            return true;
+    }
+
+    bool hasKeyword(const std::string& keyword) {
+        if (m_properties.find( keyword ) == m_properties.end())
+            return false;
+        else
+            return true;
+    }
+
+
+    std::shared_ptr<GridProperty<T> > getKeyword(const std::string& keyword) {
+        if (!hasKeyword(keyword))
+            addKeyword(keyword);
+        
+        {
+            auto pair = m_properties.find( keyword );
+            return (*pair).second;
+        }
+    }
+
+    
+    bool addKeyword(const std::string& keyword) {
+        if (!supportsKeyword( keyword )) 
+            throw std::invalid_argument("The keyword: " + keyword + " is not supported in this container");
+
+        if (hasKeyword(keyword)) 
+            return false;
+        else {
+            auto supportedKeyword = m_supportedKeywords.find( keyword );
+            std::shared_ptr<GridProperty<T> > newProperty(new GridProperty<T>(m_gridVolume , keyword , (*supportedKeyword).second));
+            m_properties.insert( std::pair<std::string , std::shared_ptr<GridProperty<int> > > ( keyword , newProperty ));
+            return true;
+        }
+    }
+
+    
+private:
+    size_t m_gridVolume;
+    std::unordered_map<std::string, T> m_supportedKeywords;
+    std::map<std::string , std::shared_ptr<GridProperty<T> > > m_properties;
+};
+
+}
+
+#endif
---
+++ opm/parser/eclipse/EclipseState/Grid/tests/GridPropertiesTests.cpp
@@ -0,0 +1,72 @@
+/*
+  Copyright 2014 Statoil ASA.
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdexcept>
+#include <iostream>
+#include <boost/filesystem.hpp>
+
+#define BOOST_TEST_MODULE EclipseGridTests
+#include <boost/test/unit_test.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+
+#include <opm/parser/eclipse/Parser/Parser.hpp>
+
+#include <opm/parser/eclipse/Deck/Section.hpp>
+#include <opm/parser/eclipse/Deck/Deck.hpp>
+#include <opm/parser/eclipse/Deck/DeckKeyword.hpp>
+
+#include <opm/parser/eclipse/EclipseState/Grid/GridProperties.hpp>
+
+
+BOOST_AUTO_TEST_CASE(Empty) {
+    Opm::GridProperties<int> gridProperties( 10000 , std::vector<std::pair<std::string,int> > { {"SATNUM" , 0} , {"FIPNUM" , 2 } });
+    
+    BOOST_CHECK( gridProperties.supportsKeyword("SATNUM") );
+    BOOST_CHECK( gridProperties.supportsKeyword("FIPNUM") );
+    BOOST_CHECK( !gridProperties.supportsKeyword("FLUXNUM") );
+    BOOST_CHECK( !gridProperties.hasKeyword("SATNUM"));
+    BOOST_CHECK( !gridProperties.hasKeyword("FLUXNUM"));
+}
+
+
+
+BOOST_AUTO_TEST_CASE(addKeyword) {
+    Opm::GridProperties<int> gridProperties( 10000 , std::vector<std::pair<std::string,int> > { {"SATNUM" , 0} });
+    
+    BOOST_CHECK_THROW( gridProperties.addKeyword("NOT-SUPPORTED") , std::invalid_argument);
+
+    BOOST_CHECK(  gridProperties.addKeyword("SATNUM"));
+    BOOST_CHECK( !gridProperties.addKeyword("SATNUM"));
+
+    BOOST_CHECK(  gridProperties.hasKeyword("SATNUM"));
+}
+
+
+
+BOOST_AUTO_TEST_CASE(getKeyword) {
+    Opm::GridProperties<int> gridProperties( 10000 , std::vector<std::pair<std::string,int> > { {"SATNUM" , 0} });
+    std::shared_ptr<Opm::GridProperty<int> > satnum1 = gridProperties.getKeyword("SATNUM");
+    std::shared_ptr<Opm::GridProperty<int> > satnum2 = gridProperties.getKeyword("SATNUM");
+    
+    BOOST_CHECK_EQUAL( satnum1.get() , satnum2.get());
+    BOOST_CHECK_THROW( gridProperties.getKeyword("NOT-SUPPORTED") , std::invalid_argument );
+}
+
+
