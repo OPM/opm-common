f600930a5f1d545a43860eec43a4b2cee0ade916
Andreas Lauser <and@poware.org>
2014-10-01 14:22:08 +0200

    add a class for log messages of the parser
    unsurprisingly, it's called ParserLog. For now, it is not used very
    extensively, but it allows to demingle the log messages from the deck
    objects. (i.e., it's possible to pass a const pointer to the deck
    object to e.g. EclipseState and one will still get additional log
    messages.)

---
+++ opm/parser/eclipse/Parser/ParserLog.cpp
@@ -0,0 +1,138 @@
+/*
+  Copyright 2014 Andreas Lauser
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "ParserLog.hpp"
+
+#include <stdexcept>
+#include <sstream>
+#include <cassert>
+
+namespace Opm {
+ParserLog::ParserLog() {
+    m_numErrors = 0;
+    m_numWarnings = 0;
+    m_numNotes = 0;
+}
+
+size_t ParserLog::size() const {
+    return m_messages.size();
+}
+
+size_t ParserLog::numErrors() const {
+    return m_numErrors;
+}
+
+size_t ParserLog::numWarnings() const {
+    return m_numWarnings;
+}
+
+size_t ParserLog::numNotes() const {
+    return m_numNotes;
+}
+
+void ParserLog::addMessage(const std::string& fileName,
+                           int lineNumber,
+                           MessageType messageType,
+                           const std::string& description) {
+    switch (messageType) {
+    case Note:
+        ++m_numNotes;
+        break;
+
+    case Warning:
+        ++m_numWarnings;
+        break;
+
+    case Error:
+        ++m_numErrors;
+        break;
+
+    default:
+        throw std::invalid_argument("Log messages must be of type Note, Warning or Error");
+    }
+
+    m_messages.push_back(MessageTuple(fileName, lineNumber, messageType, description));
+}
+
+void ParserLog::addNote(const std::string& fileName,
+                           int lineNumber,
+                           const std::string& description) {
+    addMessage(fileName, lineNumber, Note, description);
+}
+
+void ParserLog::addWarning(const std::string& fileName,
+                           int lineNumber,
+                           const std::string& description) {
+    addMessage(fileName, lineNumber, Warning, description);
+}
+
+void ParserLog::addError(const std::string& fileName,
+                         int lineNumber,
+                         const std::string& description) {
+    addMessage(fileName, lineNumber, Error, description);
+}
+
+const std::string& ParserLog::getFileName(size_t msgIdx) const {
+    assert(msgIdx < size());
+    return std::get<0>(m_messages[msgIdx]);
+}
+
+int ParserLog::getLineNumber(size_t msgIdx) const {
+    assert(msgIdx < size());
+    return std::get<1>(m_messages[msgIdx]);
+}
+
+int ParserLog::getMessageType(size_t msgIdx) const {
+    assert(msgIdx < size());
+    return std::get<2>(m_messages[msgIdx]);
+}
+
+const std::string& ParserLog::getDescription(size_t msgIdx) const {
+    assert(msgIdx < size());
+    return std::get<3>(m_messages[msgIdx]);
+}
+
+const std::string ParserLog::getFormattedMessage(size_t msgIdx) const {
+    std::ostringstream oss;
+    if (getLineNumber(msgIdx) > 0) {
+        oss << getFileName(msgIdx) << ":"
+            << getLineNumber(msgIdx) << ":";
+    }
+
+    switch (getMessageType(msgIdx)) {
+    case Note:
+        oss << " note:";
+        break;
+    case Warning:
+        oss << " warning:";
+        break;
+    case Error:
+        oss << " error:";
+        break;
+    }
+    oss << " " << getDescription(msgIdx);
+    return oss.str();
+}
+
+void ParserLog::printAll(std::ostream& os, size_t enabledTypes) const {
+    for (size_t i = 0; i < size(); ++i)
+        if (enabledTypes & getMessageType(i))
+            os << getFormattedMessage(i) << "\n";
+}
+
+} // namespace Opm
---
+++ opm/parser/eclipse/Parser/ParserLog.hpp
@@ -0,0 +1,108 @@
+/*
+  Copyright 2014 Andreas Lauser
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef OPM_PARSER_LOG_HPP
+#define OPM_PARSER_LOG_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <memory>
+
+namespace Opm {
+/*!
+ * \brief Provides a simple sytem for log message which are found by the
+ *        Parser/Deck/EclipseState classes during processing the deck.
+ */
+class ParserLog {
+public:
+    enum MessageType {
+        Note = 0x01,
+        Warning = 0x02,
+        Error = 0x04,
+
+        AllTypes = 0xff
+    };
+
+    ParserLog();
+
+    size_t size() const;
+    size_t numErrors() const;
+    size_t numWarnings() const;
+    size_t numNotes() const;
+
+    void addMessage(const std::string& fileName,
+                    int lineNumber,
+                    MessageType messageType,
+                    const std::string& description);
+
+    void addNote(const std::string& fileName,
+                 int lineNumber,
+                 const std::string& description);
+    void addWarning(const std::string& fileName,
+                    int lineNumber,
+                    const std::string& description);
+    void addError(const std::string& fileName,
+                  int lineNumber,
+                  const std::string& description);
+
+
+    const std::string& getFileName(size_t msgIdx) const;
+    int getLineNumber(size_t msgIdx) const;
+    int getMessageType(size_t msgIdx) const;
+    const std::string& getDescription(size_t msgIdx) const;
+
+    /*!
+     * \brief This method takes the information provided by the methods above and returns
+     *        them in a fully-formatted string.
+     *
+     * It is thus covenience method to convert a log message into a GCC-like format,
+     * e.g. a "Note" message triggered by the file "SPE1DECK.DATA" on line 15 which says
+     * that no grid can be constructed would yield:
+     *
+     * SPE1DECK.DATA:15:note: No grid found.
+     */
+    const std::string getFormattedMessage(size_t msgIdx) const;
+
+    /*!
+     * \brief Print all issues described in a log object to a std::ostream.
+     *
+     * This is just another convenience method...
+     */
+    void printAll(std::ostream &os = std::cerr,
+                  size_t enabledTypes = AllTypes) const;
+
+private:
+    typedef std::tuple</*file=*/std::string,
+                       /*line=*/int,
+                       /*MessageType=*/int,
+                       std::string> MessageTuple;
+    std::vector<MessageTuple> m_messages;
+
+    size_t m_numErrors;
+    size_t m_numWarnings;
+    size_t m_numNotes;
+};
+
+typedef std::shared_ptr<ParserLog> ParserLogPtr;
+typedef std::shared_ptr<const ParserLog> ParserLogConstPtr;
+} // namespace Opm
+
+#endif
+
