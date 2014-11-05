2ac15ffae22a5d9e304509ecbded089bd7b2ec90
Joakim Hove <joakim.hove@gmail.com>
2014-11-05 20:30:19 +0100

    Rudimentary example of Log singleton.
    This commit shows a very basic example of how Logging singleton can be
    implemented. The commit consists of a fully static class OpmLog which
    manages a Logger class; in this case an instance of the ParserLog
    class. The static OpmLog class can then be used from anywhere in the opm
    code with:
    
      OpmLog::addMessage( messageType , "Log message")
    
    Before this can be really useful the Logger class should be extended /
    subclassed / ... for general usability.

---
+++ opm/parser/eclipse/Log/OpmLog.hpp
@@ -0,0 +1,51 @@
+/*
+  Copyright 2014 Statoil ASA.
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef OPMLOG_HPP
+#define OPMLOG_HPP
+
+#include <memory>
+#include <opm/parser/eclipse/Parser/ParserLog.hpp>
+
+
+namespace Opm {
+
+typedef ParserLog Logger;
+
+
+/*
+  The OpmLog class is a fully static class which manages a proper
+  Logger instance.
+*/
+
+class OpmLog {
+public:
+    static void addMessage(ParserLog::MessageType messageType , const std::string& message);
+private:
+    static std::shared_ptr<Logger> getLogger();
+    static std::shared_ptr<Logger> m_logger;
+};
+
+
+}
+
+
+
+
+#endif
