56b7f4e8c02f16477ac932a552eb88a35ae755e9
Joakim Hove <joakim.hove@gmail.com>
2014-11-16 19:05:59 +0100

    Removed emacs config

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-;; <http://www.gnu.org/software/emacs/manual/html_node/emacs/Directory-Variables.html>
-(( c-mode . ((c-basic-offset . 4)
-             (tab-width . 8)
-             (indent-tabs-mode . nil)))
-
