9f16165dfd373b4f382e67a2f5dcfe6c1d4a4fd2
Joakim Hove <joakim.hove@gmail.com>
2015-02-06 17:05:19 +0100

    Will compare connection factor > 0 before setting

--- opm/parser/eclipse/EclipseState/Schedule/Completion.cpp
+++ opm/parser/eclipse/EclipseState/Schedule/Completion.cpp
@@ -83,7 +83,7 @@ namespace Opm {
             DeckItemConstPtr diameterItem = compdatRecord->getItem("DIAMETER");
             DeckItemConstPtr skinFactorItem = compdatRecord->getItem("SKIN");
 
-            if (!connectionTransmissibilityFactorItem->defaultApplied(0))
+            if (connectionTransmissibilityFactorItem->hasValue(0) && connectionTransmissibilityFactorItem->getSIDouble(0) > 0)
                 connectionTransmissibilityFactor.setValue(connectionTransmissibilityFactorItem->getSIDouble(0));
 
             if (diameterItem->hasValue(0))
