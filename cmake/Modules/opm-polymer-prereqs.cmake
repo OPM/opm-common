65d49ef35a548d9c9f131d746b2e75f768ee864a
Joakim Hove <joakim.hove@gmail.com>
2015-04-28 09:06:02 +0200

    Updated to opm-core/master

--- cmake/Modules/opm-polymer-prereqs.cmake
+++ cmake/Modules/opm-polymer-prereqs.cmake
@@ -17,8 +17,5 @@ set (opm-polymer_DEPS
 	# Ensembles-based Reservoir Tools
 	"ERT"
 	# OPM dependency
-	"opm-autodiff REQUIRED"
-	"opm-core REQUIRED"
-	# Eigen
-	"Eigen3 3.1 REQUIRED"
+	"opm-core"
 	)
