3ffbe756802a9ee820320d77d0845a8e6c1f2958
Joakim Hove <joakim.hove@gmail.com>
2015-08-05 21:52:36 +0200

    !! Fixed test due to bug in ScheduleParsing
    During the work with ParseMode the the construction of the Schedule
    object has been changed; previously the complete deck was iterated in
    the method iterateSchedulesection() - whereas the correct is to only
    iterate over the keywords in the Schedule section. This has led to a
    minor fixup in the testdata for this commit.
    
    There are probably more bugs in the IOConfig object - because there
    RPTRST / RPTSCHED / keywords in the SOLUTION section have been taken
    into account twice, both as part of the SOLUTION and then again
    incorrectly as part of the SCHEDULE section.

--- opm/parser/eclipse/EclipseState/tests/EclipseStateTests.cpp
+++ opm/parser/eclipse/EclipseState/tests/EclipseStateTests.cpp
@@ -486,10 +486,10 @@ BOOST_AUTO_TEST_CASE(TestIOConfigCreationWithSolutionRPTRST) {
 
     IOConfigConstPtr ioConfig = state.getIOConfigConst();
 
-    BOOST_CHECK_EQUAL(true, ioConfig->getWriteRestartFile(0));
-    BOOST_CHECK_EQUAL(true, ioConfig->getWriteRestartFile(1));
-    BOOST_CHECK_EQUAL(false, ioConfig->getWriteRestartFile(2));
-    BOOST_CHECK_EQUAL(true, ioConfig->getWriteRestartFile(3));
+    BOOST_CHECK_EQUAL(true  , ioConfig->getWriteRestartFile(0));
+    BOOST_CHECK_EQUAL(false , ioConfig->getWriteRestartFile(1));
+    BOOST_CHECK_EQUAL(false , ioConfig->getWriteRestartFile(2));
+    BOOST_CHECK_EQUAL(true  , ioConfig->getWriteRestartFile(3));
 }
 
 
