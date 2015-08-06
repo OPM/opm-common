46d787c7ce73797fdee6701bcd6635c78d7389d5
Tor Harald Sandve <thsa@iris.no>
2015-08-06 11:48:33 +0200

    Add support for PVDS -the PVDS implementation is essentially a copy of the PVDG implementation as the PVDS table analogous the PVDG table

---
+++ opm/parser/eclipse/EclipseState/Tables/PvdsTable.hpp
@@ -0,0 +1,75 @@
+/*
+  Copyright 2015 IRIS AS
+
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef OPM_PARSER_PVDS_TABLE_HPP
+#define OPM_PARSER_PVDS_TABLE_HPP
+
+#include "SingleRecordTable.hpp"
+
+namespace Opm {
+    // forward declaration
+    class EclipseState;
+
+    class PvdsTable : protected SingleRecordTable {
+        typedef SingleRecordTable ParentType;
+
+        friend class EclipseState;
+
+        PvdsTable() = default;
+
+        /*!
+         * \brief Read the PVDS keyword and provide some convenience
+         *        methods for it.
+         */
+        void init(Opm::DeckKeywordConstPtr keyword,
+                  int recordIdx)
+        {
+            ParentType::init(keyword,
+                             std::vector<std::string>{"P", "BG", "MUG"},
+                             recordIdx,
+                             /*firstEntityOffset=*/0);
+
+            ParentType::checkNonDefaultable("P");
+            ParentType::checkMonotonic("P", /*isAscending=*/true);
+
+            ParentType::applyDefaultsLinear("BG");
+            ParentType::checkMonotonic("BG", /*isAscending=*/false);
+
+            ParentType::applyDefaultsLinear("MUG");
+            ParentType::checkMonotonic("MUG", /*isAscending=*/true, /*strictlyMonotonic=*/false);
+        }
+
+    public:
+        using ParentType::numTables;
+        using ParentType::numRows;
+        using ParentType::numColumns;
+        using ParentType::evaluate;
+
+        const std::vector<double> &getPressureColumn() const
+        { return ParentType::getColumn(0); }
+
+        const std::vector<double> &getFormationFactorColumn() const
+        { return ParentType::getColumn(1); }
+
+        const std::vector<double> &getViscosityColumn() const
+        { return ParentType::getColumn(2); }
+    };
+}
+
+#endif
