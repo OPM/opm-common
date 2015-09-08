8e13912b2b0c5d7fccc0832bcb0a9c3739dd72ab
Andreas Lauser <and@poware.org>
2015-09-08 10:39:49 +0200

    add CMakeLists.txt and CMakeLists_files.cmake and remove dummy Makefile
    as for the patch before, this will only strictly required once c++
    code is added to the repo.

---
+++ CMakeLists_files.cmake
@@ -0,0 +1,46 @@
+# This file sets up five lists:
+#	MAIN_SOURCE_FILES     List of compilation units which will be included in
+#	                      the library. If it isn't on this list, it won't be
+#	                      part of the library. Please try to keep it sorted to
+#	                      maintain sanity.
+#
+#	TEST_SOURCE_FILES     List of programs that will be run as unit tests.
+#
+#	TEST_DATA_FILES       Files from the source three that should be made
+#	                      available in the corresponding location in the build
+#	                      tree in order to run tests there.
+#
+#	EXAMPLE_SOURCE_FILES  Other programs that will be compiled as part of the
+#	                      build, but which is not part of the library nor is
+#	                      run as tests.
+#
+#	PUBLIC_HEADER_FILES   List of public header files that should be
+#	                      distributed together with the library. The source
+#	                      files can of course include other files than these;
+#	                      you should only add to this list if the *user* of
+#	                      the library needs it.
+
+# originally generated with the command:
+# find opm -name '*.c*' -printf '\t%p\n' | sort
+list (APPEND MAIN_SOURCE_FILES
+)
+
+# originally generated with the command:
+# find tests -name '*.cpp' -a ! -wholename '*/not-unit/*' -printf '\t%p\n' | sort
+list (APPEND TEST_SOURCE_FILES
+	)
+
+# originally generated with the command:
+# find tests -name '*.xml' -a ! -wholename '*/not-unit/*' -printf '\t%p\n' | sort
+list (APPEND TEST_DATA_FILES
+	)
+
+# originally generated with the command:
+# find tutorials examples -name '*.c*' -printf '\t%p\n' | sort
+list (APPEND EXAMPLE_SOURCE_FILES
+	)
+
+# programs listed here will not only be compiled, but also marked for
+# installation
+list (APPEND PROGRAM_SOURCE_FILES
+	)
