6c8736f1cedd5a12bda53f6dd19218516609a83c
Kai Bao <kai.bao@sintef.no>
2015-11-10 10:44:38 +0100

    updating ParseMULTSEGWELL intergration test.

--- opm/parser/eclipse/IntegrationTests/ParseMULTSEGWELL.cpp
+++ opm/parser/eclipse/IntegrationTests/ParseMULTSEGWELL.cpp
@@ -37,31 +37,35 @@ BOOST_AUTO_TEST_CASE( PARSE_MULTISEGMENT_ABS ) {
     ParserPtr parser(new Parser());
     boost::filesystem::path deckFile("testdata/integration_tests/SCHEDULE/SCHEDULE_MULTISEGMENT_WELL");
     DeckPtr deck =  parser->parseFile(deckFile.string(), ParseMode());
+    // for WELSEGS keyword
     DeckKeywordConstPtr kw = deck->getKeyword("WELSEGS");
 
     // check the size of the keywords
     BOOST_CHECK_EQUAL( 6, kw->size() );
 
-    DeckRecordConstPtr rec1 = kw->getRecord(0); // top segment
+    // check the information for the top segment and the segment set
+    {
+        DeckRecordConstPtr rec1 = kw->getRecord(0); // top segment
 
-    const std::string well_name = rec1->getItem("WELL")->getTrimmedString(0);
-    const double depth_top = rec1->getItem("DEPTH")->getRawDouble(0);
-    const double length_top = rec1->getItem("LENGTH")->getRawDouble(0);
-    const double volume_top = rec1->getItem("WELLBORE_VOLUME")->getRawDouble(0);
-    const WellSegment::LengthDepthEnum length_depth_type = WellSegment::LengthDepthEnumFromString(rec1->getItem("INFO_TYPE")->getTrimmedString(0));
-    const WellSegment::CompPressureDropEnum comp_pressure_drop = WellSegment::CompPressureDropEnumFromString(rec1->getItem("PRESSURE_COMPONENTS")->getTrimmedString(0));
-    const WellSegment::MultiPhaseModelEnum multiphase_model = WellSegment::MultiPhaseModelEnumFromString(rec1->getItem("FLOW_MODEL")->getTrimmedString(0));
+        const std::string well_name = rec1->getItem("WELL")->getTrimmedString(0);
+        const double depth_top = rec1->getItem("DEPTH")->getRawDouble(0);
+        const double length_top = rec1->getItem("LENGTH")->getRawDouble(0);
+        const double volume_top = rec1->getItem("WELLBORE_VOLUME")->getRawDouble(0);
+        const WellSegment::LengthDepthEnum length_depth_type = WellSegment::LengthDepthEnumFromString(rec1->getItem("INFO_TYPE")->getTrimmedString(0));
+        const WellSegment::CompPressureDropEnum comp_pressure_drop = WellSegment::CompPressureDropEnumFromString(rec1->getItem("PRESSURE_COMPONENTS")->getTrimmedString(0));
+        const WellSegment::MultiPhaseModelEnum multiphase_model = WellSegment::MultiPhaseModelEnumFromString(rec1->getItem("FLOW_MODEL")->getTrimmedString(0));
 
-    BOOST_CHECK_EQUAL( "PROD01", well_name );
-    BOOST_CHECK_EQUAL( 2512.5, depth_top );
-    BOOST_CHECK_EQUAL( 2512.5, length_top );
-    BOOST_CHECK_EQUAL( 1.0e-5, volume_top );
-    const std::string length_depth_type_string = WellSegment::LengthDepthEnumToString(length_depth_type);
-    BOOST_CHECK_EQUAL( length_depth_type_string, "ABS" );
-    const std::string comp_pressure_drop_string = WellSegment::CompPressureDropEnumToString(comp_pressure_drop);
-    BOOST_CHECK_EQUAL( comp_pressure_drop_string, "H--" );
-    const std::string multiphase_model_string = WellSegment::MultiPhaseModelEnumToString(multiphase_model);
-    BOOST_CHECK_EQUAL( multiphase_model_string, "HO" );
+        BOOST_CHECK_EQUAL( "PROD01", well_name );
+        BOOST_CHECK_EQUAL( 2512.5, depth_top );
+        BOOST_CHECK_EQUAL( 2512.5, length_top );
+        BOOST_CHECK_EQUAL( 1.0e-5, volume_top );
+        const std::string length_depth_type_string = WellSegment::LengthDepthEnumToString(length_depth_type);
+        BOOST_CHECK_EQUAL( length_depth_type_string, "ABS" );
+        const std::string comp_pressure_drop_string = WellSegment::CompPressureDropEnumToString(comp_pressure_drop);
+        BOOST_CHECK_EQUAL( comp_pressure_drop_string, "H--" );
+        const std::string multiphase_model_string = WellSegment::MultiPhaseModelEnumToString(multiphase_model);
+        BOOST_CHECK_EQUAL( multiphase_model_string, "HO" );
+    }
 
     // check the information for the other segments
     // Here, we check the information for the segment 2 and 6 as samples.
@@ -73,10 +77,10 @@ BOOST_AUTO_TEST_CASE( PARSE_MULTISEGMENT_ABS ) {
         BOOST_CHECK_EQUAL( 2, segment2 );
         const int branch = rec2->getItem("BRANCH")->getInt(0);
         const int outlet_segment = rec2->getItem("JOIN_SEGMENT")->getInt(0);
-        const double segment_length = rec2->getItem("SEGMENT_LENGTH")->getSIDouble(0);
-        const double depth_change = rec2->getItem("DEPTH_CHANGE")->getSIDouble(0);
-        const double diameter = rec2->getItem("DIAMETER")->getSIDouble(0);
-        const double roughness = rec2->getItem("ROUGHNESS")->getSIDouble(0);
+        const double segment_length = rec2->getItem("SEGMENT_LENGTH")->getRawDouble(0);
+        const double depth_change = rec2->getItem("DEPTH_CHANGE")->getRawDouble(0);
+        const double diameter = rec2->getItem("DIAMETER")->getRawDouble(0);
+        const double roughness = rec2->getItem("ROUGHNESS")->getRawDouble(0);
         BOOST_CHECK_EQUAL( 1, branch );
         BOOST_CHECK_EQUAL( 1, outlet_segment );
         BOOST_CHECK_EQUAL( 2537.5, segment_length );
@@ -93,10 +97,10 @@ BOOST_AUTO_TEST_CASE( PARSE_MULTISEGMENT_ABS ) {
         BOOST_CHECK_EQUAL( 6, segment2 );
         const int branch = rec6->getItem("BRANCH")->getInt(0);
         const int outlet_segment = rec6->getItem("JOIN_SEGMENT")->getInt(0);
-        const double segment_length = rec6->getItem("SEGMENT_LENGTH")->getSIDouble(0);
-        const double depth_change = rec6->getItem("DEPTH_CHANGE")->getSIDouble(0);
-        const double diameter = rec6->getItem("DIAMETER")->getSIDouble(0);
-        const double roughness = rec6->getItem("ROUGHNESS")->getSIDouble(0);
+        const double segment_length = rec6->getItem("SEGMENT_LENGTH")->getRawDouble(0);
+        const double depth_change = rec6->getItem("DEPTH_CHANGE")->getRawDouble(0);
+        const double diameter = rec6->getItem("DIAMETER")->getRawDouble(0);
+        const double roughness = rec6->getItem("ROUGHNESS")->getRawDouble(0);
         BOOST_CHECK_EQUAL( 2, branch );
         BOOST_CHECK_EQUAL( 5, outlet_segment );
         BOOST_CHECK_EQUAL( 3137.5, segment_length );
@@ -104,5 +108,52 @@ BOOST_AUTO_TEST_CASE( PARSE_MULTISEGMENT_ABS ) {
         BOOST_CHECK_EQUAL( 0.2, diameter );
         BOOST_CHECK_EQUAL( 0.0001, roughness );
     }
+
+    // for COMPSEG keyword
+    DeckKeywordConstPtr kw1 = deck->getKeyword("COMPSEGS");
+    // check the size of the keywords
+    BOOST_CHECK_EQUAL( 7, kw1->size() );
+    // first record only contains the well name
+    {
+        DeckRecordConstPtr rec1 = kw1->getRecord(0);
+        const std::string well_name = rec1->getItem("WELL")->getTrimmedString(0);
+        BOOST_CHECK_EQUAL( "PROD01", well_name );
+    }
+
+    // check the third record and the seventh record
+    {
+        DeckRecordConstPtr rec3 = kw1->getRecord(2);
+        const int i = rec3->getItem("I")->getInt(0);
+        const int j = rec3->getItem("J")->getInt(0);
+        const int k = rec3->getItem("K")->getInt(0);
+        const int branch = rec3->getItem("BRANCH")->getInt(0);
+        const double distance_start = rec3->getItem("DISTANCE_START")->getRawDouble(0);
+        const double distance_end = rec3->getItem("DISTANCE_END")->getRawDouble(0);
+
+        BOOST_CHECK_EQUAL( 20, i );
+        BOOST_CHECK_EQUAL(  1, j );
+        BOOST_CHECK_EQUAL(  2, k );
+        BOOST_CHECK_EQUAL(  1, branch );
+        BOOST_CHECK_EQUAL(  2525.0, distance_start );
+        BOOST_CHECK_EQUAL(  2550.0, distance_end );
+    }
+
+    {
+        DeckRecordConstPtr rec7 = kw1->getRecord(6);
+        const int i = rec7->getItem("I")->getInt(0);
+        const int j = rec7->getItem("J")->getInt(0);
+        const int k = rec7->getItem("K")->getInt(0);
+        const int branch = rec7->getItem("BRANCH")->getInt(0);
+        const double distance_start = rec7->getItem("DISTANCE_START")->getRawDouble(0);
+        const double distance_end = rec7->getItem("DISTANCE_END")->getRawDouble(0);
+
+        BOOST_CHECK_EQUAL( 17, i );
+        BOOST_CHECK_EQUAL(  1, j );
+        BOOST_CHECK_EQUAL(  2, k );
+        BOOST_CHECK_EQUAL(  2, branch );
+        BOOST_CHECK_EQUAL(  3037.5, distance_start );
+        BOOST_CHECK_EQUAL(  3237.5, distance_end );
+    }
+
 
 }
