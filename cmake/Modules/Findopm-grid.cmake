c2b36fa760ecf2c665bd40f7662db8c404f2bc8e
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2016-04-05 14:54:08 +0200

    Fix Findopm-grid.cmake module.

--- cmake/Modules/Findopm-grid.cmake
+++ cmake/Modules/Findopm-grid.cmake
@@ -1,23 +1,23 @@
 # - Find OPM corner-point grid library
 #
 # Defines the following variables:
-#   opm-core_INCLUDE_DIRS    Directory of header files
-#   opm-core_LIBRARIES       Directory of shared object files
-#   opm-core_DEFINITIONS     Defines that must be set to compile
-#   opm-core_CONFIG_VARS     List of defines that should be in config.h
+#   opm-grid_INCLUDE_DIRS    Directory of header files
+#   opm-grid_LIBRARIES       Directory of shared object files
+#   opm-grid_DEFINITIONS     Defines that must be set to compile
+#   opm-grid_CONFIG_VARS     List of defines that should be in config.h
 #   HAVE_OPM_GRID            Binary value to use in config.h
 
 # Copyright (C) 2013 Uni Research AS
 # This code is licensed under The GNU General Public License v3.0
 
-include (opm-core-prereqs)
+include (opm-grid-prereqs)
 include (OpmPackage)
 find_opm_package (
   # module name
-  "opm-core"
+  "opm-grid"
 
   # dependencies
-  "${opm-core_DEPS}"
+  "${opm-grid_DEPS}"
   
   # header to search for
   "dune/grid/CpGrid.hpp"
@@ -36,7 +36,7 @@ int main (void) {
 }
 "
   # config variables
-  "${opm-core_CONFIG_VAR}"
+  "${opm-grid_CONFIG_VAR}"
   )
 
-#debug_find_vars ("opm-core")
+#debug_find_vars ("opm-grid")
