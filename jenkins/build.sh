5e9e9790514786d928ba90eba8ac51d6c8a00f6e
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2016-08-23 15:27:25 +0200

    add multiple build configuration support to jenkins scripts
    will be used for mpi-enabled builds
    
    note options required for opm-parser, ewoms have been
    moved to the build configurations for simpler maintenance.

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -6,30 +6,7 @@ source `dirname $0`/build-opm-module.sh
 mkdir deps
 ln -sf $WORKSPACE deps/opm-common
 
-pushd .
-mkdir -p serial/build-opm-common
-cd serial/build-opm-common
-build_module "-DCMAKE_INSTALL_PREFIX=$WORKSPACE/serial/install" 1 $WORKSPACE
-test $? -eq 0 || exit 1
-popd
-
-# If no downstream builds we are done
-if ! grep -q "with downstreams" <<< $ghprbCommentBody
-then
-  cp serial/build-opm-common/testoutput.xml .
-  exit 0
-fi
-
-ERT_REVISION=master
-
-if grep -q "ert=" <<< $ghprbCommentBody
-then
-  ERT_REVISION=pull/`echo $ghprbCommentBody | sed -r 's/.*ert=([0-9]+).*/\1/g'`/merge
-fi
-
-source $WORKSPACE/deps/opm-common/jenkins/setup-opm-data.sh
-
-# Downstream revisions
+# Downstreams and revisions
 declare -a downstreams
 downstreams=(ert
              opm-parser
@@ -52,6 +29,13 @@ downstreamRev[opm-simulators]=master
 downstreamRev[opm-upscaling]=master
 downstreamRev[ewoms]=master
 
-build_downstreams opm-common
+parseRevisions
+printHeader opm-common
 
-test $? -eq 0 || exit 1
+# Setup opm-data if necessary
+if grep -q "with downstreams" <<< $ghprbCommentBody
+then
+  source $WORKSPACE/deps/opm-common/jenkins/setup-opm-data.sh
+fi
+
+build_module_full opm-common
