7da4e4a91947b213c4c78e7c17270135153dada8
Markus Blatt <markus@dr-blatt.de>
2016-09-16 09:46:55 +0200

    Resort to inherit StreamLog constructors in EclipsePRTLog.
    That way we will never miss a constructor. Before this commit
    some of them were actually missing.

--- opm/common/OpmLog/EclipsePRTLog.hpp
+++ opm/common/OpmLog/EclipsePRTLog.hpp
@@ -29,6 +29,7 @@ namespace Opm {
 class EclipsePRTLog : public StreamLog {
 
 public:
+    using StreamLog::StreamLog;
 
     void addTaggedMessage(int64_t messageType, const std::string& messageTag, const std::string& message);
 
@@ -37,10 +38,10 @@ public:
     ~EclipsePRTLog();
 
     EclipsePRTLog(const std::string& logFile , int64_t messageMask,
-                  bool append, bool print_summary=true);
+                  bool append, bool print_summary);
 
     EclipsePRTLog(std::ostream& os , int64_t messageMask,
-                  bool print_summary=true);
+                  bool print_summary);
 private:
     std::map<int64_t, size_t> m_count;
     bool print_summary_;
