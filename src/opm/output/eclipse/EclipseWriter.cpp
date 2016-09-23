eb85e1b02c681a4df4d011224c60a7c6f16c5e3d
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2016-09-23 15:00:16 +0200

    Remove unused grid argument in RFT::writeTimeStep().

--- src/opm/output/eclipse/EclipseWriter.cpp
+++ src/opm/output/eclipse/EclipseWriter.cpp
@@ -313,7 +313,6 @@ class RFT {
              const EclipseGrid& grid_);
 
         void writeTimeStep( std::vector< const Well* >,
-                            const EclipseGrid& grid,
                             int report_step,
                             time_t current_time,
                             double days,
@@ -347,7 +346,6 @@ inline ert_ecl_unit_enum to_ert_unit( UnitSystem::UnitType t ) {
 }
 
 void RFT::writeTimeStep( std::vector< const Well* > wells,
-                         const EclipseGrid& grid,
                          int report_step,
                          time_t current_time,
                          double days,
@@ -751,7 +749,6 @@ void EclipseWriter::writeTimeStep(int report_step,
 
     const auto unit_type = es.getDeckUnitSystem().getType();
     this->impl->rft.writeTimeStep( schedule.getWells( report_step ),
-                                   grid,
                                    report_step,
                                    current_posix_time,
                                    days,
