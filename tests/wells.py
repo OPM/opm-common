25bd997ffd8661ca5cdd36612f171e9a1677a4ca
Jørgen Kvalsvik <jokva@statoil.com>
2016-12-09 15:51:07 +0100

    Remove broken well test

--- tests/wells.py
+++ tests/wells.py
@@ -7,16 +7,6 @@ class TestWells(unittest.TestCase):
     def setUp(self):
         self.wells = spe3.schedule.wells
 
-    def testWells(self):
-        self.assertEqual(2, len(self.wells))
-
-        well = wells
-        well.I
-        well.I( timestep )
-        (I, J, refDepth ) = well.pos( timestep )
-
-        well.at( timestep ).I
-
     def testWellPos0(self):
         well = self.wells[0]
         i, j, refdepth = well.pos(0)
