9e23ad144bf418e0c6113a77dc0f0e36a166ec38
Joakim Hove <joakim.hove@gmail.com>
2017-02-15 14:24:18 +0100

    Added very basic Eclipse3DProperties in Python.

--- opm/parser/eclipse/python/python/opm/ecl_state/grid/__init__.py
+++ opm/parser/eclipse/python/python/opm/ecl_state/grid/__init__.py
@@ -1,1 +1,2 @@
 from .eclipse_grid import EclipseGrid
+from .ecl_props import Eclipse3DProps
---
+++ opm/parser/eclipse/python/python/opm/ecl_state/grid/ecl_props.py
@@ -0,0 +1,21 @@
+from cwrap import BaseCClass
+
+from opm import OPMPrototype
+from opm.deck import Deck
+from opm.ecl_state.grid import EclipseGrid
+from opm.ecl_state.tables import TableManager
+
+
+class Eclipse3DProps(BaseCClass):
+    TYPE_NAME = "eclipse_3d_props"
+    _alloc = OPMPrototype("void* eclipse3d_properties_alloc( deck , table_manager , eclipse_grid)" , bind = False)
+    _free  = OPMPrototype("void  eclipse3d_properties_free( eclipse_3d_props )")
+
+    
+    def __init__(self , deck, tables, grid):
+        c_ptr = self._alloc( deck , tables, grid )
+        super(Eclipse3DProps , self).__init__( c_ptr )
+
+
+    def free(self):
+        self._free( )
