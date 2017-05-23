e884b0664c2a33273f15f7777fede79087d53bd0
Jørgen Kvalsvik <jokva@statoil.com>
2017-05-23 12:03:08 +0200

    Redesign cmake
    Tune the makefile according to new principles, which adds a few bells
    and whistles and for clarity.
    
    Synopsis:
    
    * The dependency on opm-common is completely gone. This is reflected in
      travis and appveyor as well. No non-kitware cmake modules are used.
    * Directories are flattened, quite a bit - source code is located in the
      lib/ directory if it belongs to opm-parser, and external/ if third
      party.
    * The sibling build feature is implemented through cmake's
      export(PACKAGE) rather than implicitly looking through source files.
    * Targets explicitly set required public and private include
      directories, compile options and definitions, which cmake will handle
      and propagate
    * opm-parser-config.cmake for downstream users is now provided.
    * Dependencies are set up using targets. In the future, when cmake 3.x+
      can be used, these should be either targets from newer Find modules,
      or interface libraries.
    * Fewer system specific assumptions are coded in, instead we assume
      cmake or users set up system specific details.
    * All module wide configuration and looking up libraries is handled in
      the root makefile - all sub directories only set up libraries and
      compile options for the module in question.
    * Targets are defined and links handled transitively because cmake now
      is told about them. ${module_LIBRARIES} variables are gone.
    
    This is largely guided by the principles outlined in
    https://rix0r.nl/blog/2015/08/13/cmake-guide/
    
    Most source files are just moved - if they have some content change then
    it's nothing more than include fixes or similar in order to make them
    compile.

---
+++ src/opm/parser/eclipse/IntegrationTests/ParsePLYADSS.cpp
@@ -0,0 +1,41 @@
+/*
+  Copyright 2013 Statoil ASA.
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define BOOST_TEST_MODULE ParsePLYVISC
+#include <boost/test/unit_test.hpp>
+
+#include <opm/parser/eclipse/Deck/Deck.hpp>
+#include <opm/parser/eclipse/Deck/DeckKeyword.hpp>
+#include <opm/parser/eclipse/Parser/Parser.hpp>
+#include <opm/parser/eclipse/Parser/ParseContext.hpp>
+
+using namespace Opm;
+
+inline std::string prefix() {
+    return boost::unit_test::framework::master_test_suite().argv[1];
+}
+
+BOOST_AUTO_TEST_CASE( PARSE_PLYADSS_OK) {
+    Parser parser;
+    std::string deckFile(prefix() + "POLYMER/plyadss.data");
+    auto deck =  parser.parseFile(deckFile, ParseContext());
+    const auto& kw = deck.getKeyword("PLYADSS");
+
+    BOOST_CHECK_EQUAL( kw.size() , 11U );
+}
---
+++ src/opm/parser/eclipse/IntegrationTests/TransMultIntegrationTests.cpp
@@ -0,0 +1,54 @@
+/*
+  Copyright 2013 Statoil ASA.
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define BOOST_TEST_MODULE TransMultTests
+#include <boost/test/unit_test.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+#include <opm/parser/eclipse/Deck/Deck.hpp>
+#include <opm/parser/eclipse/Parser/Parser.hpp>
+#include <opm/parser/eclipse/Parser/ParseContext.hpp>
+#include <opm/parser/eclipse/EclipseState/EclipseState.hpp>
+#include <opm/parser/eclipse/EclipseState/Schedule/Schedule.hpp>
+
+using namespace Opm;
+
+inline std::string pathprefix() {
+    return boost::unit_test::framework::master_test_suite().argv[1];
+}
+
+BOOST_AUTO_TEST_CASE(MULTFLT_IN_SCHEDULE) {
+    Parser parser;
+    std::string scheduleFile(pathprefix() + "TRANS/Deck1");
+    ParseContext parseContext;
+    auto deck = parser.parseFile(scheduleFile, parseContext);
+    EclipseState state(deck, parseContext);
+    const auto& trans = state.getTransMult();
+    const auto& schedule = state.getSchedule();
+    const Events& events = schedule.getEvents();
+
+    BOOST_CHECK_EQUAL( 0.10 , trans.getMultiplier( 3,2,0,FaceDir::XPlus ));
+    BOOST_CHECK_EQUAL( 0.10 , trans.getMultiplier( 2,2,0,FaceDir::XPlus ));
+    BOOST_CHECK( events.hasEvent( ScheduleEvents::GEO_MODIFIER , 3 ) );
+    {
+        const auto& mini_deck = schedule.getModifierDeck(3);
+        state.applyModifierDeck( mini_deck );
+    }
+    BOOST_CHECK_EQUAL( 2.00 , trans.getMultiplier( 2,2,0,FaceDir::XPlus ));
+    BOOST_CHECK_EQUAL( 0.10 , trans.getMultiplier( 3,2,0,FaceDir::XPlus ));
+}
