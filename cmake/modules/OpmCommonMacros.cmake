4c933bcecdcff9057733ccccb1b6859b3819e44c
Andreas Lauser <and@poware.org>
2017-11-01 13:02:42 +0100

    detect OpenMP
    it is not turned on by default even if it is available.

--- cmake/modules/OpmCommonMacros.cmake
+++ cmake/modules/OpmCommonMacros.cmake
@@ -4,6 +4,17 @@
 # suggests opm-common!
 #
 
+option(ENABLE_OPENMP "Turn OpenMP on if it is available?" OFF)
+if(ENABLE_OPENMP)
+  find_package(OpenMP)
+
+  if(OPENMP_FOUND)
+    set(HAVE_OPENMP 1)
+    dune_register_package_flags(
+      COMPILE_OPTIONS "${OpenMP_CXX_FLAGS}"
+      LIBRARIES "${OpenMP_CXX_FLAGS}")
+  endif()
+endif()
 
 # Check if valgrind library is available or not.
 find_package(Valgrind)
