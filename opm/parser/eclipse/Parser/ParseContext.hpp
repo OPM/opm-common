22074a46bf334ce0c9ad021b8c93fa5c0ca50220
Joakim Hove <joakim.hove@gmail.com>
2017-12-03 08:30:04 +0100

    Added one-argument ParseContext constructor.

--- opm/parser/eclipse/Parser/ParseContext.hpp
+++ opm/parser/eclipse/Parser/ParseContext.hpp
@@ -79,7 +79,9 @@ namespace Opm {
     class ParseContext {
     public:
         ParseContext();
+        explicit ParseContext(InputError::Action default_action);
         explicit ParseContext(const std::vector<std::pair<std::string , InputError::Action>>& initial);
+
         Message::type handleError( const std::string& errorKey, MessageContainer& msgContainer, const std::string& msg ) const;
         bool hasKey(const std::string& key) const;
         ParseContext  withKey(const std::string& key, InputError::Action action = InputError::WARN) const;
