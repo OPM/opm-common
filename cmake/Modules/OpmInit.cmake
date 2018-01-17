de7f4e168d06695a9eb43f395d5ec5053baa4f1a
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2018-01-17 15:26:26 +0100

    fixed: we need to do mpi disabling earlier
    if not, prereqs do not see this file, and the option ends
    up not being operational for prereqs looking for MPI.

--- cmake/Modules/OpmInit.cmake
+++ cmake/Modules/OpmInit.cmake
@@ -74,3 +74,10 @@ if (USE_VERSIONED_DIR)
 else ()
   set (${project}_VER_DIR "")
 endif ()
+
+# parallel computing must be explicitly enabled
+# This needs to be in OpmInit as prereqs is called before OpmLibMain is included.
+option (USE_MPI "Use Message Passing Interface for parallel computing" OFF)
+if (NOT USE_MPI)
+  set (CMAKE_DISABLE_FIND_PACKAGE_MPI TRUE)
+endif ()
