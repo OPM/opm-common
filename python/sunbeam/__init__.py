ee0bbb70bb51d0edd436e4778e6e693693e6d428
Joakim Hove <joakim.hove@gmail.com>
2018-02-14 21:19:15 +0100

    Raised version number to 0.0.4

--- python/sunbeam/__init__.py
+++ python/sunbeam/__init__.py
@@ -7,5 +7,5 @@ from .libsunbeam import action
 from .config     import EclipseConfig
 from .parser     import parse_deck, parse
 
-__version__     = '0.0.3'
+__version__     = '0.0.4'
 __license__     = 'GNU General Public License version 3'
