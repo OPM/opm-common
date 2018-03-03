1083db48a3be7fe697beaeb2b50d6749a9469abe
Andreas Lauser <and@poware.org>
2018-03-03 13:23:37 +0100

    test_LinearisedOutputTable: include config.h unconditionally
    this is already the case for the vast majority of the other OPM code.

--- tests/test_LinearisedOutputTable.cpp
+++ tests/test_LinearisedOutputTable.cpp
@@ -16,10 +16,7 @@
   You should have received a copy of the GNU General Public License
   along with OPM.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-#if defined(HAVE_CONFIG_H)
-#include <config.h>
-#endif  // defined(HAVE_CONFIG_H)
+#include "config.h"
 
 #define NVERBOSE
 
