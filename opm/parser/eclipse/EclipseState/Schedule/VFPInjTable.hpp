5d9c6f9fc4771d00fb0e06ed6a93ecd0f5c31b3a
Joakim Hove <joakim.hove@gmail.com>
2018-04-12 18:50:23 +0200

    Remove comments

--- opm/parser/eclipse/EclipseState/Schedule/VFPInjTable.hpp
+++ opm/parser/eclipse/EclipseState/Schedule/VFPInjTable.hpp
@@ -31,25 +31,18 @@ namespace Opm {
 
     class DeckKeyword;
 
-/**
- * Class for reading data from a VFPINJ (vertical flow performance injection) table
- */
 class VFPInjTable {
 public:
     typedef boost::multi_array<double, 2> array_type;
     typedef boost::array<array_type::index, 2> extents;
 
-    ///Rate type
     enum FLO_TYPE {
-        FLO_OIL=1, //< Oil rate
-        FLO_WAT, //< Water rate
-        FLO_GAS, //< Gas rate
+        FLO_OIL=1,
+        FLO_WAT,
+        FLO_GAS,
         FLO_INVALID
     };
 
-    /**
-     * Constructor
-     */
     inline VFPInjTable() : m_table_num(-1),
             m_datum_depth(-1),
             m_flo_type(FLO_INVALID) {
@@ -65,16 +58,6 @@ public:
 
   VFPInjTable(const DeckKeyword& table, const UnitSystem& deck_unit_system);
 
-    /**
-         * Initializes objects from raw data. NOTE: All raw data assumed to be in SI units
-         * @param table_num VFP table number
-         * @param datum_depth Reference depth for BHP
-         * @param flo_type Specifies what flo_data represents
-         * @param flo_data Axis for flo_type
-         * @param thp_data Axis for thp_type
-         * @param data BHP to be interpolated. Given as a 2D array so that
-         *        BHP = data[thp][flo] for the indices thp, flo.
-         */
         void init(int table_num,
                 double datum_depth,
                 FLO_TYPE flo_type,
@@ -82,48 +65,24 @@ public:
                 const std::vector<double>& thp_data,
                 const array_type& data);
 
-        /**
-         * Constructor which parses a deck keyword and retrieves the relevant parts for a
-         * VFP table.
-         */
         void init(const DeckKeyword& table, const UnitSystem& deck_unit_system);
 
-        /**
-         * Returns the table number
-         * @return table number
-         */
         inline int getTableNum() const {
             return m_table_num;
         }
 
-        /**
-         * Returns the datum depth for the table data
-         * @return datum depth
-         */
         inline double getDatumDepth() const {
             return m_datum_depth;
         }
 
-        /**
-         * Returns the rate/flo type for the flo axis
-         * @return flo type
-         */
         inline FLO_TYPE getFloType() const {
             return m_flo_type;
         }
 
-        /**
-         * Returns the coordinates of the FLO sample points in the table
-         * @return Flo sample coordinates
-         */
         inline const std::vector<double>& getFloAxis() const {
             return m_flo_data;
         }
 
-        /**
-         * Returns the coordinates for the tubing head pressure sample points in the table
-         * @return Tubing head pressure coordinates
-         */
         inline const std::vector<double>& getTHPAxis() const {
             return m_thp_data;
         }
@@ -147,22 +106,15 @@ public:
         }
 private:
 
-    //"Header" variables
     int m_table_num;
     double m_datum_depth;
     FLO_TYPE m_flo_type;
 
-    //The actual table axes
     std::vector<double> m_flo_data;
     std::vector<double> m_thp_data;
 
-    //The data itself, using the data ordering m_data[thp][flo]
-    array_type m_data;
 
-    /**
-     * Debug function that runs a series of asserts to check for sanity of inputs.
-     * Called after init to check that everything looks ok.
-     */
+    array_type m_data;
     void check();
 
     static FLO_TYPE getFloType(std::string flo_string);
@@ -178,7 +130,7 @@ private:
 };
 
 
-} //Namespace Opm
+}
 
 
-#endif /* OPM_PARSER_ECLIPSE_ECLIPSESTATE_TABLES_VFPINJTABLE_HPP_ */
+#endif
