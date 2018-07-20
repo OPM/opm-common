a41596004a2ab98684b652894439cf28868c8e69
Jostein Alvestad <jal@statoil.com>
2018-07-20 15:16:58 +0200

    Added  correction for writing of unformatted restart

--- src/opm/output/eclipse/libECLRestart.cpp
+++ src/opm/output/eclipse/libECLRestart.cpp
@@ -219,6 +219,44 @@ static const char * string_type = "int";
 
 
 
+#if UINTPTR_MAX == 0xFFFFFFFF
+#define ARCH32
+#elif UINTPTR_MAX == 0xFFFFFFFFFFFFFFFF
+#define ARCH64
+#else
+#error "Could not determine if this is a 32 bit or 64 bit computer?"
+#endif
+
+
+/*
+   Macros for endian flipping. The macros create a new endian-flipped
+   value, and should be used as: 
+
+     flipped_value = FLIP32( value )
+ 
+   The macros are not exported and only available through the function
+   util_endian_flip_vector().  
+*/
+
+
+#define FLIP16(var) (((var >> 8) & 0x00ff) | ((var << 8) & 0xff00))
+
+#define FLIP32(var) (( (var >> 24) & 0x000000ff) | \
+                      ((var >>  8) & 0x0000ff00) | \
+                      ((var <<  8) & 0x00ff0000) | \
+                      ((var << 24) & 0xff000000))
+
+#define FLIP64(var)  (((var >> 56) & 0x00000000000000ff) | \
+                      ((var >> 40) & 0x000000000000ff00) | \
+                      ((var >> 24) & 0x0000000000ff0000) | \
+                      ((var >>  8) & 0x00000000ff000000) | \
+                      ((var <<  8) & 0x000000ff00000000) | \
+                      ((var << 24) & 0x0000ff0000000000) | \
+                      ((var << 40) & 0x00ff000000000000) | \
+                      ((var << 56) & 0xff00000000000000))
+
+
+
 
 struct int_vector_struct {
   UTIL_TYPE_ID_DECLARATION;
@@ -1401,6 +1439,7 @@ bool ecl_type_is_numeric(const ::Opm::RestartIO::ecl_data_type ecl_type) {
           ::Opm::RestartIO::ecl_type_is_double(ecl_type));
 }
 
+
 static uint16_t util_endian_convert16( uint16_t u ) {
   return (( u >> 8U ) & 0xFFU) | (( u & 0xFFU) >> 8U);
 }
@@ -1409,7 +1448,7 @@ static uint16_t util_endian_convert16( uint16_t u ) {
 static uint32_t util_endian_convert32( uint32_t u ) {
   const uint32_t m8  = (uint32_t) 0x00FF00FFUL;
   const uint32_t m16 = (uint32_t) 0x0000FFFFUL;
-
+  
   u = (( u >> 8U ) & m8)   | ((u & m8) << 8U);
   u = (( u >> 16U ) & m16) | ((u & m16) << 16U);
   return u;
@@ -1421,24 +1460,37 @@ static uint64_t util_endian_convert64( uint64_t u ) {
   const uint64_t m16 = (uint64_t) 0x0000FFFF0000FFFFULL;
   const uint64_t m32 = (uint64_t) 0x00000000FFFFFFFFULL;
 
-
+  
   u = (( u >> 8U ) & m8)   | ((u & m8) << 8U);
   u = (( u >> 16U ) & m16) | ((u & m16) << 16U);
   u = (( u >> 32U ) & m32) | ((u & m32) << 32U);
   return u;
 }
 
+
+static uint64_t util_endian_convert32_64( uint64_t u ) {
+  const uint64_t m8  = (uint64_t) 0x00FF00FF00FF00FFULL;
+  const uint64_t m16 = (uint64_t) 0x0000FFFF0000FFFFULL;
+
+  
+  u = (( u >> 8U ) & m8)   | ((u & m8) << 8U);
+  u = (( u >> 16U ) & m16) | ((u & m16) << 16U);
+  return u;
+}
+
+
+
 void util_endian_flip_vector(void *data, int element_size , int elements) {
   int i;
   switch (element_size) {
   case(1):
     break;
-  case(2):
+  case(2): 
     {
       uint16_t *tmp16 = (uint16_t *) data;
 
       for (i = 0; i <elements; i++)
-        tmp16[i] = ::Opm::RestartIO::util_endian_convert16(tmp16[i]);
+        tmp16[i] = util_endian_convert16(tmp16[i]);
       break;
     }
   case(4):
@@ -1449,25 +1501,25 @@ void util_endian_flip_vector(void *data, int element_size , int elements) {
         variables will be by swapping two elements in one operation;
         this is provided by the util_endian_convert32_64() function. In the case
         of binary ECLIPSE files this case is quite common, and
-        therefor worth supporting as a special case.
+        therefore worth supporting as a special case.
       */
       uint64_t *tmp64 = (uint64_t *) data;
 
       for (i = 0; i <elements/2; i++)
-        tmp64[i] = ::Opm::RestartIO::util_endian_convert32_64(tmp64[i]);
+        tmp64[i] = util_endian_convert32_64(tmp64[i]);
 
       if ( elements & 1 ) {
         // Odd number of elements - flip the last element as an ordinary 32 bit swap.
         uint32_t *tmp32 = (uint32_t *) data;
-        tmp32[ elements - 1] = ::Opm::RestartIO::util_endian_convert32( tmp32[elements - 1] );
+        tmp32[ elements - 1] = util_endian_convert32( tmp32[elements - 1] );
       }
       break;
 #else
       uint32_t *tmp32 = (uint32_t *) data;
-
+      
       for (i = 0; i <elements; i++)
         tmp32[i] = util_endian_convert32(tmp32[i]);
-
+      
       break;
 #endif
     }
@@ -1476,7 +1528,7 @@ void util_endian_flip_vector(void *data, int element_size , int elements) {
       uint64_t *tmp64 = (uint64_t *) data;
 
       for (i = 0; i <elements; i++)
-        tmp64[i] = ::Opm::RestartIO::util_endian_convert64(tmp64[i]);
+        tmp64[i] = util_endian_convert64(tmp64[i]);
       break;
     }
   default:
@@ -1484,6 +1536,7 @@ void util_endian_flip_vector(void *data, int element_size , int elements) {
     util_abort("%s: can only endian flip 1/2/4/8 byte variables - aborting \n",__func__);
   }
 }
+
 
 void ecl_kw_iset(::Opm::RestartIO::ecl_kw_type *ecl_kw , int i , const void *iptr) {
   ::Opm::RestartIO::ecl_kw_iset_static(ecl_kw , i , iptr);
@@ -3865,41 +3918,63 @@ void ecl_kw_fwrite_header(const ::Opm::RestartIO::ecl_kw_type *ecl_kw , fortio_t
 }
 
 
-static void ecl_kw_fwrite_data_unformatted( ::Opm::RestartIO::ecl_kw_type * ecl_kw , fortio_type * fortio ) {
-  if (ECL_ENDIAN_FLIP)
-    ::Opm::RestartIO::ecl_kw_endian_convert_data(ecl_kw);
 
+static char * ecl_kw_alloc_output_buffer(const ::Opm::RestartIO::ecl_kw_type * ecl_kw) {
+  size_t sizeof_iotype = ecl_type_get_sizeof_iotype(ecl_kw->data_type);
+  size_t buffer_size = ecl_kw->size * sizeof_iotype;
+  char * buffer = (char*)util_malloc( buffer_size );
+
+  if (ecl_type_is_bool(ecl_kw->data_type)) {
+    int * int_data = (int *) buffer;
+    bool * bool_data = (bool *) ecl_kw->data;
+
+    for (int i=0; i < ecl_kw->size; i++)
+      if (bool_data[i])
+        int_data[i] = ECL_BOOL_TRUE_INT;
+      else
+        int_data[i] = ECL_BOOL_FALSE_INT;
+
+    util_endian_flip_vector(buffer, sizeof_iotype, ecl_kw->size);
+    return buffer;
+  }
+
+  if (ecl_type_is_char(ecl_kw->data_type) || ecl_type_is_string(ecl_kw->data_type)) {
+    size_t sizeof_ctype = ecl_type_get_sizeof_ctype(ecl_kw->data_type);
+    for (int i=0; i < ecl_kw->size; i++) {
+      size_t buffer_offset = i * sizeof_iotype;
+      size_t data_offset = i * sizeof_ctype;
+      memcpy(&buffer[buffer_offset], &ecl_kw->data[data_offset], sizeof_iotype);
+    }
+
+    return buffer;
+  }
+
+  if (ecl_type_is_mess(ecl_kw->data_type))
+    return buffer;
+
+  memcpy(buffer, ecl_kw->data, buffer_size);
+  util_endian_flip_vector(buffer, sizeof_iotype, ecl_kw->size);
+  return buffer;
+}
+
+
+static void ecl_kw_fwrite_data_unformatted( const ::Opm::RestartIO::ecl_kw_type * ecl_kw , fortio_type * fortio ) {
+  char * iobuffer = ecl_kw_alloc_output_buffer(ecl_kw);
+  int sizeof_iotype = ecl_type_get_sizeof_iotype(ecl_kw->data_type);
   {
-    const int blocksize  = ::Opm::RestartIO::get_blocksize( ecl_kw->data_type );
+    const int blocksize  = get_blocksize( ecl_kw->data_type );
     const int num_blocks = ecl_kw->size / blocksize + (ecl_kw->size % blocksize == 0 ? 0 : 1);
     int block_nr;
 
     for (block_nr = 0; block_nr < num_blocks; block_nr++) {
       int this_blocksize = util_int_min((block_nr + 1)*blocksize , ecl_kw->size) - block_nr*blocksize;
-      if (::Opm::RestartIO::ecl_type_is_char(ecl_kw->data_type) || ::Opm::RestartIO::ecl_type_is_mess(ecl_kw->data_type) || ::Opm::RestartIO::ecl_type_is_string(ecl_kw->data_type)) {
-        /*
-           Due to the terminating \0 characters there is not a
-           continous file/memory mapping - the \0 characters arel
-           skipped.
-        */
-        FILE *stream    = fortio_get_FILE(fortio);
-        int word_size   = ::Opm::RestartIO::ecl_type_get_sizeof_ctype_fortio(ecl_kw->data_type);
-        int record_size = this_blocksize * word_size;     /* The total size in bytes of the record written by the fortio layer. */
-        int i;
-        fortio_init_write(fortio , record_size );
-        for (i = 0; i < this_blocksize; i++)
-          fwrite(&ecl_kw->data[(block_nr * blocksize + i) * ::Opm::RestartIO::ecl_kw_get_sizeof_ctype(ecl_kw)] , 1 , word_size , stream);
-        fortio_complete_write(fortio , record_size);
-      } else {
-        int   record_size = this_blocksize * ::Opm::RestartIO::ecl_kw_get_sizeof_ctype(ecl_kw);  /* The total size in bytes of the record written by the fortio layer. */
-        fortio_fwrite_record(fortio , &ecl_kw->data[block_nr * blocksize * ::Opm::RestartIO::ecl_kw_get_sizeof_ctype(ecl_kw)] , record_size);
-      }
+      int record_size = this_blocksize * sizeof_iotype;  /* The total size in bytes of the record written by the fortio layer. */
+      fortio_fwrite_record(fortio , &iobuffer[block_nr * blocksize * sizeof_iotype] , record_size);
     }
   }
-
-  if (ECL_ENDIAN_FLIP)
-    ::Opm::RestartIO::ecl_kw_endian_convert_data(ecl_kw);
+  free(iobuffer);
 }
+
 
 
 static int get_columns(const ::Opm::RestartIO::ecl_data_type data_type) {
