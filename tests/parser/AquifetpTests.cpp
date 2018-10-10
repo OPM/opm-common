3819910396735544bad24eca8abd9f42941496f2
WesselZ <wessel.zeeuw@tno.nl>
2018-10-10 11:35:22 +0200

    Modification of AquifetpTests.cpp -- 100% test passing Added nempty Aquifetp container -- Null object -- No parsing, succesfull

--- tests/parser/AquifetpTests.cpp
+++ tests/parser/AquifetpTests.cpp
@@ -51,8 +51,42 @@ inline Deck createAquifetpDeck() {
         "SOLUTION\n"
         "\n"
         "AQUFETP\n"
-        "   1	70000.0	4.0e3	2.0e9	1.0e-5	500	1	0	0 /\n"
-        "/ \n";
+        "1  70000.0  4.0e3 2.0e9 1.0e-5	500 1 0	0\n"
+        "/\n";
+
+    Parser parser;
+    return parser.parseString(deckData, ParseContext());
+}
+
+inline Deck createNullAquifetpDeck(){
+     const char *deckData =        
+	"DIMENS\n"
+        "3 3 3 /\n"
+        "\n"
+        "AQUDIMS\n"
+        "1* 1* 2 100 1 1000 /\n"
+        "GRID\n"
+        "\n"
+        "ACTNUM\n"
+        " 0 8*1 0 8*1 0 8*1 /\n"
+        "DXV\n"
+        "1 1 1 /\n"
+        "\n"
+        "DYV\n"
+        "1 1 1 /\n"
+        "\n"
+        "DZV\n"
+        "1 1 1 /\n"
+        "\n"
+        "TOPS\n"
+        "9*100 /\n"
+        "\n"
+        "PROPS\n"
+        "AQUTAB\n"
+        " 0.01 0.112 \n" 
+        " 0.05 0.229 /\n" 
+        "SOLUTION\n"
+         ;
 
     Parser parser;
     return parser.parseString(deckData, ParseContext());
@@ -66,12 +100,21 @@ inline std::vector<Aquifetp::AQUFETP_data> init_aquifetp(){
     return aquifetp;
 }
 
+inline std::vector<Aquifetp::AQUFETP_data> init_empty(){
+    auto deck = createNullAquifetpDeck();
+    Aquifetp aqufetp(deck);
+    std::vector<Aquifetp::AQUFETP_data> aquifetp = aqufetp.getAquifers();
+
+    return aquifetp;
+}
+
 BOOST_AUTO_TEST_CASE(AquifetpTest){
     std::vector< Aquifetp::AQUFETP_data > aquifetp = init_aquifetp();
+    std::vector< Aquifetp::AQUFETP_data > aquifetp_null = init_empty();
+    BOOST_CHECK_EQUAL(aquifetp_null.size(), 0);
     for (const auto& it : aquifetp){
         BOOST_CHECK_EQUAL(it.aquiferID , 1);
-        BOOST_CHECK_EQUAL(it.V0 , 2.0E9);
-        BOOST_CHECK_EQUAL(it.J, 500/86400e5);
-        
+	BOOST_CHECK_EQUAL(it.V0, 2.0e9);
+	BOOST_CHECK_EQUAL(it.J, 500/86400e5);     
     }    
 }    
