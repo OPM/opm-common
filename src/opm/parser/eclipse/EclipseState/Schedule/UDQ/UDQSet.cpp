c09bc36c03ae0c550b7ef66160fa3cee47a30209
Joakim Hove <joakim.hove@gmail.com>
2019-06-16 08:40:06 +0200

    UDQSet::eval - make sure to return well node

--- src/opm/parser/eclipse/EclipseState/Schedule/UDQ/UDQSet.cpp
+++ src/opm/parser/eclipse/EclipseState/Schedule/UDQ/UDQSet.cpp
@@ -213,7 +213,7 @@ UDQVarType UDQSet::var_type() const {
 
 void UDQSet::operator+=(const UDQSet& rhs) {
     if (this->size() != rhs.size())
-        throw std::invalid_argument("Incompatible size");
+        throw std::invalid_argument("Incompatible size in UDQSet operator+");
 
     for (std::size_t index = 0; index < this->size(); index++)
         this->values[index] += rhs[index];
