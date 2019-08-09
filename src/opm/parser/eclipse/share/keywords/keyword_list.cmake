e013066040046490d6d5a59f4e7cf68642098c58
Steinar Foss <steinar@unicus.no>
2019-08-09 08:32:19 +0200

    added keyword CECONT.

--- src/opm/parser/eclipse/share/keywords/keyword_list.cmake
+++ src/opm/parser/eclipse/share/keywords/keyword_list.cmake
@@ -68,6 +68,7 @@ set( keywords
      000_Eclipse100/C/CART
      000_Eclipse100/C/CBMOPTS
      000_Eclipse100/C/CECON
+     000_Eclipse100/C/CECONT
      000_Eclipse100/C/COMPDAT
      000_Eclipse100/C/COMPDATL
      000_Eclipse100/C/COMPIMB
