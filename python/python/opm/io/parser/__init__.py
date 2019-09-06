ff79a404be1486ec0a5033093aa15935d8184903
Steinar Foss <steinar@unicus.no>
2019-09-06 10:45:33 +0200

    Can extract parserkw's in python.
    added keyword PYINPUT.
    
    added pythonclass ParserKW.
    
    python acces to ParserKW.
    
    removed wrong kw PYINPUT.
    
    ...

--- python/python/opm/io/parser/__init__.py
+++ python/python/opm/io/parser/__init__.py
@@ -1,5 +1,6 @@
 from opm._common import action
 from opm._common import Parser
 from opm._common import ParseContext
+from opm._common import ParserKeyword as ParserKW
 
 from .parser_module import parse, load_deck, load_deck_string, parse_string
