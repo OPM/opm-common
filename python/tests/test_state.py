41a05a7a98de5db7d23015afa8d4efd0ea0bf753
Steinar Foss <steinar@unicus.no>
2019-09-13 15:10:24 +0200

    test_state.py: uncommented test_summary.

--- python/tests/test_state.py
+++ python/tests/test_state.py
@@ -3,13 +3,11 @@ import opm.io
 
 class TestState(unittest.TestCase):
 
-    """
+    
     @classmethod
     def setUpClass(cls):
         cls.spe3 = opm.io.parse('tests/spe3/SPE3CASE1.DATA')
         cpa = opm.io.parse('tests/data/CORNERPOINT_ACTNUM.DATA')
-        cls.state = cls.spe3.state
-        cls.cp_state = cpa.state
     
 
     def test_summary(self):
@@ -18,7 +16,7 @@ class TestState(unittest.TestCase):
         self.assertTrue('WOPR' in smry) # hasKeyword
         self.assertFalse('NONO' in smry) # hasKeyword
     
-    """
+    
 
 
 if __name__ == "__main__":
