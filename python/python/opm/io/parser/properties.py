15d70c446f919c8c76e88655120e24b82e24015c
Steinar Foss <steinar@unicus.no>
2019-09-13 09:56:22 +0200

    properties.py: removed class EclipseState.

--- python/python/opm/io/parser/properties.py
+++ python/python/opm/io/parser/properties.py
@@ -5,18 +5,13 @@ from opm import libopmcommon_python as lib
 from .sunbeam import delegate
 from ..schedule import Schedule
 
-@delegate(lib.EclipseState)
-class EclipseState(object):
-    def __repr__(self):
-        return 'EclipseState(title = "%s")' % self.title
-
 
 @delegate(lib.SunbeamState)
 class SunbeamState(object):
 
     @property
     def state(self):
-        return EclipseState(self._state())
+        return self._state()
 
 
     @property
