a177d3b887e29a6971481d46efa8796d3ed64ab8
Steinar Foss <steinar@unicus.no>
2019-10-11 22:40:35 +0200

    python/cxx/export.hpp: rearranged UnitSystem position.

--- python/cxx/export.cpp
+++ python/cxx/export.cpp
@@ -3,7 +3,6 @@
 
 
 void python::common::export_all(py::module& module) {
-    export_UnitSystem(module);
     export_ParseContext(module);
     export_Parser(module);
     export_Deck(module);
@@ -17,6 +16,7 @@ void python::common::export_all(py::module& module) {
     export_EclipseState(module);
     export_TableManager(module);
     export_EclipseGrid(module);
+    export_UnitSystem(module);
     export_Log(module);
 }
 
