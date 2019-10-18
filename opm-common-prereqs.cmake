16e5a8ddd0b726470e6a5bce3f618449e97c68cb
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2019-10-18 08:45:49 +0200

    drop conditions ENABLE_ECL_INPUT / ENABLE_ECL_OUTPUT

--- opm-common-prereqs.cmake
+++ opm-common-prereqs.cmake
@@ -13,17 +13,10 @@ set (opm-common_DEPS
 	"CXX11Features REQUIRED"
 	)
 
-if(ENABLE_ECL_INPUT)
-  list(APPEND opm-common_DEPS
-        # various runtime library enhancements
-        "Boost 1.44.0
-          COMPONENTS system filesystem unit_test_framework regex REQUIRED")
-else()
-  list(APPEND opm-common_DEPS
-        # various runtime library enhancements
-        "Boost 1.44.0
-          COMPONENTS system unit_test_framework REQUIRED")
-endif()
+list(APPEND opm-common_DEPS
+      # various runtime library enhancements
+      "Boost 1.44.0
+        COMPONENTS system filesystem unit_test_framework regex REQUIRED")
 # We need a defined target for libecl when linking.
 # The definition of the target is done implicitly below when
 # libecl is searched for.
