be64447cee3d69d55decd45101cc96387804781d
Joakim Hove <joakim.hove@gmail.com>
2019-12-10 16:54:07 +0100

    Add small struct to report memory usage from FieldPropsManager

--- src/opm/parser/eclipse/EclipseState/Grid/FieldProps.hpp
+++ src/opm/parser/eclipse/EclipseState/Grid/FieldProps.hpp
@@ -207,6 +207,16 @@ public:
         return def;
     }
 
+    std::size_t active_size;
+    std::size_t global_size;
+
+    std::size_t num_int() const {
+        return this->int_data.size();
+    }
+
+    std::size_t num_double() const {
+        return this->double_data.size();
+    }
 
 private:
     void scanGRIDSection(const GRIDSection& grid_section);
@@ -235,8 +245,6 @@ private:
     void init_satfunc(const std::string& keyword, FieldData<double>& satfunc);
 
     const UnitSystem unit_system;
-    std::size_t active_size;
-    std::size_t global_size;
     std::size_t nx,ny,nz;
     std::vector<int> m_actnum;
     std::vector<double> cell_volume;
