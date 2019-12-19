ce5b9b1e1d237f2c80fcba983b73d740d70ade9b
Joakim Hove <joakim.hove@gmail.com>
2019-12-19 10:55:57 +0100

    Add documentation to the FieldPropsManager

--- opm/parser/eclipse/EclipseState/Grid/FieldPropsManager.hpp
+++ opm/parser/eclipse/EclipseState/Grid/FieldPropsManager.hpp
@@ -111,27 +111,104 @@ public:
      */
 
 
+    /*
+      Return the keyword values as a std::vector<>. All elements in the return
+      value are guaranteed to be assigned a valid value. If the keyword is not
+      in the container, or not all elements have a valid value - an exception
+      will be raised:
+
+       - keyword which is not supported at all -> std::logic_error
+       - keyword which is not in the deck at all -> std::out_of_range
+       - keyword which has not been fully initialized -> std::runtime_error
+
+      Many of the keywords in the container can be automatically created, in
+      that case the get() method will silently create a new keyword and default
+      initialize if it is not already in the container. The different exceptions
+      raised for the different error conditions are the same for get(),
+      get_copy() and get_global().
+    */
     template <typename T>
     const std::vector<T>& get(const std::string& keyword) const;
+
+
+    /*
+      The get_copy() has exactly the same behaviour as get(), but the important
+      difference is that said keyword is not already in the container it is not
+      installed in the container; if we look at SATNUM which is a keywor which
+      can be automatically instantiated we have the following behavior:
+
+      get():
+          fp.has<int>("SATNUM") -> false
+          const std::vector<int>& satnum = fp.get<int>("SATNUM")
+          fp.has<int>("SATNUM") -> true;
+
+
+      get_copy():
+          fp.has<int>("SATNUM") -> false
+          const std::vector<int>& satnum = fp.get_copy<int>("SATNUM")
+          fp.has<int>("SATNUM") -> false
+    */
+
 
     template <typename T>
     std::vector<T> get_copy(const std::string& keyword, bool global=false) const;
 
-    template <typename T>
-    const std::vector<T>* try_get(const std::string& keyword) const;
-
+    /*
+      This is exactly like the get() method, but the returned vector will have
+      global cartesian size, where all inactive cells have been filled with
+      zeros.
+    */
     template <typename T>
     std::vector<T> get_global(const std::string& keyword) const;
 
-    template <typename T>
-    std::vector<bool> defaulted(const std::string& keyword) const;
+    /*
+      Will return a pointer to the keyword data, or nullptr if the container
+      does not have suce a keyword. Observe that container will hold on to an
+      manage the underlying keyword data.
 
-    template <typename T>
-    static bool supported(const std::string& keyword);
+      The try_get function will return a nullptr if the container does not
+      contain said keyword, or if the keyword has not been fully initialized. If
+      you ask for a totally unknown keyword the method will return nullptr.
+    */
+    template <typename T> const std::vector<T>* try_get(const
+    std::string& keyword) const;
 
+    /*
+      Will check if the container has the keyword loaded; in a fully initialized
+      state. If you ask for a keyword which is not supported at all you will
+      just get false back.
+    */
     template <typename T>
     bool has(const std::string& keyword) const;
 
+    /*
+      You can ask whether the elements in the keyword have a default value -
+      which typically is calculated in some way, or if it has been explicitly
+      assigned to in the deck.
+    */
+    template <typename T>
+    std::vector<bool> defaulted(const std::string& keyword) const;
+
+
+    /*
+      Check whether the container supports/recognizes a keyword at all:
+
+        supported<double>("PORO")            => true
+        supported<double>("NO_SUCH_KEYWORD") => false
+
+      The method does not at all consult the content of the container - it is a
+      static method.
+    */
+    template <typename T>
+    static bool supported(const std::string& keyword);
+
+    /*
+      The keys() function will return a list of keys corresponding to the fully
+      initialized keywords in the container. Observe that the implementation
+      special cases the PORV and ACTNUM keywords, since these are present with
+      special functions porv(bool) and actnum() the "PORV" and "ACTNUM" string
+      literals are excluded from the keys() list.
+    */
     template <typename T>
     std::vector<std::string> keys() const;
 
