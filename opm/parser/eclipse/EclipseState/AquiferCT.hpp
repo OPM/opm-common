f666ab05443ed52bbf51b50efbaf99d0079723fe
Joakim Hove <joakim.hove@gmail.com>
2020-02-05 19:43:21 +0100

    Refactored the AquiferCT class to construct AQUCT_data struct

--- opm/parser/eclipse/EclipseState/AquiferCT.hpp
+++ opm/parser/eclipse/EclipseState/AquiferCT.hpp
@@ -44,55 +44,35 @@ namespace Opm {
     class AquiferCT {
         public:
 
-            struct AQUCT_data{
+        struct AQUCT_data{
 
-                    // Aquifer ID
-                    int aquiferID;
-                    // Table IDs
-                    int inftableID, pvttableID;
-                    std::vector<int> cell_id;
-                    // Variables constants
-                    double  phi_aq , //aquifer porosity
-                            d0,   //aquifer datum depth
-                            C_t , //total compressibility
-                            r_o , //aquifer inner radius
-                            k_a , //aquifer permeability
-                            c1, // 0.008527 (METRIC, PVT-M); 0.006328 (FIELD); 3.6 (LAB)
-                            h , //aquifer thickness
-                            theta , //angle subtended by the aquifer boundary
-                            c2 ; //6.283 (METRIC, PVT-M); 1.1191 (FIELD); 6.283 (LAB).
+            AQUCT_data(const DeckRecord& record, const TableManager& tables);
 
-                    std::shared_ptr<double> p0; //Initial aquifer pressure at datum depth, d0
-                    std::vector<double> td, pi;
-            };
+            int aquiferID;
+            int inftableID, pvttableID;
 
-            AquiferCT(const TableManager& tables, const Deck& deck);
+            double  phi_aq , // aquifer porosity
+                    d0,      // aquifer datum depth
+                    C_t ,    // total compressibility
+                    r_o ,    // aquifer inner radius
+                    k_a ,    // aquifer permeability
+                    c1,      // 0.008527 (METRIC, PVT-M); 0.006328 (FIELD); 3.6 (LAB)
+                    h ,      // aquifer thickness
+                    theta ,  // angle subtended by the aquifer boundary
+                    c2 ;     // 6.283 (METRIC, PVT-M); 1.1191 (FIELD); 6.283 (LAB).
 
-            std::size_t size() const;
-            std::vector<AquiferCT::AQUCT_data>::const_iterator begin() const;
-            std::vector<AquiferCT::AQUCT_data>::const_iterator end() const;
-        private:
+            std::shared_ptr<double> p0; //Initial aquifer pressure at datum depth, d0
+            std::vector<double> td, pi;
+            std::vector<int> cell_id;
+        };
 
-            std::vector<AquiferCT::AQUCT_data> m_aquct;
+        AquiferCT(const TableManager& tables, const Deck& deck);
 
-            //Set the default Pd v/s Td tables (constant terminal rate case for an infinite aquifer) as described in
-            //Van Everdingen, A. & Hurst, W., December, 1949.The Application of the Laplace Transformation to Flow Problems in Reservoirs.
-            //Petroleum Transactions, AIME.
-            inline void set_default_tables(std::vector<double>& td, std::vector<double>& pi)
-            {
-                std::vector<double> default_pressure_ = { 0.112, 0.229, 0.315, 0.376, 0.424, 0.469, 0.503, 0.564, 0.616, 0.659, 0.702, 0.735,
-                                                          0.772, 0.802, 0.927, 1.02, 1.101, 1.169, 1.275, 1.362, 1.436, 1.5, 1.556, 1.604,
-                                                          1.651, 1.829, 1.96, 2.067, 2.147, 2.282, 2.388, 2.476, 2.55, 2.615, 2.672, 2.723,
-                                                          2.921, 3.064, 3.173, 3.263, 3.406, 3.516, 3.608, 3.684, 3.75, 3.809, 3.86 };
-
-                std::vector<double> default_time_ = { 0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1,
-                                                        1.5, 2, 2.5, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 40, 50, 60, 70,
-                                                        80, 90, 100, 150, 200, 250, 300, 400, 500, 600, 700, 800, 900, 1000 };
-
-                td = default_time_;
-                pi = default_pressure_;
-            }
-
+        std::size_t size() const;
+        std::vector<AquiferCT::AQUCT_data>::const_iterator begin() const;
+        std::vector<AquiferCT::AQUCT_data>::const_iterator end() const;
+    private:
+        std::vector<AquiferCT::AQUCT_data> m_aquct;
     };
 }
 
--- src/opm/parser/eclipse/EclipseState/AquiferCT.cpp
+++ src/opm/parser/eclipse/EclipseState/AquiferCT.cpp
@@ -22,60 +22,84 @@
 
 namespace Opm {
 
-    AquiferCT::AquiferCT(const TableManager& tables, const Deck& deck)
-    {
-        if (!deck.hasKeyword("AQUCT"))
-            return;
+namespace {
 
-        const auto& aquctKeyword = deck.getKeyword("AQUCT");
-        for (auto& aquctRecord : aquctKeyword){
+// The default Pd v/s Td tables (constant terminal rate case for an infinite
+// aquifer) as described in Van Everdingen, A. & Hurst, W., December, 1949.The
+// Application of the Laplace Transformation to Flow Problems in Reservoirs.
+// Petroleum Transactions, AIME.
 
-            AquiferCT::AQUCT_data data;
+static const std::vector<double> default_pressure = { 0.112, 0.229, 0.315, 0.376, 0.424, 0.469, 0.503, 0.564,
+                                                      0.616, 0.659, 0.702, 0.735, 0.772, 0.802, 0.927, 1.020,
+                                                      1.101, 1.169, 1.275, 1.362, 1.436, 1.500, 1.556, 1.604,
+                                                      1.651, 1.829, 1.960, 2.067, 2.147, 2.282, 2.388, 2.476,
+                                                      2.550, 2.615, 2.672, 2.723, 2.921, 3.064, 3.173, 3.263,
+                                                      3.406, 3.516, 3.608, 3.684, 3.750, 3.809, 3.86 };
 
-            data.c1 = 1.0;
-            data.c2 = 6.283;        // Value of C2 used by E100 (for METRIC, PVT-M and LAB unit systems)
-            data.aquiferID = aquctRecord.getItem("AQUIFER_ID").template get<int>(0);
-            data.h = aquctRecord.getItem("THICKNESS_AQ").getSIDouble(0);
-            data.phi_aq = aquctRecord.getItem("PORO_AQ").getSIDouble(0);
-            data.d0 = aquctRecord.getItem("DAT_DEPTH").getSIDouble(0);
-            data.C_t = aquctRecord.getItem("C_T").getSIDouble(0);
-            data.r_o = aquctRecord.getItem("RAD").getSIDouble(0);
-            data.k_a = aquctRecord.getItem("PERM_AQ").getSIDouble(0);
-            data.theta = aquctRecord.getItem("INFLUENCE_ANGLE").getSIDouble(0)/360.0;
-            data.inftableID = aquctRecord.getItem("TABLE_NUM_INFLUENCE_FN").template get<int>(0);
-            data.pvttableID = aquctRecord.getItem("TABLE_NUM_WATER_PRESS").template get<int>(0);
+static const std::vector<double> default_time =     { 0.010, 0.050, 0.100, 0.150, 0.200, 0.250, 0.300, 0.400,
+                                                      0.500, 0.600, 0.700, 0.800, 0.900, 1.000, 1.500, 2.000,
+                                                      2.500, 3.000, 4.000, 5.000, 6.000, 7.000, 8.000, 9.000,
+                                                      10.00, 15.00, 20.00, 25.00, 30.00, 40.00, 50.00, 60.00,
+                                                      70.00, 80.00, 90.00, 100.0, 150.0, 200.0, 250.0, 300.0,
+                                                      400.0, 500.0, 600.0, 700.0, 800.0, 900.0, 1000 };
 
-            if (aquctRecord.getItem("P_INI").hasValue(0)) {
-                double * raw_ptr = new double( aquctRecord.getItem("P_INI").getSIDouble(0) );
-                data.p0.reset( raw_ptr );
-            }
+}
 
-            // Get the correct influence table values
-            if (data.inftableID > 1){
-                const auto& aqutabTable = tables.getAqutabTables().getTable(data.inftableID - 2);
-                const auto& aqutab_tdColumn = aqutabTable.getColumn(0);
-                const auto& aqutab_piColumn = aqutabTable.getColumn(1);
-                data.td = aqutab_tdColumn.vectorCopy();
-                data.pi = aqutab_piColumn.vectorCopy();
-                }
-            else
-                {
-                    set_default_tables(data.td,data.pi);
-                }
-                m_aquct.push_back( std::move(data) );
-            }
+
+AquiferCT::AQUCT_data::AQUCT_data(const DeckRecord& record, const TableManager& tables) :
+    aquiferID(record.getItem("AQUIFER_ID").get<int>(0)),
+    inftableID(record.getItem("TABLE_NUM_INFLUENCE_FN").get<int>(0)),
+    pvttableID(record.getItem("TABLE_NUM_WATER_PRESS").get<int>(0)),
+    phi_aq(record.getItem("PORO_AQ").getSIDouble(0)),
+    d0(record.getItem("DAT_DEPTH").getSIDouble(0)),
+    C_t(record.getItem("C_T").getSIDouble(0)),
+    r_o(record.getItem("RAD").getSIDouble(0)),
+    k_a(record.getItem("PERM_AQ").getSIDouble(0)),
+    c1(1.0),
+    h(record.getItem("THICKNESS_AQ").getSIDouble(0)),
+    theta( record.getItem("INFLUENCE_ANGLE").getSIDouble(0)/360.0),
+    c2(6.283)        // Value of C2 used by E100 (for METRIC, PVT-M and LAB unit systems)
+{
+    if (record.getItem("P_INI").hasValue(0)) {
+        double * raw_ptr = new double( record.getItem("P_INI").getSIDouble(0) );
+        this->p0.reset( raw_ptr );
     }
 
-
-    std::size_t AquiferCT::size() const {
-        return this->m_aquct.size();
+    // Get the correct influence table values
+    if (this->inftableID > 1) {
+        const auto& aqutabTable = tables.getAqutabTables().getTable(this->inftableID - 2);
+        const auto& aqutab_tdColumn = aqutabTable.getColumn(0);
+        const auto& aqutab_piColumn = aqutabTable.getColumn(1);
+        this->td = aqutab_tdColumn.vectorCopy();
+        this->pi = aqutab_piColumn.vectorCopy();
+    } else {
+        this->td = default_time;
+        this->pi = default_pressure;
     }
+}
 
-    std::vector<AquiferCT::AQUCT_data>::const_iterator AquiferCT::begin() const {
-        return this->m_aquct.begin();
-    }
 
-    std::vector<AquiferCT::AQUCT_data>::const_iterator AquiferCT::end() const {
-        return this->m_aquct.end();
-    }
+AquiferCT::AquiferCT(const TableManager& tables, const Deck& deck)
+{
+    using AQUCT = ParserKeywords::AQUCT;
+    if (!deck.hasKeyword<AQUCT>())
+        return;
+
+    const auto& aquctKeyword = deck.getKeyword<AQUCT>();
+    for (auto& record : aquctKeyword)
+        this->m_aquct.emplace_back(record, tables);
+}
+
+
+std::size_t AquiferCT::size() const {
+    return this->m_aquct.size();
+}
+
+std::vector<AquiferCT::AQUCT_data>::const_iterator AquiferCT::begin() const {
+    return this->m_aquct.begin();
+}
+
+std::vector<AquiferCT::AQUCT_data>::const_iterator AquiferCT::end() const {
+    return this->m_aquct.end();
+}
 }
