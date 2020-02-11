0525ea52a9478d34ec4250ffcffb130367a9f473
Joakim Hove <joakim.hove@gmail.com>
2020-02-11 12:51:11 +0100

    White space reformatting

--- opm/parser/eclipse/EclipseState/Schedule/MSW/Segment.hpp
+++ opm/parser/eclipse/EclipseState/Schedule/MSW/Segment.hpp
@@ -45,11 +45,19 @@ namespace Opm {
         Segment(int segment_number_in, int branch_in, int outlet_segment_in, double length_in, double depth_in,
                 double internal_diameter_in, double roughness_in, double cross_area_in, double volume_in, bool data_ready_in, SegmentType segment_type_in);
 
-        Segment(int segmentNumber, int branchNumber, int outlegSegment,
+
+        Segment(int segmentNumber,
+                int branchNumber,
+                int outlegSegment,
                 const std::vector<int>& inletSegments,
-                double totalLength, double depth, double internalDiameter,
-                double roughness, double crossArea, double volume,
-                bool dataReady, SegmentType segmentType,
+                double totalLength,
+                double depth,
+                double internalDiameter,
+                double roughness,
+                double crossArea,
+                double volume,
+                bool dataReady,
+                SegmentType segmentType,
                 std::shared_ptr<SpiralICD> spiralICD,
                 std::shared_ptr<Valve> valv);
 
