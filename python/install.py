9eec147616d639ba9a58efad6821029f2970ac5b
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2020-02-26 11:52:00 +0100

    fixed: respect DESTDIR in python binding installation script

--- python/install.py
+++ python/install.py
@@ -7,6 +7,9 @@ import filecmp
 src_root = sys.argv[1]
 target_prefix = sys.argv[2]
 install = int(sys.argv[3])
+target_destdir = os.environ.get("DESTDIR", "")
+if not target_destdir is "":
+    target_prefix = target_destdir + target_prefix
 
 if not os.path.isdir(src_root):
     sys.exit("No such directory: {}".format(src_root))
