7b446b4d8925ab95064f026f3e4fad3da8e68f7d
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2020-03-17 10:42:22 +0100

    add serialization template to TableManager

--- opm/parser/eclipse/EclipseState/EclipseState.hpp
+++ opm/parser/eclipse/EclipseState/EclipseState.hpp
@@ -118,7 +118,7 @@ namespace Opm {
         void serializeOp(Serializer& serializer)
         {
             // FieldPropsManager is handled otherwise, do not add
-            serializer(m_tables);
+            m_tables.serializeOp(serializer);
             m_runspec.serializeOp(serializer);
             m_eclipseConfig.serializeOp(serializer);
             serializer(m_deckUnitSystem);
