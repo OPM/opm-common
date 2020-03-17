d7b6b282ca76c6e974d4a0dbd24dc5805b96e35c
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2020-03-17 10:42:22 +0100

    add serialization template to Rock2dtrTable

--- opm/parser/eclipse/EclipseState/Tables/Rock2dtrTable.hpp
+++ opm/parser/eclipse/EclipseState/Tables/Rock2dtrTable.hpp
@@ -44,6 +44,13 @@ namespace Opm {
 
         bool operator==(const Rock2dtrTable& data) const;
 
+        template<class Serializer>
+        void serializeOp(Serializer& serializer)
+        {
+            serializer(m_transMultValues);
+            serializer(m_pressureValues);
+        }
+
     protected:
         std::vector< std::vector <double> > m_transMultValues;
         std::vector< double > m_pressureValues;
