524b1801023c43a18d26a25c688590b5161257d7
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2020-03-18 15:46:28 +0100

    update UDQConfig serialization
    to avoid the need for OPM defined types in underlying
    serializer

--- opm/parser/eclipse/EclipseState/Schedule/UDQ/UDQConfig.hpp
+++ opm/parser/eclipse/EclipseState/Schedule/UDQ/UDQConfig.hpp
@@ -87,7 +87,7 @@ namespace Opm {
             serializer.map(m_assignments);
             serializer(units);
             input_index.serializeOp(serializer);
-            serializer(type_count);
+            serializer.template map<decltype(type_count),false>(type_count);
             // The UDQFunction table is constant up to udq_params.
             // So we can just construct a new instance here.
             if (!serializer.isSerializing())
