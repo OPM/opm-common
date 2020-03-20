ab71b43f103698a9a83894e238eab8c92703f0e6
Williham Williham Totland <williham.totland@gmail.com>
2020-03-20 14:48:32 +0100

    Fixes minor formatting glitch in the SummaryNode copyright statement.

--- src/opm/io/eclipse/SummaryNode.cpp
+++ src/opm/io/eclipse/SummaryNode.cpp
@@ -3,7 +3,8 @@
 
    This file is part of the Open Porous Media project (OPM).
 
-   OPM is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+   OPM is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
 
