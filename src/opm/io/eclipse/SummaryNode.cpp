4e08ff7f6d5941cc7b2f80ee34f8f9a34cd28dbe
Williham Williham Totland <williham.totland@gmail.com>
2020-03-26 10:53:09 +0100

    Adds ::Aquifer to Opm::EclIO::SummaryNode::Category.

--- src/opm/io/eclipse/SummaryNode.cpp
+++ src/opm/io/eclipse/SummaryNode.cpp
@@ -29,6 +29,7 @@ namespace {
 
 constexpr bool use_number(Opm::EclIO::SummaryNode::Category category) {
     switch (category) {
+    case Opm::EclIO::SummaryNode::Category::Aquifer:       [[fallthrough]];
     case Opm::EclIO::SummaryNode::Category::Block:         [[fallthrough]];
     case Opm::EclIO::SummaryNode::Category::Connection:    [[fallthrough]];
     case Opm::EclIO::SummaryNode::Category::Region:        [[fallthrough]];
@@ -49,6 +50,7 @@ constexpr bool use_name(Opm::EclIO::SummaryNode::Category category) {
     case Opm::EclIO::SummaryNode::Category::Segment:       [[fallthrough]];
     case Opm::EclIO::SummaryNode::Category::Well:
         return true;
+    case Opm::EclIO::SummaryNode::Category::Aquifer:       [[fallthrough]];
     case Opm::EclIO::SummaryNode::Category::Block:         [[fallthrough]];
     case Opm::EclIO::SummaryNode::Category::Field:         [[fallthrough]];
     case Opm::EclIO::SummaryNode::Category::Miscellaneous: [[fallthrough]];
