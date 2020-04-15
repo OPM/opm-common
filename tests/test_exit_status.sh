133ff18ee373bf8cca050b7fb90c8d954384886b
Joakim Hove <joakim.hove@gmail.com>
2020-04-15 20:25:02 +0200

    Make sure all generated files are in $work_dir

--- tests/test_exit_status.sh
+++ tests/test_exit_status.sh
@@ -5,11 +5,11 @@ SIMULATOR="$2"
 EXIT_STATUS="$3"
 
 work_dir=$(mktemp -d)
-cp $INPUT_CASE ${work_dir}
-pushd $work_dir
+cp "${INPUT_CASE}" ${work_dir}
+pushd ${work_dir}
 
 ecode=0
-${SIMULATOR} ${INPUT_CASE}
+${SIMULATOR} $(basename "${INPUT_CASE}")
 
 sim_status=$?
 if [ ${sim_status} -ne ${EXIT_STATUS} ]
