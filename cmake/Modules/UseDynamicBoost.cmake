d3b4f41acf79e1fd792ab7b307c6b92b57b1b066
Gaute Lindkvist <lindkvis@gmail.com>
2020-06-10 14:19:43 +0200

    Support building Python module statically

--- cmake/Modules/UseDynamicBoost.cmake
+++ cmake/Modules/UseDynamicBoost.cmake
@@ -1,3 +1,7 @@
+
+if(NOT OPM_ENABLE_DYNAMIC_BOOST)
+  set(Boost_USE_STATIC_LIBS ON)
+endif()
 find_package (Boost 1.44.0 COMPONENTS unit_test_framework QUIET)
 
 if (Boost_UNIT_TEST_FRAMEWORK_FOUND)
@@ -9,7 +13,9 @@ if (Boost_UNIT_TEST_FRAMEWORK_FOUND)
   list (APPEND CMAKE_REQUIRED_LIBRARIES ${Boost_LIBRARIES})
 
   unset(HAVE_DYNAMIC_BOOST_TEST CACHE)
-  check_cxx_source_compiles("
+  unset(HAVE_STATIC_BOOST_TEST CACHE)
+  if (OPM_ENABLE_DYNAMIC_BOOST)
+    check_cxx_source_compiles("
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE DYNLINK_TEST
 #include <boost/test/unit_test.hpp>
@@ -21,6 +27,19 @@ BOOST_AUTO_TEST_CASE(DynlinkConfigureTest) {
                       \"Apparently, multiplication doesn't \"
                       \"work: f(2) = \" << f(2));
 }" HAVE_DYNAMIC_BOOST_TEST)
+  else() # Static linking
+    check_cxx_source_compiles("
+#define BOOST_TEST_MODULE STATICLINK_TEST
+#include <boost/test/unit_test.hpp>
+
+int f(int x) { return 2 * x; }
+
+BOOST_AUTO_TEST_CASE(StaticlinkConfigureTest) {
+  BOOST_CHECK_MESSAGE(f(2) == 4,
+                      \"Apparently, multiplication doesn't \"
+                      \"work: f(2) = \" << f(2));
+}" HAVE_STATIC_BOOST_TEST)
+  endif()
   cmake_pop_check_state ()
 else (Boost_UNIT_TEST_FRAMEWORK_FOUND)
   # no Boost, no compile
@@ -31,6 +50,10 @@ endif (Boost_UNIT_TEST_FRAMEWORK_FOUND)
 set (HAVE_DYNAMIC_BOOST_TEST "${HAVE_DYNAMIC_BOOST_TEST}"
   CACHE BOOL "Whether Boost::Test is dynamically linked or not"
   )
+set (HAVE_DYNAMIC_BOOST_TEST "${HAVE_STATIC_BOOST_TEST}"
+  CACHE BOOL "Whether Boost::Test is statically linked or not"
+  )
 
 # include in config.h
 list (APPEND TESTING_CONFIG_VARS "HAVE_DYNAMIC_BOOST_TEST")
+list (APPEND TESTING_CONFIG_VARS "HAVE_STATIC_BOOST_TEST")
