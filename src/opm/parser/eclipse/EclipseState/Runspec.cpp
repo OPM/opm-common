0c6d1c31d7c391baa6b07fea5c71392f2baa9358
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2020-06-10 14:55:20 +0200

    Implement and use Runspec::nupcol().

--- src/opm/parser/eclipse/EclipseState/Runspec.cpp
+++ src/opm/parser/eclipse/EclipseState/Runspec.cpp
@@ -20,10 +20,13 @@
 #include <type_traits>
 
 #include <opm/parser/eclipse/Deck/Deck.hpp>
+#include <opm/parser/eclipse/Deck/DeckSection.hpp>
+#include <opm/parser/eclipse/Parser/ParserKeywords/N.hpp>
 #include <opm/parser/eclipse/Parser/ParserKeywords/S.hpp>
 #include <opm/parser/eclipse/Parser/ParserKeywords/T.hpp>
 #include <opm/parser/eclipse/Parser/ParserKeywords/W.hpp>
 #include <opm/parser/eclipse/EclipseState/Runspec.hpp>
+#include <opm/common/OpmLog/OpmLog.hpp>
 
 namespace Opm {
 
@@ -311,8 +314,22 @@ Runspec::Runspec( const Deck& deck ) :
     udq_params( deck ),
     hystpar( deck ),
     m_actdims( deck ),
-    m_sfuncctrl( deck )
-{}
+    m_sfuncctrl( deck ),
+    m_nupcol( ParserKeywords::NUPCOL::NUM_ITER::defaultValue )
+{
+    if (DeckSection::hasRUNSPEC(deck)) {
+        const RUNSPECSection runspecSection{deck};
+        if (runspecSection.hasKeyword("NUPCOL")) {
+            using NC = ParserKeywords::NUPCOL;
+            const auto& item = runspecSection.getKeyword<NC>().getRecord(0).getItem<NC::NUM_ITER>();
+            m_nupcol = item.get<int>(0);
+            if (item.defaultApplied(0)) {
+                std::string msg = "OPM Flow uses 12 as default NUPCOL value";
+                OpmLog::note(msg);
+            }
+        }
+    }
+}
 
 Runspec Runspec::serializeObject()
 {
@@ -364,6 +381,11 @@ const EclHysterConfig& Runspec::hysterPar() const noexcept
 const SatFuncControls& Runspec::saturationFunctionControls() const noexcept
 {
     return this->m_sfuncctrl;
+}
+
+int Runspec::nupcol() const noexcept
+{
+    return this->m_nupcol;
 }
 
 /*
