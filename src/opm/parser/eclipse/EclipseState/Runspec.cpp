08df29c991f9a3cbe7c4e64e6a3ab176fbdc7be8
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2020-09-16 14:58:56 +0200

    fixed: serialization nupcol in Runspec
    also add it to comparison operator

--- src/opm/parser/eclipse/EclipseState/Runspec.cpp
+++ src/opm/parser/eclipse/EclipseState/Runspec.cpp
@@ -343,6 +343,7 @@ Runspec Runspec::serializeObject()
     result.hystpar = EclHysterConfig::serializeObject();
     result.m_actdims = Actdims::serializeObject();
     result.m_sfuncctrl = SatFuncControls::serializeObject();
+    result.m_nupcol = 2;
 
     return result;
 }
@@ -415,7 +416,8 @@ bool Runspec::operator==(const Runspec& data) const {
            this->wellSegmentDimensions() == data.wellSegmentDimensions() &&
            this->hysterPar() == data.hysterPar() &&
            this->actdims() == data.actdims() &&
-           this->saturationFunctionControls() == data.saturationFunctionControls();
+           this->saturationFunctionControls() == data.saturationFunctionControls() &&
+           this->m_nupcol == data.m_nupcol;
 }
 
 }
