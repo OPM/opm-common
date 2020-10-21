413a16157cf5dcb1acbb7436df290f45c0180248
Joakim Hove <joakim.hove@gmail.com>
2020-10-21 13:49:07 +0200

    Make sure UDQ define statements are evaluated in definition order

--- src/opm/parser/eclipse/EclipseState/Schedule/UDQ/UDQConfig.cpp
+++ src/opm/parser/eclipse/EclipseState/Schedule/UDQ/UDQConfig.cpp
@@ -154,20 +154,26 @@ namespace Opm {
     std::vector<UDQDefine> UDQConfig::definitions() const {
         std::vector<UDQDefine> ret;
 
-        for (const auto& key : this->define_order) {
-            if (this->action_type(key) == UDQAction::DEFINE)
-                ret.push_back(this->m_definitions.at(key));
+        for (const auto& index_pair : this->input_index) {
+            if (index_pair.second.action == UDQAction::DEFINE) {
+                const std::string& key = index_pair.first;
+                ret.push_back( this->m_definitions.at(key) );
+            }
         }
+
         return ret;
     }
 
 
     std::vector<UDQDefine> UDQConfig::definitions(UDQVarType var_type) const {
         std::vector<UDQDefine> filtered_defines;
-        for (const auto& key : this->define_order) {
-            const auto& udq_define = this->m_definitions.at(key);
-            if (udq_define.var_type() == var_type && this->action_type(key) == UDQAction::DEFINE)
-                filtered_defines.push_back(udq_define);
+        for (const auto& index_pair : this->input_index) {
+            if (index_pair.second.action == UDQAction::DEFINE) {
+                const std::string& key = index_pair.first;
+                const auto& udq_define = this->m_definitions.at(key);
+                if (udq_define.var_type() == var_type)
+                    filtered_defines.push_back(udq_define);
+            }
         }
         return filtered_defines;
     }
