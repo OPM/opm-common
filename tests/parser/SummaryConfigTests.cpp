ed5ed62203faf9d06f03b63ed8407719b70ae6f4
Joakim Hove <joakim.hove@gmail.com>
2020-11-16 07:12:59 +0100

    Parse summary keyword ROEW_xxx

--- tests/parser/SummaryConfigTests.cpp
+++ tests/parser/SummaryConfigTests.cpp
@@ -1181,12 +1181,16 @@ ROPT_REG
 
 RRPV_REG
 /
+
+ROEW_REG
+/
 )";
     const auto& summary_config = createSummary(deck_string);
-    BOOST_CHECK_EQUAL(summary_config.size(), 9U);
+    BOOST_CHECK_EQUAL(summary_config.size(), 12U);
     BOOST_CHECK(summary_config.hasKeyword("RPR__REG"));
     BOOST_CHECK(summary_config.hasKeyword("ROPT_REG"));
     BOOST_CHECK(summary_config.hasKeyword("RRPV_REG"));
+    BOOST_CHECK(summary_config.hasKeyword("ROEW_REG"));
     BOOST_CHECK(!summary_config.hasKeyword("RPR"));
     BOOST_CHECK(!summary_config.match("BPR*"));
     BOOST_CHECK(summary_config.match("RPR*"));
