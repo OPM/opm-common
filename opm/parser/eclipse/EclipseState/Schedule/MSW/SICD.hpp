6971705c30876b7a2d5ce46689558921758978fc
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2020-12-16 08:33:57 +0100

    fix indentation

--- opm/parser/eclipse/EclipseState/Schedule/MSW/SICD.hpp
+++ opm/parser/eclipse/EclipseState/Schedule/MSW/SICD.hpp
@@ -38,16 +38,16 @@ namespace Opm {
         SICD();
         explicit SICD(const DeckRecord& record);
         SICD(double strength,
-                  double length,
-                  double densityCalibration,
-                  double viscosityCalibration,
-                  double criticalValue,
-                  double widthTransitionRegion,
-                  double maxViscosityRatio,
-                  int methodFlowScaling,
-                  double maxAbsoluteRate,
-                  ICDStatus status,
-                  double scalingFactor);
+             double length,
+             double densityCalibration,
+             double viscosityCalibration,
+             double criticalValue,
+             double widthTransitionRegion,
+             double maxViscosityRatio,
+             int methodFlowScaling,
+             double maxAbsoluteRate,
+             ICDStatus status,
+             double scalingFactor);
 
         static SICD serializeObject();
 
