adc52eb8700e4b53076324a8fc7eba5dcbf2eeb9
Joakim Hove <joakim.hove@gmail.com>
2021-04-09 10:34:16 +0200

    Refactor IMPORT handler to accept all data keywords

--- src/opm/parser/eclipse/Deck/ImportContainer.cpp
+++ src/opm/parser/eclipse/Deck/ImportContainer.cpp
@@ -27,17 +27,24 @@
 namespace Opm {
 
 ImportContainer::ImportContainer(const Parser& parser, const UnitSystem& unit_system, const std::string& fname, bool formatted, std::size_t deck_size) {
-    const std::unordered_set<std::string> float_kw = {"COORD", "MULTPV", "NTG", "PERMX", "PERMY", "PERMZ", "PORO", "ZCORN"};
-    const std::unordered_set<std::string> int_kw = {"ACTNUM", "FIPNUM" , "MULTNUM", "FLUXNUM", "OPERNUM"};
-
     EclIO::EclFile ecl_file(fname, EclIO::EclFile::Formatted{formatted});
     const auto& header = ecl_file.getList();
     for (std::size_t kw_index = 0; kw_index < header.size(); kw_index++) {
         const auto& [name, data_type, _] = header[kw_index];
         (void)_;
+        if (!parser.isRecognizedKeyword(name)) {
+            OpmLog::info(fmt::format("{:<5} Skipping {} from IMPORT file {}", "", name, fname));
+            continue;
+        }
 
-        if (float_kw.count(name)) {
-            const auto& parser_kw = parser.getKeyword(name);
+        const auto& parser_kw = parser.getKeyword(name);
+        if (!parser_kw.isDataKeyword()) {
+            OpmLog::info(fmt::format("{:<5} Skipping {} from IMPORT file {}", "", name, fname));
+            continue;
+        }
+
+        const auto& parser_item = parser_kw.getRecord(0).get(0);
+        if (parser_item.dataType() == type_tag::fdouble) {
             if (data_type == EclIO::REAL) {
                 auto& float_data = ecl_file.get<float>(kw_index);
                 std::vector<double> double_data;
@@ -47,22 +54,15 @@ ImportContainer::ImportContainer(const Parser& parser, const UnitSystem& unit_sy
                 auto& double_data = ecl_file.get<double>(kw_index);
                 this->keywords.emplace_back(parser_kw, double_data, unit_system, unit_system);
             }
-            deck_size += 1;
-            auto msg = fmt::format("{:5} Loading {:<8} from IMPORT file {}", deck_size, name, fname);
-            OpmLog::info(msg);
-            continue;
-        }
-
-        if (int_kw.count(name)) {
-            const auto& parser_kw = parser.getKeyword(name);
+        } else if (parser_item.dataType() == type_tag::integer) {
             const auto& data = ecl_file.get<int>(kw_index);
             this->keywords.emplace_back(parser_kw, data);
-            deck_size += 1;
-            auto msg = fmt::format("{:5} Loading {:<8} from IMPORT file {}", deck_size, name, fname);
-            continue;
-        }
+        } else
+            throw std::logic_error(fmt::format("File: {} keyword:{}\nIMPORT keyword only supports integer and floating point data keywords", fname, name));
 
-        OpmLog::info(fmt::format("{:<5} Skipping {} from IMPORT file {}", "", name, fname));
+        deck_size += 1;
+        auto msg = fmt::format("{:5} Loading {:<8} from IMPORT file {}", deck_size, name, fname);
+        OpmLog::info(msg);
     }
 }
 
