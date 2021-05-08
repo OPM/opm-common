04ae01263864dc00c13a522a3fb894c3cd2ee4ff
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2021-05-08 19:25:06 +0200

    changed: python 3.5 is no longer support in pypi

--- python/generate-pypi-package.sh
+++ python/generate-pypi-package.sh
@@ -5,7 +5,6 @@
 
 VERSION_TAG=${1:-""}
 
-export PYTHON35=/opt/python/cp35-cp35m/bin/python
 export PYTHON36=/opt/python/cp36-cp36m/bin/python
 export PYTHON37=/opt/python/cp37-cp37m/bin/python
 export PYTHON38=/opt/python/cp38-cp38/bin/python
@@ -22,21 +21,16 @@ fi
 
 mkdir build && cd build
 
-cmake3 -DPYTHON_EXECUTABLE=${PYTHON35} -DBOOST_INCLUDEDIR=/usr/include/boost169 -DBOOST_LIBRARYDIR=/usr/lib64/boost169 \
+cmake3 -DPYTHON_EXECUTABLE=${PYTHON36} -DBOOST_INCLUDEDIR=/usr/include/boost169 -DBOOST_LIBRARYDIR=/usr/lib64/boost169 \
 -DOPM_ENABLE_PYTHON=ON -DOPM_PYTHON_PACKAGE_VERSION_TAG=${VERSION_TAG} -DBUILD_TESTING=0 ..
 
 # make step is necessary until the generated ParserKeywords/*.hpp are generated in the Python step
 make -j4
 
 ./setup-package.sh
-${PYTHON35} -m auditwheel repair python/dist/*cp35*.whl
-
-# Run setup-package.sh four times (Python 3.5, 3.6, 3.7 and 3.8)
-cmake3 -DPYTHON_EXECUTABLE=${PYTHON36} -DBOOST_INCLUDEDIR=/usr/include/boost169 -DBOOST_LIBRARYDIR=/usr/lib64/boost169 \
--DOPM_ENABLE_PYTHON=ON -DOPM_PYTHON_PACKAGE_VERSION_TAG=${VERSION_TAG} ..
-./setup-package.sh
 ${PYTHON36} -m auditwheel repair python/dist/*cp36*.whl
 
+# Run setup-package.sh three more times (Python 3.7, 3.8 and 3.9)
 cmake3 -DPYTHON_EXECUTABLE=${PYTHON37} -DBOOST_INCLUDEDIR=/usr/include/boost169 -DBOOST_LIBRARYDIR=/usr/lib64/boost169 \
 -DOPM_ENABLE_PYTHON=ON -DOPM_PYTHON_PACKAGE_VERSION_TAG=${VERSION_TAG} ..
 ./setup-package.sh
