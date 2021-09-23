7a65fe951e4a66e428531b3eebe465142e9b0b19
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2021-09-23 08:28:18 +0200

    set python path in jenkins build script

--- jenkins/build-opm-module.sh
+++ jenkins/build-opm-module.sh
@@ -222,8 +222,12 @@ function build_downstreams {
 
 # $1 = Name of main module
 function build_module_full {
+  PY_MAJOR=`python3 --version | awk -F ' ' '{print $2}' | awk -F '.' '{print $1}'`
+  PY_MINOR=`python3 --version | awk -F ' ' '{print $2}' | awk -F '.' '{print $2}'`
   for configuration in ${!configurations[@]}
   do
+    export PYTHONPATH="$WORKSPACE/$configuration/install/lib/python$PY_MAJOR.$PY_MINOR/dist-packages;$PYTHONPATH"
+
     # Build upstream modules
     build_upstreams
 
