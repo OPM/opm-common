344dd516a06d1a9acef8f281939022ff9ada1590
Joakim Hove <joakim.hove@gmail.com>
2021-10-23 17:49:22 +0200

    Fix rst_deck for non skiprest

--- src/opm/parser/eclipse/Deck/FileDeck.cpp
+++ src/opm/parser/eclipse/Deck/FileDeck.cpp
@@ -461,7 +461,7 @@ void FileDeck::skip(int report_step) {
 
         using D = ParserKeywords::DATES;
         current_report -= deck_keyword.size();
-        for (int record_index = 0; record_index < (report_step - current_report); record_index++) {
+        for (int record_index = report_step - current_report; record_index < deck_keyword.size(); record_index++) {
             const auto& record = deck_keyword[record_index];
             records.push_back( {DeckValue{record.getItem<D::DAY>().get<int>(0)},
                                 DeckValue{record.getItem<D::MONTH>().get<std::string>(0)},
