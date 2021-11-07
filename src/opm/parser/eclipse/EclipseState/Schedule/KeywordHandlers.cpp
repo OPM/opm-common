8bf2b456537fd5dca4c18f8ada4327f38d9ba196
Joakim Hove <joakim.hove@gmail.com>
2021-11-07 20:35:34 +0100

    Remove ParseContext::UNSUPPORTED_SCHEDULE_GEO_MODIFIERS

--- src/opm/parser/eclipse/EclipseState/Schedule/KeywordHandlers.cpp
+++ src/opm/parser/eclipse/EclipseState/Schedule/KeywordHandlers.cpp
@@ -730,7 +730,7 @@ namespace {
         std::string msg_fmt = fmt::format("Problem with keyword {{keyword}} at report step {}\n"
                                           "In {{file}} line {{line}}\n"
                                           "OPM does not support grid property modifier {} in the Schedule section", handlerContext.currentStep, handlerContext.keyword.name());
-        parseContext.handleError( ParseContext::UNSUPPORTED_SCHEDULE_GEO_MODIFIER , msg_fmt, handlerContext.keyword.location(), errors );
+        OpmLog::warning(OpmInputError::format(msg_fmt, handlerContext.keyword.location()));
     }
 
     void Schedule::handleNETBALAN(HandlerContext& handlerContext, const ParseContext&, ErrorGuard&) {
