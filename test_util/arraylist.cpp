1a0c81fbd4a2676dca6d1bd8f952d1e94ddc1d98
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2021-12-13 09:40:50 +0100

    Add missing include, and copyright header.

--- test_util/arraylist.cpp
+++ test_util/arraylist.cpp
@@ -1,8 +1,28 @@
+/*
+  Copyright 2021 Equinor ASA.
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <iomanip>
 #include <iostream>
 #include <tuple>
 #include <getopt.h>
 #include <filesystem>
+#include <sstream>
 
 #include <opm/io/eclipse/EclFile.hpp>
 #include <opm/io/eclipse/ERst.hpp>
