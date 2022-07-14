1a703f10a13d60e2be98dee97baaba34bf759cab
OPMUSER <opmuser0@gmail.com>
2022-07-14 12:40:26 +0800

    Update ConstructorMINPV Test
    Update ConstructorMINPV Test for MINPV/MINPORV

--- tests/parser/EclipseGridTests.cpp
+++ tests/parser/EclipseGridTests.cpp
@@ -255,7 +255,7 @@ static Opm::Deck createMinpvDefaultCPDeck() {
         "  1000*1 / \n"
         "MINPV \n"
         "  / \n"
-        "MINPVV \n"
+        "MINPORV \n"
         "  / \n"
         "EDIT\n"
         "\n";
@@ -847,7 +847,7 @@ BOOST_AUTO_TEST_CASE(ConstructorMINPV) {
     auto deck3 = createMinpvCPDeck();
 
     Opm::EclipseGrid grid1(deck1);
-    BOOST_CHECK_THROW(Opm::EclipseGrid grid2(deck2), std::invalid_argument);
+    BOOST_CHECK_THROW(Opm::EclipseGrid grid2(deck2), OpmInputError);
     Opm::EclipseGrid grid3(deck3);
 
     BOOST_CHECK(!grid1.equal( grid3 ));
