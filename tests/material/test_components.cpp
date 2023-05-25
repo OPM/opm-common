edefd93f380c7b8efd81000d377840baf6220069
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2023-05-25 21:43:41 +0200

    test_components: convert to boost::test

--- tests/material/test_components.cpp
+++ tests/material/test_components.cpp
@@ -27,6 +27,9 @@
  */
 #include "config.h"
 
+#define BOOST_TEST_MODULE Components
+#include <boost/test/unit_test.hpp>
+
 #include <opm/material/densead/Evaluation.hpp>
 #include <opm/material/densead/Math.hpp>
 
@@ -61,77 +64,9 @@
 #include <opm/material/common/UniformTabulated2DFunction.hpp>
 
 template <class Scalar, class Evaluation>
-void testSimpleH2O()
-{
-    typedef Opm::H2O<Scalar> H2O;
-    typedef Opm::SimpleHuDuanH2O<Scalar> SimpleHuDuanH2O;
-    typedef Opm::MathToolbox<Evaluation> EvalToolbox;
-
-    int numT = 67;
-    int numP = 45;
-    Evaluation T = 280;
-    Evaluation p = 1e6;
-
-    for (int iT = 0; iT < numT; ++iT) {
-        p = 1e6;
-        T += 5;
-        for (int iP = 0; iP < numP; ++iP) {
-            p *= 1.1;
-            if (!EvalToolbox::isSame(H2O::liquidDensity(T,p), SimpleHuDuanH2O::liquidDensity(T,p,false), /*tolerance=*/1e-3*H2O::liquidDensity(T,p).value()))
-                throw std::logic_error("oops: the water density based on Hu-Duan has more then 1e-3 deviation from IAPWS'97");
-
-            if (T >= 570) // for temperature larger then 570 the viscosity based on HuDuan is too far from IAPWS.
-                continue;
-
-            if (!EvalToolbox::isSame(H2O::liquidViscosity(T,p), SimpleHuDuanH2O::liquidViscosity(T,p,false), /*tolerance=*/5.e-2*H2O::liquidViscosity(T,p).value())){
-                throw std::logic_error("oops: the water viscosity based on Hu-Duan has more then 5e-2 deviation from IAPWS'97");
-            }
-        }
-    }
-}
-
-template <class Scalar, class Evaluation>
-void testDynamicBrine()
-{
-    typedef Opm::SimpleHuDuanH2O<Scalar> SimpleHuDuanH2O;
-    typedef Opm::Brine<Scalar, SimpleHuDuanH2O> Brine;
-    typedef Opm::BrineDynamic<Scalar, SimpleHuDuanH2O> BrineDyn;
-    typedef Opm::MathToolbox<Evaluation> EvalToolbox;
-
-    Brine::salinity = 0.1;
-    Evaluation sal = Brine::salinity;
-
-    int numT = 67;
-    int numP = 45;
-    Evaluation T = 280;
-    Evaluation p = 1e6;
-
-    for (int iT = 0; iT < numT; ++iT) {
-        p = 1e6;
-        T += 5;
-        for (int iP = 0; iP < numP; ++iP) {
-            p *= 1.1;
-
-            if (!EvalToolbox::isSame(Brine::liquidDensity(T,p), BrineDyn::liquidDensity(T,p, sal), /*tolerance=*/1e-5))
-                throw std::logic_error("oops: the brine density differes between Brine and Brine dynamic");
-
-            if (!EvalToolbox::isSame(Brine::liquidViscosity(T,p), BrineDyn::liquidViscosity(T,p,sal), /*tolerance=*/1e-5))
-                throw std::logic_error("oops: the brine viscosity differes between Brine and Brine dynamic");
-
-            if (!EvalToolbox::isSame(Brine::liquidEnthalpy(T,p), BrineDyn::liquidEnthalpy(T,p,sal), /*tolerance=*/1e-5))
-                throw std::logic_error("oops: the brine liquidEnthalpy differes between Brine and Brine dynamic");
-
-            if (!EvalToolbox::isSame(Brine::molarMass(), BrineDyn::molarMass(sal), /*tolerance=*/1e-5))
-                throw std::logic_error("oops: the brine molar mass differes between Brine and Brine dynamic");
-
-        }
-    }
-
-}
-template <class Scalar, class Evaluation>
 void testAllComponents()
 {
-    typedef Opm::H2O<Scalar> H2O;
+    using H2O = Opm::H2O<Scalar>;
 
     checkComponent<Opm::Air<Scalar>, Evaluation>();
     checkComponent<Opm::Brine<Scalar, H2O>, Evaluation>();
@@ -152,23 +87,90 @@ void testAllComponents()
     checkComponent<Opm::Xylene<Scalar>, Evaluation>();
 }
 
-template <class Scalar>
-inline void testAll()
+using Types = std::tuple<float,double>;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(All, Scalar, Types)
 {
-    typedef Opm::DenseAd::Evaluation<Scalar, 3> Evaluation;
+    using Evaluation = Opm::DenseAd::Evaluation<Scalar, 3>;
 
     // ensure that all components are API-compliant
     testAllComponents<Scalar, Scalar>();
     testAllComponents<Scalar, Evaluation>();
-    testSimpleH2O<Scalar, Evaluation>();
-    testDynamicBrine<Scalar, Evaluation>();
-
 }
 
-int main()
+BOOST_AUTO_TEST_CASE_TEMPLATE(SimpleH2O, Scalar, Types)
 {
-    testAll<double>();
-    testAll<float>();
+    using Evaluation = Opm::DenseAd::Evaluation<Scalar, 3>;
 
-    return 0;
+    using H2O = Opm::H2O<Scalar>;
+    using SimpleHuDuanH2O = Opm::SimpleHuDuanH2O<Scalar>;
+    using EvalToolbox = Opm::MathToolbox<Evaluation>;
+
+    int numT = 67;
+    int numP = 45;
+    Evaluation T = 280;
+
+    for (int iT = 0; iT < numT; ++iT) {
+        Evaluation p = 1e6;
+        T += 5;
+        for (int iP = 0; iP < numP; ++iP) {
+            p *= 1.1;
+            BOOST_CHECK_MESSAGE(EvalToolbox::isSame(H2O::liquidDensity(T,p),
+                                                    SimpleHuDuanH2O::liquidDensity(T,p,false),
+                                                    1e-3*H2O::liquidDensity(T,p).value()),
+                                "oops: the water density based on Hu-Duan has more then 1e-3 deviation from IAPWS'97");
+
+            if (T >= 570) // for temperature larger then 570 the viscosity based on HuDuan is too far from IAPWS.
+                continue;
+
+            BOOST_CHECK_MESSAGE(EvalToolbox::isSame(H2O::liquidViscosity(T,p),
+                                                    SimpleHuDuanH2O::liquidViscosity(T,p,false),
+                                                    5.e-2*H2O::liquidViscosity(T,p).value()),
+                                                    "oops: the water viscosity based on Hu-Duan has more then 5e-2 deviation from IAPWS'97");
+        }
+    }
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(DynamicBrine, Scalar, Types)
+{
+    using Evaluation = Opm::DenseAd::Evaluation<Scalar, 3>;
+    using SimpleHuDuanH2O = Opm::SimpleHuDuanH2O<Scalar>;
+    using Brine = Opm::Brine<Scalar, SimpleHuDuanH2O>;
+    using BrineDyn = Opm::BrineDynamic<Scalar, SimpleHuDuanH2O>;
+    using EvalToolbox = Opm::MathToolbox<Evaluation>;
+
+    Brine::salinity = 0.1;
+    Evaluation sal = Brine::salinity;
+
+    int numT = 67;
+    int numP = 45;
+    Evaluation T = 280;
+
+    for (int iT = 0; iT < numT; ++iT) {
+        Evaluation p = 1e6;
+        T += 5;
+        for (int iP = 0; iP < numP; ++iP) {
+            p *= 1.1;
+
+            BOOST_CHECK_MESSAGE(EvalToolbox::isSame(Brine::liquidDensity(T, p),
+                                                    BrineDyn::liquidDensity(T, p, sal),
+                                                   1e-5),
+                                "oops: the brine density differs between Brine and Brine dynamic");
+
+            BOOST_CHECK_MESSAGE(EvalToolbox::isSame(Brine::liquidViscosity(T, p),
+                                                    BrineDyn::liquidViscosity(T, p, sal),
+                                                    1e-5),
+                                "oops: the brine viscosity differs between Brine and Brine dynamic");
+
+            BOOST_CHECK_MESSAGE(EvalToolbox::isSame(Brine::liquidEnthalpy(T, p),
+                                                    BrineDyn::liquidEnthalpy(T, p, sal),
+                                                    1e-5),
+                                "oops: the brine liquidEnthalpy differs between Brine and Brine dynamic");
+
+            BOOST_CHECK_MESSAGE(EvalToolbox::isSame(Brine::molarMass(),
+                                                    BrineDyn::molarMass(sal),
+                                                    1e-5),
+                                "oops: the brine molar mass differs between Brine and Brine dynamic");
+        }
+    }
 }
