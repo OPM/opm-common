dea519c89a32d8c0a71c231df3448c706c2961f3
hnil <Halvor.M.Nilsen@sintef.no>
2023-06-07 10:35:13 +0200

    -- add config.h

--- src/opm/input/eclipse/Python/Python.cpp
+++ src/opm/input/eclipse/Python/Python.cpp
@@ -16,7 +16,7 @@
   You should have received a copy of the GNU General Public License
   along with OPM.  If not, see <http://www.gnu.org/licenses/>.
 */
-
+#include "config.h"
 #include <opm/input/eclipse/Schedule/SummaryState.hpp>
 #include <opm/input/eclipse/Python/Python.hpp>
 #include "PythonInterp.hpp"
