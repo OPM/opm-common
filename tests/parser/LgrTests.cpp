c0404e798e08087a4f4a555d33f7a22cb123c91f
goncalvesmachadoc <cintia.machado@tno.nl>
2023-07-17 17:24:25 +0200

    update tests

--- tests/parser/LgrTests.cpp
+++ tests/parser/LgrTests.cpp
@@ -13,11 +13,35 @@
   along with OPM.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define BOOST_TEST_MODULE CarfinManagerTests
+#define BOOST_TEST_MODULE LgrTests
 
 #include <boost/test/unit_test.hpp>
+#include <boost/test/test_tools.hpp>
 
+#include <opm/input/eclipse/Deck/Deck.hpp>
+#include <opm/input/eclipse/Parser/Parser.hpp>
+#include <opm/input/eclipse/EclipseState/EclipseState.hpp>
 #include <opm/input/eclipse/EclipseState/Grid/LgrCollection.hpp>
+#include <opm/input/eclipse/EclipseState/Grid/Carfin.hpp>
+#include <opm/input/eclipse/EclipseState/EclipseState.hpp>
+
+#include <filesystem>
+
+using namespace Opm;
+
+namespace {
+
+std::string prefix() {
+    return boost::unit_test::framework::master_test_suite().argv[1];
+}
+
+Deck makeDeck(const std::string& fileName) {
+    Parser parser;
+    std::filesystem::path boxFile(fileName);
+    return parser.parseFile(boxFile.string());
+}
+
+}
 
 BOOST_AUTO_TEST_CASE(CreateLgrCollection) {
     Opm::LgrCollection lgrs;
@@ -26,21 +50,17 @@ BOOST_AUTO_TEST_CASE(CreateLgrCollection) {
     BOOST_CHECK_THROW( lgrs.getLgr("NO") , std::invalid_argument );
 }
 
-BOOST_AUTO_TEST_CASE(AddLgrsToCollection) {
-    Opm::LgrCollection lgrs;
+BOOST_AUTO_TEST_CASE(ReadLgrCollection) {
+    auto deck = makeDeck( prefix() + "CARFIN/CARFINTEST1" ); 
+    Opm::EclipseState state(deck);
+    Opm::LgrCollection lgrs = state.getLgrs();
 
-    lgrs.addLgr("LGR1");
-    BOOST_CHECK_EQUAL( lgrs.size() , 1U );
-    BOOST_CHECK(lgrs.hasLGR("LGR1"));
-
-    const auto& lgr1 = lgrs.getLgr("LGR1");
-    const auto& lgr12 = lgrs.getLgr(0);
-    BOOST_CHECK_EQUAL(lgr1.getName(), lgr12.getName());
-    BOOST_CHECK_EQUAL(lgr1.getName(), "LGR1");
-
-    lgrs.addLgr("LGR2");
-    const auto& lgr2 = lgrs.getFault("LGR2");
     BOOST_CHECK_EQUAL( lgrs.size() , 2U );
+    BOOST_CHECK(lgrs.hasLgr("LGR1"));
     BOOST_CHECK(lgrs.hasLgr("LGR2"));
-    BOOST_CHECK_EQUAL( lgr2.getName() , lgrs.getLgr(1).getName());
+
+    const auto& lgr1 = state.getLgrs().getLgr("LGR1");
+    BOOST_CHECK_EQUAL(lgr1.NAME(), "LGR1");
+    const auto& lgr2 = lgrs.getLgr("LGR2");
+    BOOST_CHECK_EQUAL( lgr2.NAME() , "LGR2");
 }
