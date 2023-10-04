54bdca66e1aef8956aa53991f15fe3c021cf4e82
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2023-10-04 17:52:56 +0200

    added: enable WLIST in ACTIONX

--- src/opm/input/eclipse/Schedule/Action/ActionX.cpp
+++ src/opm/input/eclipse/Schedule/Action/ActionX.cpp
@@ -67,7 +67,7 @@ bool ActionX::valid_keyword(const std::string& keyword) {
         "MULTX", "MULTX-", "MULTY", "MULTY-", "MULTZ", "MULTZ-",
         "NEXT", "NEXTSTEP",
         "UDQ",
-        "WCONINJE", "WCONPROD", "WECON", "WEFAC", "WELOPEN", "WELPI", "WELTARG", "WGRUPCON", "WPIMULT", "WELSEGS", "WELSPECS", "WSEGVALV", "WTEST", "WTMULT",
+        "WCONINJE", "WCONPROD", "WECON", "WEFAC", "WELOPEN", "WELPI", "WELTARG", "WGRUPCON", "WPIMULT", "WELSEGS", "WELSPECS", "WSEGVALV", "WTEST", "WTMULT", "WLIST",
         "TEST"
     };
     return (actionx_allowed_list.find(keyword) != actionx_allowed_list.end());
