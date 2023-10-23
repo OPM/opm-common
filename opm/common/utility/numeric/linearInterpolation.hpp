568c06192f496bcd50a81b0b043630bb975294cd
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2023-10-23 13:24:00 +0200

    linearInterpolation.hpp: reformat

--- opm/common/utility/numeric/linearInterpolation.hpp
+++ opm/common/utility/numeric/linearInterpolation.hpp
@@ -22,86 +22,80 @@
 #define OPM_LINEARINTERPOLATION_HEADER_INCLUDED
 
 
-#include <vector>
 #include <algorithm>
+#include <vector>
 
 namespace Opm
 {
 
-    inline int tableIndex(const std::vector<double>& table, double x)
-    {
-	// Returns an index in an ordered table such that x is between
-	// table[j] and table[j+1]. If x is out of range, first or last
-	// interval is returned; Binary search.
-	int n = table.size() - 1;
-	if (n < 2) {
-	    return 0;
-	}
-	int jl = 0;
-	int ju = n;
-	bool ascend = (table[n] > table[0]);
-	while (ju - jl > 1) {
-	    int jm = (ju + jl)/2;   // Compute a midpoint
-	    if ( (x >= table[jm]) == ascend) {
-		jl = jm;     // Replace lower limit
-	    } else {
-		ju = jm;     // Replace upper limit
-	    }
-	}
-	return jl;
+inline int tableIndex(const std::vector<double>& table, double x)
+{
+    // Returns an index in an ordered table such that x is between
+    // table[j] and table[j+1]. If x is out of range, first or last
+    // interval is returned; Binary search.
+    int n = table.size() - 1;
+    if (n < 2) {
+        return 0;
     }
-
-
-    inline double linearInterpolationDerivative(const std::vector<double>& xv,
-                                                const std::vector<double>& yv, double x)
-    {
-        // Extrapolates if x is outside xv
-	int ix1 = tableIndex(xv, x);
-	int ix2 = ix1 + 1;
-	return  (yv[ix2] - yv[ix1])/(xv[ix2] - xv[ix1]);
-    }
-
-    inline double linearInterpolation(const std::vector<double>& xv,
-                                      const std::vector<double>& yv, double x)
-    {
-	// Extrapolates if x is outside xv
-	int ix1 = tableIndex(xv, x);
-	int ix2 = ix1 + 1;
-	return  (yv[ix2] - yv[ix1])/(xv[ix2] - xv[ix1])*(x - xv[ix1]) + yv[ix1];
-    }
-
-    inline double linearInterpolationNoExtrapolation(const std::vector<double>& xv,
-                                                     const std::vector<double>& yv, double x)
-    {
-        // Return end values if x is outside xv
-        if (x < xv.front()) {
-            return yv.front();
+    int jl = 0;
+    int ju = n;
+    bool ascend = (table[n] > table[0]);
+    while (ju - jl > 1) {
+        int jm = (ju + jl)/2;   // Compute a midpoint
+        if ( (x >= table[jm]) == ascend) {
+            jl = jm;     // Replace lower limit
+        } else {
+            ju = jm;     // Replace upper limit
         }
-        if (x > xv.back()) {
-            return yv.back();
-        }
+    }
+    return jl;
+}
 
-        int ix1 = tableIndex(xv, x);
-        int ix2 = ix1 + 1;
-        return  (yv[ix2] - yv[ix1])/(xv[ix2] - xv[ix1])*(x - xv[ix1]) + yv[ix1];
+
+inline double linearInterpolationDerivative(const std::vector<double>& xv,
+                                            const std::vector<double>& yv, double x)
+{
+    // Extrapolates if x is outside xv
+    int ix1 = tableIndex(xv, x);
+    int ix2 = ix1 + 1;
+    return  (yv[ix2] - yv[ix1])/(xv[ix2] - xv[ix1]);
+}
+
+inline double linearInterpolation(const std::vector<double>& xv,
+                                  const std::vector<double>& yv, double x)
+{
+    // Extrapolates if x is outside xv
+    int ix1 = tableIndex(xv, x);
+    int ix2 = ix1 + 1;
+    return  (yv[ix2] - yv[ix1])/(xv[ix2] - xv[ix1])*(x - xv[ix1]) + yv[ix1];
+}
+
+inline double linearInterpolationNoExtrapolation(const std::vector<double>& xv,
+                                                 const std::vector<double>& yv, double x)
+{
+    // Return end values if x is outside xv
+    if (x < xv.front()) {
+        return yv.front();
+    }
+    if (x > xv.back()) {
+        return yv.back();
     }
 
-    inline double linearInterpolation(const std::vector<double>& xv,
-                                      const std::vector<double>& yv,
-                                      double x, int& ix1)
-    {
-	// Extrapolates if x is outside xv
-	ix1 = tableIndex(xv, x);
-	int ix2 = ix1 + 1;
-	return (yv[ix2] - yv[ix1])/(xv[ix2] - xv[ix1])*(x - xv[ix1]) + yv[ix1];
-    }
+    int ix1 = tableIndex(xv, x);
+    int ix2 = ix1 + 1;
+    return  (yv[ix2] - yv[ix1])/(xv[ix2] - xv[ix1])*(x - xv[ix1]) + yv[ix1];
+}
 
-
+inline double linearInterpolation(const std::vector<double>& xv,
+                                  const std::vector<double>& yv,
+                                  double x, int& ix1)
+{
+    // Extrapolates if x is outside xv
+    ix1 = tableIndex(xv, x);
+    int ix2 = ix1 + 1;
+    return (yv[ix2] - yv[ix1])/(xv[ix2] - xv[ix1])*(x - xv[ix1]) + yv[ix1];
+}
 
 } // namespace Opm
-
-
-
-
 
 #endif // OPM_LINEARINTERPOLATION_HEADER_INCLUDED
