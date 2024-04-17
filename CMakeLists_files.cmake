14323dd59ada9deadeefa2260247d2963c286258
Lisa Julia Nebel <lisa.nebel@dr-blatt.de>
2024-04-17 10:19:24 +0200

    Put the Python ScheduleState class in its own file

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -356,6 +356,7 @@ if(ENABLE_ECL_INPUT)
           python/cxx/parsecontext.cpp
           python/cxx/parser.cpp
           python/cxx/schedule.cpp
+          python/cxx/schedule_state.cpp
           python/cxx/summary_state.cpp
           python/cxx/table_manager.cpp
           python/cxx/unit_system.cpp
