9ca1db320023309a0a961c08f5864addf3c377f0
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2024-05-30 12:50:04 +0200

    Explicitly Initialise GTNode Parent as Nullopt
    Mostly to placate static analyzers.

--- opm/input/eclipse/Schedule/Group/GTNode.cpp
+++ opm/input/eclipse/Schedule/Group/GTNode.cpp
@@ -15,52 +15,67 @@
 
   You should have received a copy of the GNU General Public License
   along with OPM.  If not, see <http://www.gnu.org/licenses/>.
- */
+*/
 
 #include <opm/input/eclipse/Schedule/Group/GTNode.hpp>
 
+#include <fmt/format.h>
+
 namespace Opm {
 
-GTNode::GTNode(const Group& group_arg, std::size_t level, const std::optional<std::string>& parent_name) :
-    m_group(group_arg),
-    m_level(level),
-    m_parent_name(parent_name)
-{
-}
+GTNode::GTNode(const Group&                      group_arg,
+               const std::size_t                 level,
+               const std::optional<std::string>& parent_name)
+    : m_group      (group_arg)
+    , m_level      (level)
+    , m_parent_name(parent_name)
+{}
 
-const std::string& GTNode::name() const {
+const std::string& GTNode::name() const
+{
     return this->m_group.name();
 }
 
-const Group& GTNode::group() const {
+const Group& GTNode::group() const
+{
     return this->m_group;
 }
 
-const std::string& GTNode::parent_name() const {
-    if (this->m_parent_name.has_value())
+const std::string& GTNode::parent_name() const
+{
+    if (this->m_parent_name.has_value()) {
         return *this->m_parent_name;
+    }
 
-    throw std::invalid_argument("Tried to access parent of root in GroupTree. Root: " + this->name());
+    throw std::invalid_argument {
+        fmt::format("Tried to access parent of root "
+                    "in GroupTree. Root: {}", this->name())
+    };
 }
 
 
-void GTNode::add_well(const Well& well) {
+void GTNode::add_well(const Well& well)
+{
     this->m_wells.push_back(well);
 }
 
-void GTNode::add_group(const GTNode& child_group) {
+void GTNode::add_group(const GTNode& child_group)
+{
     this->m_child_groups.push_back(child_group);
 }
 
-const std::vector<Well>& GTNode::wells() const {
+const std::vector<Well>& GTNode::wells() const
+{
     return this->m_wells;
 }
 
-const std::vector<GTNode>& GTNode::groups() const {
+const std::vector<GTNode>& GTNode::groups() const
+{
     return this->m_child_groups;
 }
 
-std::vector<const GTNode*> GTNode::all_nodes() const {
+std::vector<const GTNode*> GTNode::all_nodes() const
+{
     std::vector<const GTNode*> nodes { this };
 
     for (const auto& child_group : m_child_groups) {
@@ -71,8 +86,9 @@ std::vector<const GTNode*> GTNode::all_nodes() const {
     return nodes;
 }
 
-std::size_t GTNode::level() const {
+std::size_t GTNode::level() const
+{
     return this->m_level;
 }
 
-}
+} // namespace Opm
