bfdde14d2d178aba6de18644e746c1651c7b6ddc
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2024-06-14 11:54:22 +0200

    Lay Foundation for More Strict Array Operations
    This commit makes the backend implementations of ADD, MULTIPLY, &c
    capable of halting processing if we encounter any undefined source
    elements.  We nevertheless maintain the definition of "defined" as
    
        value::has_value()
    
    (from the value_status.hpp header), at least for the time being, as
    changing the definition requires making more parts of the array
    processing aware of the currently active section.
    
    The new helper function reject_undefined_operation() will throw an
    OpmInputError exception with (hopefully) enough context that fixing
    the input will be easier.
    
    While here, make the apply() function private to the FieldProps.cpp
    implementation file.  It was already marked 'static' and does not
    need access to any of the FieldProps data members.

--- opm/input/eclipse/EclipseState/Grid/FieldProps.cpp
+++ opm/input/eclipse/EclipseState/Grid/FieldProps.cpp
@@ -58,6 +58,7 @@
 #include <set>
 #include <stdexcept>
 #include <string>
+#include <string_view>
 #include <tuple>
 #include <unordered_map>
 #include <unordered_set>
@@ -211,7 +212,6 @@ global_kw_info(const std::string& name, bool)
 
 }} // namespace Fieldprops::keywords
 
-
 namespace {
 
 // The EQUALREG, MULTREG, COPYREG, ... keywords are used to manipulate
@@ -254,6 +254,23 @@ std::string default_region_keyword(const Deck& deck)
     }
 
     return "FLUXNUM";
+}
+
+void reject_undefined_operation(const KeywordLocation& loc,
+                                const std::size_t      numUnInit,
+                                const std::size_t      numElements,
+                                std::string_view       operation,
+                                std::string_view       arrayName)
+{
+    const auto* plural = (numElements > 1) ? "s" : "";
+
+    throw OpmInputError {
+        fmt::format
+        (R"({0} operation on array {1} would
+generate an undefined result in {2} out of {3} block{4}.)",
+         operation, arrayName, numUnInit, numElements, plural),
+        loc
+    };
 }
 
 template <typename T>
@@ -366,9 +383,9 @@ void multiply_deck(const Fieldprops::keywords::keyword_info<T>& kw_info,
 }
 
 template <typename T>
-void assign_scalar(std::vector<T>& data,
-                   std::vector<value::status>& value_status,
-                   T value,
+void assign_scalar(std::vector<T>&                     data,
+                   std::vector<value::status>&         value_status,
+                   const T                             value,
                    const std::vector<Box::cell_index>& index_list)
 {
     for (const auto& cell_index : index_list) {
@@ -378,56 +395,146 @@ void assign_scalar(std::vector<T>& data,
 }
 
 template <typename T>
-void multiply_scalar(std::vector<T>& data,
-                     std::vector<value::status>& value_status,
-                     T value,
+void multiply_scalar(const KeywordLocation&              loc,
+                     std::string_view                    arrayName,
+                     std::vector<T>&                     data,
+                     std::vector<value::status>&         value_status,
+                     const T                             value,
                      const std::vector<Box::cell_index>& index_list)
 {
+    auto unInit = 0;
+
     for (const auto& cell_index : index_list) {
-        if (value::has_value(value_status[cell_index.active_index])) {
-            data[cell_index.active_index] *= value;
+        const auto ix = cell_index.active_index;
+
+        if (value::has_value(value_status[ix])) {
+            data[ix] *= value;
         }
+        else {
+            ++unInit;
+        }
+    }
+
+    if (unInit > 0) {
+        reject_undefined_operation(loc, unInit,
+                                   index_list.size(),
+                                   "Multiplication", arrayName);
     }
 }
 
 template <typename T>
-void add_scalar(std::vector<T>& data,
-                std::vector<value::status>& value_status,
-                T value,
+void add_scalar(const KeywordLocation&              loc,
+                std::string_view                    arrayName,
+                std::vector<T>&                     data,
+                std::vector<value::status>&         value_status,
+                const T                             value,
                 const std::vector<Box::cell_index>& index_list)
 {
+    auto unInit = 0;
+
     for (const auto& cell_index : index_list) {
-        if (value::has_value(value_status[cell_index.active_index])) {
-            data[cell_index.active_index] += value;
+        const auto ix = cell_index.active_index;
+
+        if (value::has_value(value_status[ix])) {
+            data[ix] += value;
         }
+        else {
+            ++unInit;
+        }
+    }
+
+    if (unInit > 0) {
+        reject_undefined_operation(loc, unInit,
+                                   index_list.size(),
+                                   "Addition", arrayName);
     }
 }
 
 template <typename T>
-void min_value(std::vector<T>& data,
-               std::vector<value::status>& value_status,
-               T min_value,
+void min_value(const KeywordLocation&              loc,
+               std::string_view                    arrayName,
+               std::vector<T>&                     data,
+               std::vector<value::status>&         value_status,
+               const T                             value,
                const std::vector<Box::cell_index>& index_list)
 {
+    auto unInit = 0;
+
     for (const auto& cell_index : index_list) {
-        if (value::has_value(value_status[cell_index.active_index])) {
-            T value = data[cell_index.active_index];
-            data[cell_index.active_index] = std::max(value, min_value);
+        const auto ix = cell_index.active_index;
+
+        if (value::has_value(value_status[ix])) {
+            data[ix] = std::max(data[ix], value);
         }
+        else {
+            ++unInit;
+        }
+    }
+
+    if (unInit > 0) {
+        reject_undefined_operation(loc, unInit,
+                                   index_list.size(),
+                                   "Minimum threshold",
+                                   arrayName);
     }
 }
 
 template <typename T>
-void max_value(std::vector<T>& data,
-               std::vector<value::status>& value_status,
-               T max_value,
+void max_value(const KeywordLocation&              loc,
+               std::string_view                    arrayName,
+               std::vector<T>&                     data,
+               std::vector<value::status>&         value_status,
+               const T                             value,
                const std::vector<Box::cell_index>& index_list)
 {
+    auto unInit = 0;
+
     for (const auto& cell_index : index_list) {
-        if (value::has_value(value_status[cell_index.active_index])) {
-            T value = data[cell_index.active_index];
-            data[cell_index.active_index] = std::min(value, max_value);
+        const auto ix = cell_index.active_index;
+
+        if (value::has_value(value_status[ix])) {
+            data[ix] = std::min(data[ix], value);
         }
+        else {
+            ++unInit;
+        }
+    }
+
+    if (unInit > 0) {
+        reject_undefined_operation(loc, unInit,
+                                   index_list.size(),
+                                   "Maximum threshold",
+                                   arrayName);
+    }
+}
+
+template <typename T>
+void apply(const Fieldprops::ScalarOperation   op,
+           const KeywordLocation&              loc,
+           std::string_view                    arrayName,
+           std::vector<T>&                     data,
+           std::vector<value::status>&         value_status,
+           const T                             scalar_value,
+           const std::vector<Box::cell_index>& index_list)
+{
+    if (op == Fieldprops::ScalarOperation::EQUAL) {
+        assign_scalar(data, value_status, scalar_value, index_list);
+    }
+
+    else if (op == Fieldprops::ScalarOperation::MUL) {
+        multiply_scalar(loc, arrayName, data, value_status, scalar_value, index_list);
+    }
+
+    else if (op == Fieldprops::ScalarOperation::ADD) {
+        add_scalar(loc, arrayName, data, value_status, scalar_value, index_list);
+    }
+
+    else if (op == Fieldprops::ScalarOperation::MIN) {
+        min_value(loc, arrayName, data, value_status, scalar_value, index_list);
+    }
+
+    else if (op == Fieldprops::ScalarOperation::MAX) {
+        max_value(loc, arrayName, data, value_status, scalar_value, index_list);
     }
 }
 
@@ -546,7 +653,6 @@ bool rst_compare_data(const std::unordered_map<std::string, Fieldprops::FieldDat
 }
 
 } // Anonymous namespace
-
 
 bool FieldProps::operator==(const FieldProps& other) const
 {
@@ -1068,7 +1174,6 @@ std::vector<std::string> FieldProps::keys<int>() const
     return klist;
 }
 
-
 template <>
 void FieldProps::erase<int>(const std::string& keyword)
 {
@@ -1133,7 +1238,6 @@ double FieldProps::getSIValue(const ScalarOperation op,
         : this->getSIValue(keyword, raw_value);
 }
 
-
 void FieldProps::handle_int_keyword(const Fieldprops::keywords::keyword_info<int>& kw_info,
                                     const DeckKeyword& keyword,
                                     const Box& box)
@@ -1187,34 +1291,6 @@ void FieldProps::handle_double_keyword(const Section section,
                                        const Box& box)
 {
     this->handle_double_keyword(section, kw_info, keyword, keyword.name(), box);
-}
-
-template <typename T>
-void FieldProps::apply(const Fieldprops::ScalarOperation op,
-                       std::vector<T>& data,
-                       std::vector<value::status>& value_status,
-                       const T scalar_value,
-                       const std::vector<Box::cell_index>& index_list)
-{
-    if (op == Fieldprops::ScalarOperation::EQUAL) {
-        assign_scalar(data, value_status, scalar_value, index_list);
-    }
-
-    else if (op == Fieldprops::ScalarOperation::MUL) {
-        multiply_scalar(data, value_status, scalar_value, index_list);
-    }
-
-    else if (op == Fieldprops::ScalarOperation::ADD) {
-        add_scalar(data, value_status, scalar_value, index_list);
-    }
-
-    else if (op == Fieldprops::ScalarOperation::MIN) {
-        min_value(data, value_status, scalar_value, index_list);
-    }
-
-    else if (op == Fieldprops::ScalarOperation::MAX) {
-        max_value(data, value_status, scalar_value, index_list);
-    }
 }
 
 double FieldProps::get_alpha(const std::string& func_name,
@@ -1404,9 +1480,9 @@ void FieldProps::handle_region_operation(const DeckKeyword& keyword)
             const auto scalar_value =
                 this->getSIValue(operation, target_kw, record.getItem(1).get<double>(0));
 
-            FieldProps::apply(operation, field_data.data,
-                              field_data.value_status,
-                              scalar_value, index_list);
+            apply(operation, keyword.location(), target_kw,
+                  field_data.data, field_data.value_status,
+                  scalar_value, index_list);
 
             continue;
         }
@@ -1498,14 +1574,15 @@ void FieldProps::handle_operation(const Section      section,
             auto& field_data = this->init_get<double>
                 (unique_name, kw_info, editSect && kw_info.multiplier);
 
-            FieldProps::apply(operation, field_data.data,
-                              field_data.value_status,
-                              scalar_value, box.index_list());
+            apply(operation, keyword.location(), target_kw,
+                  field_data.data, field_data.value_status,
+                  scalar_value, box.index_list());
 
             if (field_data.global_data) {
-                FieldProps::apply(operation, *field_data.global_data,
-                                  *field_data.global_value_status,
-                                  scalar_value, box.global_index_list());
+                apply(operation, keyword.location(), target_kw,
+                      *field_data.global_data,
+                      *field_data.global_value_status,
+                      scalar_value, box.global_index_list());
             }
 
             continue;
@@ -1516,9 +1593,11 @@ void FieldProps::handle_operation(const Section      section,
 
             auto& field_data = this->init_get<int>(target_kw);
 
-            FieldProps::apply(operation, field_data.data,
-                              field_data.value_status,
-                              scalar_value, box.index_list());
+            apply(operation, keyword.location(), target_kw,
+                  field_data.data,
+                  field_data.value_status,
+                  scalar_value, box.index_list());
+
             continue;
         }
 
