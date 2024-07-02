3e91fd21408690a8c4d5525388e1f8133fac3f81
Lisa Julia Nebel <lisa.nebel@dr-blatt.de>
2024-07-02 14:41:46 +0200

    Add WELPI to PyAction Keywords

--- opm/input/eclipse/Schedule/Action/PyAction.cpp
+++ opm/input/eclipse/Schedule/Action/PyAction.cpp
@@ -46,7 +46,7 @@ bool PyAction::valid_keyword(const std::string& keyword) {
         "GCONINJE", "GCONPROD", "GCONSUMP","GRUPTREE",
         "METRIC", "MULTX", "MULTX-", "MULTY", "MULTY-", "MULTZ", "MULTZ-",
         "NEXT", "NEXTSTEP",
-        "WCONINJE", "WCONPROD", "WECON", "WEFAC", "WELOPEN", "WELTARG", "WGRUPCON",
+        "WCONINJE", "WCONPROD", "WECON", "WEFAC", "WELOPEN", "WELPI", "WELTARG", "WGRUPCON",
         "WELSEGS", "WELSPECS", "WSEGVALV", "WLIST", "WTEST", "WTMULT"
     };
     return pyaction_allowed_list.find(keyword) != pyaction_allowed_list.end();
