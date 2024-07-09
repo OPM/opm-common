528fceb95c62478923117ab5c6f2caa8191b9e2d
Lisa Julia Nebel <lisa.nebel@dr-blatt.de>
2024-07-09 13:28:34 +0200

    Remove the argument welpi_action_mode from the Schedule::handleKeyword function
    This is not needed anymore, since welpi_action_mode is an attribute of the Schedule.

--- opm/input/eclipse/Schedule/Schedule.hpp
+++ opm/input/eclipse/Schedule/Schedule.hpp
@@ -478,7 +478,6 @@ namespace Opm {
                            ErrorGuard& errors,
                            const ScheduleGrid& grid,
                            const Action::Result::MatchingEntities& matches,
-                           bool welpi_action_mode,
                            SimulatorUpdate* sim_update,
                            const std::unordered_map<std::string, double>* target_wellpi,
                            std::unordered_map<std::string, double>& wpimult_global_factor,
