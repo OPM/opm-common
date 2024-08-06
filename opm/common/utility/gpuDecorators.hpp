a929b3620b1026c5164f1f4200756e1b6b091238
Tobias Meyer Andersen <tobiasmeyer.andersen@sintef.no>
2024-08-06 11:35:37 +0200

    improve gpuDecorators.hpp

--- opm/common/utility/gpuDecorators.hpp
+++ opm/common/utility/gpuDecorators.hpp
@@ -30,41 +30,35 @@
 // HAVE_CUDA and USE_HIP are found in config.h
 #ifndef OPM_GPUDECORATORS_HPP
   #define OPM_GPUDECORATORS_HPP
+
   #include <config.h>
+
+  // true if using nvcc/hipcc gpu compiler
+  #define OPM_IS_COMPILING_WITH_GPU_COMPILER defined(__NVCC__) || defined(__HIPCC__)
+
+  // true inside device version of functions marked __device__
+  #define OPM_IS_INSIDE_DEVICE_FUNCTION (defined(__CUDA_ARCH__) || (defined(__HIP_DEVICE_COMPILE__) && __HIP_DEVICE_COMPILE__ > 0))
+
+  // Default macros for non-GPU compilation
+  #define OPM_HOST_DEVICE
+  #define OPM_DEVICE
+  #define OPM_HOST
+  #define OPM_IS_USING_GPU 0
 
   #if HAVE_CUDA // if we will compile with GPU support
 
     //handle inclusion of correct gpu runtime headerfiles
-    #if USE_HIP // if we compile for AMD architectures
+    #if USE_HIP // use HIP if we compile for AMD architectures
       #include <hip/hip_runtime.h>
-    #else // if we compile for Nvidia architectures
+    #else // otherwise include cuda
       #include <cuda_runtime.h>
     #endif // END USE_HIP
 
-    // define host and device function attributes
     #define OPM_HOST_DEVICE __device__ __host__
     #define OPM_DEVICE __device__
     #define OPM_HOST __host__
-    // define a variable keeping track of whether or not we are compiling for GPUs or not
     #define OPM_IS_USING_GPU 1
 
-    // add a variable that can keep track of whether or not we are compiling with hipcc/nvcc (gpu compilers)
-    #if defined(__NVCC__) || defined(__HIPCC__)
-      #define OPM_IS_COMIPING_WITH_GPU_COMPILER 1
-    #endif // END IF GPUCC
-
-    // add a variable keeping track of whether or not it is placed inside a function marked __device__
-    #if defined(__CUDA_ARCH__) || defined(__HIP_DEVICE_COMPILE__)
-      #define OPM_IS_INSIDE_DEVICE_FUNCTION (defined(__CUDA_ARCH__) || (defined(__HIP_DEVICE_COMPILE__) && __HIP_DEVICE_COMPILE__ > 0))
-    #endif
-
-  #else // if we are not using CUDA/HIP, let the macros be empty
-    #define OPM_HOST_DEVICE
-    #define OPM_DEVICE
-    #define OPM_HOST
-    #define OPM_IS_COMIPING_WITH_GPU_COMPILER
-    #define OPM_IS_USING_GPU
-    #define OPM_IS_INSIDE_DEVICE_FUNCTION
   #endif // END ELSE
 
 #endif // END HEADER GUARD
