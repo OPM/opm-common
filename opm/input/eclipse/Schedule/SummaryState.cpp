fa417ec4668ffe4f9f73733202713d4d711b2b3b
Vegard Kippe <vkip@equinor.com>
2024-08-28 14:10:37 +0200

    Fix assignment of selected well variables, also if quoted.

--- opm/input/eclipse/Schedule/SummaryState.cpp
+++ opm/input/eclipse/Schedule/SummaryState.cpp
@@ -401,27 +401,13 @@ namespace Opm
     {
         const auto var_type = udq_set.var_type();
         if (var_type == UDQVarType::WELL_VAR) {
-            const std::vector<std::string> wells = this->wells(); // Intentional copy
-            for (const auto& well : wells) {
-                if (! udq_set.has(well)) {
-                    this->update_well_var(well, udq_set.name(), undefined_value);
-                    continue;
-                }
-
-                const auto& udq_value = udq_set[well].value();
-                this->update_well_var(well, udq_set.name(), udq_value.value_or(undefined_value));
+            for (const auto& udq_value : udq_set) {
+                this->update_well_var(udq_value.wgname(), udq_set.name(), udq_value.value().value_or(undefined_value));
             }
         }
         else if (var_type == UDQVarType::GROUP_VAR) {
-            const std::vector<std::string> groups = this->groups(); // Intentional copy
-            for (const auto& group : groups) {
-                if (! udq_set.has(group)) {
-                    this->update_group_var(group, udq_set.name(), undefined_value);
-                    continue;
-                }
-
-                const auto& udq_value = udq_set[group].value();
-                this->update_group_var(group, udq_set.name(), udq_value.value_or(undefined_value));
+            for (const auto& udq_value : udq_set) {
+                this->update_group_var(udq_value.wgname(), udq_set.name(), udq_value.value().value_or(undefined_value));
             }
         }
         else if (var_type == UDQVarType::SEGMENT_VAR) {
