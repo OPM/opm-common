bf67a163210cf75f65910d757500b199b4f4ee21
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2024-09-17 14:41:42 +0200

    ConstantCompressibilityOilPvt: move more code to translation unit

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -59,6 +59,7 @@ list (APPEND MAIN_SOURCE_FILES
       opm/material/fluidsystems/blackoilpvt/BrineH2Pvt.cpp
       opm/material/fluidsystems/blackoilpvt/Co2GasPvt.cpp
       opm/material/fluidsystems/blackoilpvt/ConstantCompressibilityBrinePvt.cpp
+      opm/material/fluidsystems/blackoilpvt/ConstantCompressibilityOilPvt.cpp
       opm/material/fluidsystems/blackoilpvt/DeadOilPvt.cpp
       opm/material/fluidsystems/blackoilpvt/DryGasPvt.cpp
       opm/material/fluidsystems/blackoilpvt/DryHumidGasPvt.cpp
@@ -335,7 +336,6 @@ if(ENABLE_ECL_INPUT)
     opm/material/fluidmatrixinteractions/EclMaterialLawManagerInitParams.cpp
     opm/material/fluidmatrixinteractions/EclMaterialLawManagerHystParams.cpp
     opm/material/fluidsystems/blackoilpvt/H2GasPvt.cpp
-    opm/material/fluidsystems/blackoilpvt/ConstantCompressibilityOilPvt.cpp
     opm/material/fluidsystems/blackoilpvt/ConstantCompressibilityWaterPvt.cpp
     opm/material/fluidsystems/blackoilpvt/GasPvtThermal.cpp
     opm/material/fluidsystems/blackoilpvt/OilPvtThermal.cpp
