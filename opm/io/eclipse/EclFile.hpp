911d208c8e9664e0379a931f76d232013e6d009d
Magne <magne.sjaastad@ceetronsolutions.com>
2025-01-07 10:44:17 +0100

    Reset structure used to track state of keyword load

--- opm/io/eclipse/EclFile.hpp
+++ opm/io/eclipse/EclFile.hpp
@@ -55,6 +55,8 @@ public:
       doub_array.clear();
       logi_array.clear();
       char_array.clear();
+
+      std::fill(arrayLoaded.begin(), arrayLoaded.end(), false);
     }
 
     using EclEntry = std::tuple<std::string, eclArrType, std::int64_t>;
