8973c502d51cd275d35b7b044ef637ac1e9daab1
Magne <magne.sjaastad@ceetronsolutions.com>
2025-01-07 11:12:32 +0100

    Format changes

--- opm/io/eclipse/EGrid.hpp
+++ opm/io/eclipse/EGrid.hpp
@@ -3,9 +3,8 @@
 
    This file is part of the Open Porous Media project (OPM).
 
-   OPM is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+   OPM is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
    OPM is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -23,107 +22,150 @@
 
 #include <array>
 #include <filesystem>
+#include <map>
 #include <string>
 #include <vector>
-#include <map>
 
-namespace Opm { namespace EclIO {
-
-class EGrid : public EclFile
+namespace Opm
 {
-public:
-    explicit EGrid(const std::string& filename, const std::string& grid_name = "global");
+namespace EclIO
+{
 
-    int global_index(int i, int j, int k) const;
-    int active_index(int i, int j, int k) const;
+    class EGrid : public EclFile
+    {
+    public:
+        explicit EGrid(const std::string& filename, const std::string& grid_name = "global");
 
-    const std::array<int, 3>& dimension() const { return nijk; }
+        int global_index(int i, int j, int k) const;
+        int active_index(int i, int j, int k) const;
 
-    std::array<int, 3> ijk_from_active_index(int actInd) const;
-    std::array<int, 3> ijk_from_global_index(int globInd) const;
+        const std::array<int, 3>& dimension() const
+        {
+            return nijk;
+        }
 
-    void getCellCorners(int globindex, std::array<double, 8>& X, std::array<double, 8>& Y, std::array<double, 8>& Z);
-    void getCellCorners(const std::array<int, 3>& ijk, std::array<double, 8>& X, std::array<double, 8>& Y, std::array<double, 8>& Z);
+        std::array<int, 3> ijk_from_active_index(int actInd) const;
+        std::array<int, 3> ijk_from_global_index(int globInd) const;
 
-    std::vector<std::array<float, 3>> getXYZ_layer(int layer, bool bottom=false);
-    std::vector<std::array<float, 3>> getXYZ_layer(int layer, const std::array<int, 4>& box, bool bottom=false);
+        void
+        getCellCorners(int globindex, std::array<double, 8>& X, std::array<double, 8>& Y, std::array<double, 8>& Z);
+        void getCellCorners(const std::array<int, 3>& ijk,
+                            std::array<double, 8>& X,
+                            std::array<double, 8>& Y,
+                            std::array<double, 8>& Z);
 
-    int activeCells() const { return nactive; }
-    int totalNumberOfCells() const { return nijk[0] * nijk[1] * nijk[2]; }
+        std::vector<std::array<float, 3>> getXYZ_layer(int layer, bool bottom = false);
+        std::vector<std::array<float, 3>> getXYZ_layer(int layer, const std::array<int, 4>& box, bool bottom = false);
 
-    void load_grid_data();
-    void load_nnc_data();
-    bool with_mapaxes() const { return m_mapaxes_loaded; }
-    void mapaxes_transform(double& x, double& y) const;
-    bool is_radial() const { return m_radial; }
+        int activeCells() const
+        {
+            return nactive;
+        }
+        int totalNumberOfCells() const
+        {
+            return nijk[0] * nijk[1] * nijk[2];
+        }
 
-    const std::vector<int>& hostCellsGlobalIndex() const { return host_cells; }
-    std::vector<std::array<int, 3>> hostCellsIJK();
+        void load_grid_data();
+        void load_nnc_data();
+        bool with_mapaxes() const
+        {
+            return m_mapaxes_loaded;
+        }
+        void mapaxes_transform(double& x, double& y) const;
+        bool is_radial() const
+        {
+            return m_radial;
+        }
 
-    // zero based: i1,j1,k1, i2,j2,k2, transmisibility
-    using NNCentry = std::tuple<int, int, int, int, int, int, float>;
-    std::vector<NNCentry> get_nnc_ijk();
+        const std::vector<int>& hostCellsGlobalIndex() const
+        {
+            return host_cells;
+        }
+        std::vector<std::array<int, 3>> hostCellsIJK();
 
-    const std::vector<std::string>& list_of_lgrs() const { return lgr_names; }
+        // zero based: i1,j1,k1, i2,j2,k2, transmisibility
+        using NNCentry = std::tuple<int, int, int, int, int, int, float>;
+        std::vector<NNCentry> get_nnc_ijk();
 
-    const std::array<double, 6>& get_mapaxes() const { return m_mapaxes; }
-    const std::string& get_mapunits() const { return m_mapunits; }
-    const std::vector<float>& get_coord() const { return coord_array; }
-    const std::vector<float>& get_zcorn() const { return zcorn_array; }
+        const std::vector<std::string>& list_of_lgrs() const
+        {
+            return lgr_names;
+        }
+
+        const std::array<double, 6>& get_mapaxes() const
+        {
+            return m_mapaxes;
+        }
+        const std::string& get_mapunits() const
+        {
+            return m_mapunits;
+        }
+        const std::vector<float>& get_coord() const
+        {
+            return coord_array;
+        }
+        const std::vector<float>& get_zcorn() const
+        {
+            return zcorn_array;
+        }
 
 
 
 
-private:
-    std::filesystem::path inputFileName, initFileName;
-    std::string m_grid_name;
-    bool m_radial;
+    private:
+        std::filesystem::path inputFileName, initFileName;
+        std::string m_grid_name;
+        bool m_radial;
 
-    std::array<double, 6> m_mapaxes;
-    std::string m_mapunits;
-    bool m_mapaxes_loaded;
-    std::array<double, 4> origin;
-    std::array<double, 2> unit_x;
-    std::array<double, 2> unit_y;
+        std::array<double, 6> m_mapaxes;
+        std::string m_mapunits;
+        bool m_mapaxes_loaded;
+        std::array<double, 4> origin;
+        std::array<double, 2> unit_x;
+        std::array<double, 2> unit_y;
 
-    std::array<int, 3> nijk;
-    std::array<int, 3> host_nijk;
+        std::array<int, 3> nijk;
+        std::array<int, 3> host_nijk;
 
-    int nactive;
-    mutable bool m_nncs_loaded;
+        int nactive;
+        mutable bool m_nncs_loaded;
 
-    std::vector<int> act_index;
-    std::vector<int> glob_index;
+        std::vector<int> act_index;
+        std::vector<int> glob_index;
 
-    std::vector<float> coord_array;
-    std::vector<float> zcorn_array;
+        std::vector<float> coord_array;
+        std::vector<float> zcorn_array;
 
-    std::vector<int> nnc1_array;
-    std::vector<int> nnc2_array;
-    std::vector<float> transnnc_array;
-    std::vector<int> host_cells;
-    std::map<int,int> res;
-     
-    std::vector<std::string> lgr_names;
-    
-    int numres;
-    
-    int zcorn_array_index;
-    int coord_array_index;
-    int coordsys_array_index;
-    int actnum_array_index;
-    int nnc1_array_index;
-    int nnc2_array_index;
+        std::vector<int> nnc1_array;
+        std::vector<int> nnc2_array;
+        std::vector<float> transnnc_array;
+        std::vector<int> host_cells;
+        std::map<int, int> res;
 
-    std::vector<float> get_zcorn_from_disk(int layer, bool bottom);
+        std::vector<std::string> lgr_names;
 
-    void getCellCorners(const std::array<int, 3>& ijk, const std::vector<float>& zcorn_layer,
-                        std::array<double, 4>& X, std::array<double, 4>& Y, std::array<double, 4>& Z);
+        int numres;
 
-    void mapaxes_init();
-    
-};
+        int zcorn_array_index;
+        int coord_array_index;
+        int coordsys_array_index;
+        int actnum_array_index;
+        int nnc1_array_index;
+        int nnc2_array_index;
 
-}} // namespace Opm::EclIO
+        std::vector<float> get_zcorn_from_disk(int layer, bool bottom);
+
+        void getCellCorners(const std::array<int, 3>& ijk,
+                            const std::vector<float>& zcorn_layer,
+                            std::array<double, 4>& X,
+                            std::array<double, 4>& Y,
+                            std::array<double, 4>& Z);
+
+        void mapaxes_init();
+    };
+
+} // namespace EclIO
+} // namespace Opm
 
 #endif // OPM_IO_EGRID_HPP
--- opm/io/eclipse/ERst.hpp
+++ opm/io/eclipse/ERst.hpp
@@ -3,9 +3,8 @@
 
    This file is part of the Open Porous Media project (OPM).
 
-   OPM is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+   OPM is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
    OPM is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -28,77 +27,96 @@
 #include <vector>
 
 
-namespace Opm { namespace EclIO { namespace OutputStream {
-    class Restart;
-}}}
-
-namespace Opm { namespace EclIO {
-
-class ERst : public EclFile
+namespace Opm
 {
-public:
-    explicit ERst(const std::string& filename);
-
-    bool hasReportStepNumber(int number) const;
-    bool hasArray(const std::string& name, int number) const;
-    bool hasLGR(const std::string& gridname, int reportStepNumber) const;
-
-    void loadReportStepNumber(int number);
-
-    template <typename T>
-    const std::vector<T>& getRestartData(const std::string& name, int reportStepNumber)
+namespace EclIO
+{
+    namespace OutputStream
     {
-        return getRestartData<T>(name,reportStepNumber, 0);
+        class Restart;
     }
+} // namespace EclIO
+} // namespace Opm
 
-    template <typename T>
-    const std::vector<T>& getRestartData(const std::string& name, int reportStepNumber, int occurrence);
 
-    template <typename T>
-    const std::vector<T>& getRestartData(int index, int reportStepNumber)
+
+
+namespace Opm
+{
+namespace EclIO
+{
+
+    class ERst : public EclFile
     {
-        auto indRange = this->getIndexRange(reportStepNumber);
-        return  this->get<T>(index + std::get<0>(indRange));
-    }
+    public:
+        explicit ERst(const std::string& filename);
 
-    template <typename T>
-    const std::vector<T>& getRestartData(const std::string& name, int reportStepNumber, const std::string& lgr_name);
+        bool hasReportStepNumber(int number) const;
+        bool hasArray(const std::string& name, int number) const;
+        bool hasLGR(const std::string& gridname, int reportStepNumber) const;
 
-    template <typename T>
-    const std::vector<T>& getRestartData(int index, int reportStepNumber, const std::string& lgr_name);
+        void loadReportStepNumber(int number);
 
-    int occurrence_count(const std::string& name, int reportStepNumber) const;
-    size_t numberOfReportSteps() const { return seqnum.size(); };
+        template <typename T>
+        const std::vector<T>& getRestartData(const std::string& name, int reportStepNumber)
+        {
+            return getRestartData<T>(name, reportStepNumber, 0);
+        }
 
-    const std::vector<int>& listOfReportStepNumbers() const { return seqnum; }
+        template <typename T>
+        const std::vector<T>& getRestartData(const std::string& name, int reportStepNumber, int occurrence);
 
-    std::vector<EclEntry> listOfRstArrays(int reportStepNumber);
-    std::vector<EclEntry> listOfRstArrays(int reportStepNumber, const std::string& lgr_name);
+        template <typename T>
+        const std::vector<T>& getRestartData(int index, int reportStepNumber)
+        {
+            auto indRange = this->getIndexRange(reportStepNumber);
+            return this->get<T>(index + std::get<0>(indRange));
+        }
 
-    friend class OutputStream::Restart;
+        template <typename T>
+        const std::vector<T>&
+        getRestartData(const std::string& name, int reportStepNumber, const std::string& lgr_name);
 
-private:
-    int nReports;
-    std::vector<int> seqnum;                           // report step numbers, from SEQNUM array in restart file
-    mutable std::unordered_map<int,bool> reportLoaded;
-    std::map<int, std::pair<int,int>> arrIndexRange;   // mapping report step number to array indeces (start and end)
-    std::vector<std::vector<std::string>> lgr_names;                           // report step numbers, from SEQNUM array in restart file
+        template <typename T>
+        const std::vector<T>& getRestartData(int index, int reportStepNumber, const std::string& lgr_name);
 
-    void initUnified();
-    void initSeparate(const int number);
+        int occurrence_count(const std::string& name, int reportStepNumber) const;
+        size_t numberOfReportSteps() const
+        {
+            return seqnum.size();
+        };
 
-    int get_start_index_lgrname(int number, const std::string& lgr_name);
+        const std::vector<int>& listOfReportStepNumbers() const
+        {
+            return seqnum;
+        }
 
-    int getArrayIndex(const std::string& name, int seqnum, int occurrence);
-    int getArrayIndex(const std::string& name, int number, const std::string& lgr_name);
+        std::vector<EclEntry> listOfRstArrays(int reportStepNumber);
+        std::vector<EclEntry> listOfRstArrays(int reportStepNumber, const std::string& lgr_name);
 
-    std::tuple<int,int> getIndexRange(int reportStepNumber) const;
+        friend class OutputStream::Restart;
 
-    std::streampos
-    restartStepWritePosition(const int seqnumValue) const;
+    private:
+        int nReports;
+        std::vector<int> seqnum; // report step numbers, from SEQNUM array in restart file
+        mutable std::unordered_map<int, bool> reportLoaded;
+        std::map<int, std::pair<int, int>> arrIndexRange; // mapping report step number to array indeces (start and end)
+        std::vector<std::vector<std::string>> lgr_names; // report step numbers, from SEQNUM array in restart file
 
-};
+        void initUnified();
+        void initSeparate(const int number);
 
-}} // namespace Opm::EclIO
+        int get_start_index_lgrname(int number, const std::string& lgr_name);
+
+        int getArrayIndex(const std::string& name, int seqnum, int occurrence);
+        int getArrayIndex(const std::string& name, int number, const std::string& lgr_name);
+
+        std::tuple<int, int> getIndexRange(int reportStepNumber) const;
+
+        std::streampos restartStepWritePosition(const int seqnumValue) const;
+    };
+
+} // namespace EclIO
+} // namespace Opm
 
 #endif // OPM_IO_ERST_HPP
