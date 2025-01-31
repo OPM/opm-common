26751164a8d8497e5e5860e71b4b45d88dcb08d5
Svenn Tveit <svtv@norceresearch.no>
2025-01-31 14:45:13 +0100

    Update test case to EOS types RK and SRK.
    Moved reference values to JSON files for cleaner code.

--- tests/material/test_threecomponents_ptflash.cpp
+++ tests/material/test_threecomponents_ptflash.cpp
@@ -47,6 +47,8 @@
 
 #include <opm/input/eclipse/EclipseState/Compositional/CompositionalConfig.hpp>
 
+#include <opm/json/JsonObject.hpp>
+
 // It is a three component system
 using Scalar = double;
 using EOSType = Opm::CompositionalConfig::EOSType;
@@ -59,6 +61,7 @@ using FluidState = Opm::CompositionalFluidState<Evaluation, FluidSystem>;
 
 std::vector<std::string> test_methods {"newton", "ssi", "ssi+newton"};
 // std::vector<std::string> test_methods {"ssi"};
+std::vector<EOSType> test_eos_types {EOSType::PR, EOSType::PRCORR, EOSType::SRK, EOSType::RK};
 
 #if BOOST_VERSION / 100000 == 1 && BOOST_VERSION / 100 % 1000 > 66
 BOOST_DATA_TEST_CASE(PtFlash, test_methods)
@@ -69,6 +72,11 @@ BOOST_AUTO_TEST_CASE(PtFlash)
 #if BOOST_VERSION / 100000 == 1 && BOOST_VERSION / 100 % 1000 < 67
 for (const auto& sample : test_methods) {
 #endif
+
+    // Initialize JSON file with reference values
+    std::filesystem::path jsonFile("material/ref_values_threecomponents_ptflash.json");
+    Json::JsonObject parser(jsonFile);
+    
     // Initial: the primary variables are, pressure, molar fractions of the first and second component
     Evaluation p_init = Evaluation::createVariable(10e5, 0); // 10 bar
     ComponentVector comp;
@@ -78,108 +86,105 @@ for (const auto& sample : test_methods) {
 
     const Scalar temp = 300.0;
 
-    // FluidState will be the input for the flash calculation
-    FluidState fluid_state;
-    fluid_state.setPressure(FluidSystem::oilPhaseIdx, p_init);
-    fluid_state.setPressure(FluidSystem::gasPhaseIdx, p_init);
+    // Loop over EOS types
+    for (const auto& eos_type : test_eos_types) {
+        // FluidState will be the input for the flash calculation
+        FluidState fluid_state;
+        fluid_state.setPressure(FluidSystem::oilPhaseIdx, p_init);
+        fluid_state.setPressure(FluidSystem::gasPhaseIdx, p_init);
 
-    fluid_state.setMoleFraction(FluidSystem::Comp0Idx, comp[0]);
-    fluid_state.setMoleFraction(FluidSystem::Comp1Idx, comp[1]);
-    fluid_state.setMoleFraction(FluidSystem::Comp2Idx, comp[2]);
+        fluid_state.setMoleFraction(FluidSystem::Comp0Idx, comp[0]);
+        fluid_state.setMoleFraction(FluidSystem::Comp1Idx, comp[1]);
+        fluid_state.setMoleFraction(FluidSystem::Comp2Idx, comp[2]);
 
-    fluid_state.setTemperature(temp);
+        fluid_state.setTemperature(temp);
 
-    const double flash_tolerance = 1.e-8; // just to test the setup in co2-compositional
-    const int flash_verbosity = 0;
+        const double flash_tolerance = 1.e-8; // just to test the setup in co2-compositional
+        const int flash_verbosity = 0;
 
-    // TODO: should we set these?
-    // Set initial K and L
-    for (unsigned compIdx = 0; compIdx < numComponents; ++compIdx) {
-        const Evaluation Ktmp = fluid_state.wilsonK_(compIdx);
-        fluid_state.setKvalue(compIdx, Ktmp);
-    }
-    const Evaluation Ltmp = 1.;
-    fluid_state.setLvalue(Ltmp);
-
-    using Flash = Opm::PTFlash<double, FluidSystem>;
-    auto eos_type = EOSType::PR;
-    Flash::solve(fluid_state, sample, flash_tolerance, eos_type, flash_verbosity);
-
-    ComponentVector x, y;
-    const Evaluation L = fluid_state.L();
-    for (unsigned comp_idx = 0; comp_idx < numComponents; ++comp_idx) {
-        x[comp_idx] = fluid_state.moleFraction(FluidSystem::oilPhaseIdx, comp_idx);
-        y[comp_idx] = fluid_state.moleFraction(FluidSystem::gasPhaseIdx, comp_idx);
-    }
-
-    if (flash_verbosity >= 1) {
-        for (unsigned comp_idx = 0; comp_idx < numComponents; ++comp_idx) {
-            std::cout << " x for component: " << comp_idx << "is " << x[comp_idx] << std::endl;
-             for (int i = 0; i < 3; ++i) {
-                 std::cout << " x deriv " << i << " is: " << x[comp_idx].derivative(i) << std::endl;
-             }
-
-            std::cout << " y for component: " << comp_idx << "is " << y[comp_idx] << std::endl;
-             for (int i = 0; i < 3; ++i) {
-                 std::cout << " y deriv " << i << " is: " << y[comp_idx].derivative(i) << std::endl;
-             }
+        // TODO: should we set these?
+        // Set initial K and L
+        for (unsigned compIdx = 0; compIdx < numComponents; ++compIdx) {
+            const Evaluation Ktmp = fluid_state.wilsonK_(compIdx);
+            fluid_state.setKvalue(compIdx, Ktmp);
         }
-        std::cout << " L is " << L << std::endl;
-         for (int i = 0; i < L.size(); ++i) {
-                 std::cout << " L deriv " << i << " is: " << L.derivative(i) << std::endl;
-         }
+        const Evaluation Ltmp = 1.;
+        fluid_state.setLvalue(Ltmp);
+
+        using Flash = Opm::PTFlash<double, FluidSystem>;
+        Flash::solve(fluid_state, sample, flash_tolerance, eos_type, flash_verbosity);
+
+        ComponentVector x, y;
+        const Evaluation L = fluid_state.L();
+        for (unsigned comp_idx = 0; comp_idx < numComponents; ++comp_idx) {
+            x[comp_idx] = fluid_state.moleFraction(FluidSystem::oilPhaseIdx, comp_idx);
+            y[comp_idx] = fluid_state.moleFraction(FluidSystem::gasPhaseIdx, comp_idx);
+        }
+
+        if (flash_verbosity >= 1) {
+            std::cout << "Results for " << Opm::CompositionalConfig::eosTypeToString(eos_type) << " :" << std::endl;
+            for (unsigned comp_idx = 0; comp_idx < numComponents; ++comp_idx) {
+                std::cout << " x for component " << comp_idx << " :" << std::endl;
+                std::cout << " \tvalue = " << x[comp_idx].value() << std::endl;
+                for (int i = 0; i < 3; ++i) {
+                    std::cout << " \tderiv " << i << " = " << x[comp_idx].derivative(i) << std::endl;
+                }
+
+                std::cout << " y for component " << comp_idx << ":" << std::endl;
+                std::cout << " \tvalue = " << y[comp_idx].value() << std::endl;
+                for (int i = 0; i < 3; ++i) {
+                    std::cout << " \tderiv " << i << " = " << y[comp_idx].derivative(i) << std::endl;
+                }
+            }
+            std::cout << " L: " << std::endl; 
+            std::cout << " \tvalue = " << L.value() << std::endl;
+            for (int i = 0; i < L.size(); ++i) {
+                    std::cout << " \tderiv " << i << " = " << L.derivative(i) << std::endl;
+            }
+        }
+
+        // Reference values from JSON file
+        const auto eos_string = Opm::CompositionalConfig::eosTypeToString(eos_type);
+        Json::JsonObject eos_ref = parser.get_item(eos_string);
+
+        Json::JsonObject L_ref_array = eos_ref.get_item("L");
+        Evaluation ref_L = L_ref_array.get_array_item(0).as_double();
+        for (unsigned i = 0; i < 3; ++i) {
+            ref_L.setDerivative(i, L_ref_array.get_array_item(i + 1).as_double());
+        }
+
+        Json::JsonObject x_ref_array = eos_ref.get_item("x");
+        ComponentVector ref_x;
+        for (unsigned comp_idx = 0; comp_idx < numComponents; ++comp_idx) {
+            Json::JsonObject x_ref_row = x_ref_array.get_array_item(comp_idx);
+            ref_x[comp_idx].setValue(x_ref_row.get_array_item(0).as_double());
+            for (unsigned i = 0; i < 3; ++i) {
+                ref_x[comp_idx].setDerivative(i, x_ref_row.get_array_item(i + 1).as_double());
+            }
+        }
+
+        Json::JsonObject y_ref_array = eos_ref.get_item("y");
+        ComponentVector ref_y;
+        for (unsigned comp_idx = 0; comp_idx < numComponents; ++comp_idx) {
+            Json::JsonObject y_ref_row = y_ref_array.get_array_item(comp_idx);
+            ref_y[comp_idx].setValue(y_ref_row.get_array_item(0).as_double());
+            for (unsigned i = 0; i < 3; ++i) {
+                ref_y[comp_idx].setDerivative(i, y_ref_row.get_array_item(i + 1).as_double());
+            }
+        }
+
+        for (unsigned comp_idx = 0; comp_idx < numComponents; ++comp_idx) {
+            BOOST_CHECK_MESSAGE(Opm::MathToolbox<Evaluation>::isSame(x[comp_idx],
+                                                                     ref_x[comp_idx], 2e-3),
+                                "EOS type " << eos_string << ": component " << comp_idx << " of x does not match");
+            BOOST_CHECK_MESSAGE(Opm::MathToolbox<Evaluation>::isSame(y[comp_idx],
+                                                                     ref_y[comp_idx], 2e-3),
+                                "EOS type " << eos_string << ": component " << comp_idx << " of y does not match");
+        }
+
+        BOOST_CHECK_MESSAGE(Opm::MathToolbox<Evaluation>::isSame(L, ref_L, 2e-3),
+                            "EOS type " << eos_string << ": L does not match");
     }
-
-
-    Evaluation ref_L = 1 - 0.763309246;
-    ref_L.setDerivative(0, 4.072857907696467e-8);
-    ref_L.setDerivative(1, -1.1606117844565438);
-    ref_L.setDerivative(2, -1.2182584016253868);
-
-    ComponentVector ref_x;
-    ref_x[0].setValue(0.134348016);
-    ref_x[0].setDerivative(0, 1.225204984e-7);
-    ref_x[0].setDerivative(1, 0.1193427625186);
-    ref_x[0].setDerivative(2, -0.15685356397);
-
-    ref_x[1].setValue(0.021791990);
-    ref_x[1].setDerivative(0, 2.1923329015033e-8);
-    ref_x[1].setDerivative(1, -0.030587169734517);
-    ref_x[1].setDerivative(2, 0.0402010686143);
-
-    ref_x[2].setValue(0.84385999349);
-    ref_x[2].setDerivative(0, -1.44443827440285e-7);
-    ref_x[2].setDerivative(1, -0.088755592784150);
-    ref_x[2].setDerivative(2, 0.11665249535641);
-
-    ComponentVector ref_y;
-    ref_y[0].setValue(0.61338319);
-    ref_y[0].setDerivative(0, -1.2431457946797125e-8);
-    ref_y[0].setDerivative(1, 0.5447055650444589);
-    ref_y[0].setDerivative(2, -0.7159127825498286);
-
-    ref_y[1].setValue(0.38626813278337335);
-    ref_y[1].setDerivative(0, 1.2649586224979342e-8);
-    ref_y[1].setDerivative(1, -0.5447013877995585);
-    ref_y[1].setDerivative(2, 0.7159072923488614);
-
-    ref_y[2].setValue(0.00034866911404565206);
-    ref_y[2].setDerivative(0,-2.1812827818225162e-10);
-    ref_y[2].setDerivative(1, -4.177244900520176e-6);
-    ref_y[2].setDerivative(2, 5.490200967341757e-6);
-
-    for (unsigned comp_idx = 0; comp_idx < numComponents; ++comp_idx) {
-        BOOST_CHECK_MESSAGE(Opm::MathToolbox<Evaluation>::isSame(x[comp_idx],
-                                                                 ref_x[comp_idx], 2e-3),
-                            "component " << comp_idx << " of x does not match");
-        BOOST_CHECK_MESSAGE(Opm::MathToolbox<Evaluation>::isSame(y[comp_idx],
-                                                                 ref_y[comp_idx], 2e-3),
-                            "component " << comp_idx << " of y does not match");
-    }
-
-    BOOST_CHECK_MESSAGE(Opm::MathToolbox<Evaluation>::isSame(L, ref_L, 2e-3),
-                        "L does not match");
-
 #if BOOST_VERSION / 100000 == 1 && BOOST_VERSION / 100 % 1000 < 67
 }
 #endif
