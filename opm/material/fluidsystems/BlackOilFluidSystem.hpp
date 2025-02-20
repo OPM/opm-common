3fc8a9b9ad4169adf3c2f49659043541562192ca
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2025-02-20 13:04:28 +0100

    Added documentation

--- opm/material/fluidsystems/BlackOilFluidSystem.hpp
+++ opm/material/fluidsystems/BlackOilFluidSystem.hpp
@@ -18,14 +18,47 @@
 #ifndef OPM_BLACK_OIL_FLUID_SYSTEM_HPP
 #define OPM_BLACK_OIL_FLUID_SYSTEM_HPP
 
+// Here we need to define certain macros before including the macrotemplate file.
+// 
+// The idea is in essence the following:
+//   1) In the macrotemplate file, we have method declarations of the form
+//      
+//        `STATIC_OR_DEVICE void foo();`
+//      
+//      and member variable declarations of the form
+//
+//        `STATIC_OR_NOTHING int bar;`
+//
+//   2) We want to be able to compile the same code for both the dynamic (non-static) and static
+//      versions of the fluid system. The dynamic version is used when the fluid system is accessed
+//      from the GPU, while the static version is used when the fluid system is accessed from the CPU.
+//   3) We want to be able to compile the same code for both the dynamic and static versions of the
+//      fluid system, but with different method and member variable declarations.
+//
+// Furthermore, we need to specify the class name of the fluid system, which is different for the
+// nonstatic and static versions of the fluid system. We also need to specify if we are compiling
+// the static version of the fluid system, since we will define certain constructors and singleton
+// functions only in the static or nonstatic case. 
+
+// Is defined for the static version of the fluid system.
 #define COMPILING_STATIC_FLUID_SYSTEM
+
+// The static version does not need any gpu decorators, simply static
 #define STATIC_OR_DEVICE static
+
+// Make sure member variables are declared as static
 #define STATIC_OR_NOTHING static
+
+// Define the class names for the static and nonstatic versions of the fluid system
 #define FLUIDSYSTEM_CLASSNAME_NONSTATIC BlackOilFluidSystemNonStatic
 #define FLUIDSYSTEM_CLASSNAME_STATIC BlackOilFluidSystem
+
+// Define the class name for the fluid system
 #define FLUIDSYSTEM_CLASSNAME BlackOilFluidSystem
 
 
+// We need to forward-declare the nonstatic version of the fluid system, since we will 
+// make the nonstatic version a friend of the static version being defined here.
 namespace Opm
 {
 template <class Scalar,
@@ -35,8 +68,10 @@ template <class Scalar,
 class FLUIDSYSTEM_CLASSNAME_NONSTATIC;
 }
 
+// Include the macrotemplate file
 #include <opm/material/fluidsystems/BlackOilFluidSystem_macrotemplate.hpp>
 
+// Undefine the macros we defined above
 #undef STATIC_OR_DEVICE
 #undef COMPILING_STATIC_FLUID_SYSTEM
 #undef STATIC_OR_NOTHING
--- opm/material/fluidsystems/BlackOilFluidSystemNonStatic.hpp
+++ opm/material/fluidsystems/BlackOilFluidSystemNonStatic.hpp
@@ -21,12 +21,46 @@
 #include <opm/common/utility/gpuDecorators.hpp>
 
 
+// Here we need to define certain macros before including the macrotemplate file.
+// 
+// The idea is in essence the following:
+//   1) In the macrotemplate file, we have method declarations of the form
+//      
+//        `STATIC_OR_DEVICE void foo();`
+//      
+//      and member variable declarations of the form
+//
+//        `STATIC_OR_NOTHING int bar;`
+//
+//   2) We want to be able to compile the same code for both the dynamic (non-static) and static
+//      versions of the fluid system. The dynamic version is used when the fluid system is accessed
+//      from the GPU, while the static version is used when the fluid system is accessed from the CPU.
+//   3) We want to be able to compile the same code for both the dynamic and static versions of the
+//      fluid system, but with different method and member variable declarations.
+//
+// Furthermore, we need to specify the class name of the fluid system, which is different for the
+// nonstatic and static versions of the fluid system. We also need to specify if we are compiling
+// the static version of the fluid system, since we will define certain constructors and singleton
+// functions only in the static or nonstatic case. 
+
+// Nonstatic class name
 #define FLUIDSYSTEM_CLASSNAME_NONSTATIC BlackOilFluidSystemNonStatic
+
+// Static class name
 #define FLUIDSYSTEM_CLASSNAME_STATIC BlackOilFluidSystem
+
+// Fluid system class name for the fluid system we are defining now, ie the nonstatic version
 #define FLUIDSYSTEM_CLASSNAME BlackOilFluidSystemNonStatic
+
+// We need to decorate the nonstatic member functions with the gpu decorators
 #define STATIC_OR_DEVICE OPM_HOST_DEVICE
+
+// Member variables need no decorators for the nonstatic version
 #define STATIC_OR_NOTHING
 
+
+// We need to forward-declare the static version of the fluid system, since we will
+// add it as a friend to the nonstatic version.
 namespace Opm
 {
 template <class Scalar,
@@ -36,8 +70,10 @@ template <class Scalar,
 class FLUIDSYSTEM_CLASSNAME_STATIC;
 }
 
+// Include the macrotemplate file
 #include <opm/material/fluidsystems/BlackOilFluidSystem_macrotemplate.hpp>
 
+// Undefine the macros we defined above
 #undef STATIC_OR_DEVICE
 #undef STATIC_OR_NOTHING
 #undef FLUIDSYSTEM_CLASSNAME_NONSTATIC
