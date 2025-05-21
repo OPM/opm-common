cf31bd5419ac5d75b5d69cd95ca08fecd2465553
Lisa Julia Nebel <lisa.nebel@dr-blatt.de>
2025-05-21 15:31:41 +0200

    Add documentation for the ParseContext class

--- python/cxx/parsecontext.cpp
+++ python/cxx/parsecontext.cpp
@@ -5,6 +5,7 @@
 
 #include "export.hpp"
 
+#include <python/cxx/OpmCommonPythonDoc.hpp>
 
 namespace {
 
@@ -14,15 +15,17 @@ namespace {
 
 void python::common::export_ParseContext(py::module& module) {
 
-    py::class_< ParseContext >(module, "ParseContext" )
-        .def(py::init<>())
-        .def(py::init<const std::vector<std::pair<std::string, InputErrorAction>>>())
-        .def( "ignore_keyword", &ParseContext::ignoreKeyword )
-        .def( "update", ctx_update );
+    using namespace Opm::Common::DocStrings;
 
-    py::enum_< InputErrorAction >( module, "action" )
-      .value( "throw",  InputErrorAction::THROW_EXCEPTION )
-      .value( "warn",   InputErrorAction::WARN )
-      .value( "ignore", InputErrorAction::IGNORE );
+    py::class_< ParseContext >(module, "ParseContext", ParseContext_docstring)
+        .def(py::init<>(), ParseContext_init_docstring)
+        .def(py::init<const std::vector<std::pair<std::string, InputErrorAction>>>(), py::arg("action_list"), ParseContext_init_with_actions_docstring)
+        .def( "ignore_keyword", &ParseContext::ignoreKeyword, py::arg("keyword"), ParseContext_ignore_keyword_docstring)
+        .def( "update", ctx_update, py::arg("keyword"), py::arg("action"), ParseContext_update_docstring);
+
+    py::enum_< InputErrorAction >( module, "action", action_docstring)
+      .value( "throw",  InputErrorAction::THROW_EXCEPTION, action_throw_docstring)
+      .value( "warn",   InputErrorAction::WARN, action_warn_docstring)
+      .value( "ignore", InputErrorAction::IGNORE, action_ignore_docstring);
 
 }
