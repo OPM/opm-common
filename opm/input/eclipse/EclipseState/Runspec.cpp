c89fd868e65c94b62ebf07d912f65b2fd4f2286e
Halvor M Nilsen <Halvor.M.Nilsen@sintef.no>
2025-06-03 13:32:16 +0200

    Added FRAC keyword

--- opm/input/eclipse/EclipseState/Runspec.cpp
+++ opm/input/eclipse/EclipseState/Runspec.cpp
@@ -768,6 +768,14 @@ Runspec::Runspec(const Deck& deck)
             OpmLog::note(msg);
         }
 
+        if (runspecSection.hasKeyword<ParserKeywords::FRAC>()) {
+            m_frac = true;
+
+            const std::string msg = "\nSimulation will solve with fractures in wells";
+
+            OpmLog::note(msg);
+        }
+
         if (runspecSection.hasKeyword<ParserKeywords::TEMP>()) {
             m_temp = true;
 
@@ -906,6 +914,11 @@ bool Runspec::mech() const noexcept
     return this->m_mech;
 }
 
+bool Runspec::frac() const noexcept
+{
+    return this->m_frac;
+}
+
 bool Runspec::temp() const noexcept
 {
     return this->m_temp;
@@ -964,6 +977,7 @@ bool Runspec::rst_cmp(const Runspec& full_spec, const Runspec& rst_spec)
         full_spec.m_h2storage == rst_spec.m_h2storage &&
         full_spec.m_micp == rst_spec.m_micp &&
         full_spec.m_mech == rst_spec.m_mech &&
+        full_spec.m_frac == rst_spec.m_frac &&
         full_spec.m_temp == rst_spec.m_temp &&
         Welldims::rst_cmp(full_spec.wellDimensions(), rst_spec.wellDimensions());
 }
@@ -992,6 +1006,7 @@ bool Runspec::operator==(const Runspec& data) const
         && (this->m_h2storage == data.m_h2storage)
         && (this->m_micp == data.m_micp)
         && (this->m_mech == data.m_mech)
+        && (this->m_frac == data.m_frac)
         && (this->m_temp == data.m_temp)
         ;
 }
