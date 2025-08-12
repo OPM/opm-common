64d3ab67bf171ab348b264ca9950e2e1500f46e1
Atgeirr Flø Rasmussen <atgeirr.rasmussen@sintef.no>
2025-08-12 14:31:48 +0200

    Implemented inverseFormationVolumeFactorAndViscosity() for dry gas and dead oil.

--- opm/material/fluidsystems/blackoilpvt/DryGasPvt.hpp
+++ opm/material/fluidsystems/blackoilpvt/DryGasPvt.hpp
@@ -175,11 +175,14 @@ public:
      */
     template <class FluidState, class LhsEval = typename FluidState::Scalar>
     std::pair<LhsEval, LhsEval>
-    inverseFormationVolumeFactorAndViscosity(const FluidState& /*fluidState*/, unsigned /*regionIdx*/)
+    inverseFormationVolumeFactorAndViscosity(const FluidState& fluidState, unsigned regionIdx)
     {
-        throw std::logic_error("Needs fixing before merging!");
-        return {};
-    }
+        const LhsEval& p = decay<LhsEval>(fluidState.pressure(FluidState::gasPhaseIdx));
+        const auto segIdx = this->inverseGasB_[regionIdx].findSegmentIndex(p, /*extrapolate=*/ true);
+        const auto& invBg = this->inverseGasB_[regionIdx].eval(p, SegmentIndex{segIdx});
+        const auto& invMugBg = this->inverseGasBMu_[regionIdx].eval(p, SegmentIndex{segIdx});
+        return { invBg, invBg / invMugBg };
+   }
 
     /*!
      * \brief Returns the formation volume factor [-] of oil saturated gas at given pressure.
