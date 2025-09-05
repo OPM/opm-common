d33cdb2f44664aaa320ae6be801ac2989d577bbd
arturcastiel <arturcastiel@gmail.com>
2025-09-05 12:39:55 +0000

    add an overload for RestartValue store LGR specific values - added filtering of data::Wells based on lgr_grid

--- opm/output/eclipse/RestartValue.hpp
+++ opm/output/eclipse/RestartValue.hpp
@@ -84,6 +84,13 @@ namespace Opm {
                      data::GroupAndNetworkValues grpn_nwrk_arg,
                      data::Aquifers aquifer_arg);
 
+        // Overloaded constructor to handle grid containing LGR
+        RestartValue(data::Solution sol,
+                     data::Wells wells_arg,
+                     data::GroupAndNetworkValues grpn_nwrk_arg,
+                     data::Aquifers aquifer_arg,
+                     int lgr_grid);
+
         RestartValue() = default;
 
         bool hasExtra(const std::string& key) const;
@@ -92,7 +99,7 @@ namespace Opm {
         void addExtra(const std::string& key, std::vector<double> data);
         void addExtra(const std::string& key, std::vector<float> data);
         const std::vector<double>& getExtra(const std::string& key) const;
-
+        void filter_wells_for_lgr(data::Wells& allwells, int lgr_grid);
         void convertFromSI(const UnitSystem& units);
         void convertToSI(const UnitSystem& units);
 
