b49f0e34f2575c66296c9a7bc83fc6b3e7d33ad2
Markus Blatt <markus@dr-blatt.de>
2025-12-05 08:39:43 +0100

    Only check number in well completion quantities.

--- opm/input/eclipse/EclipseState/SummaryConfig/SummaryConfig.cpp
+++ opm/input/eclipse/EclipseState/SummaryConfig/SummaryConfig.cpp
@@ -517,21 +517,23 @@ namespace {
 
         bool ret = std::regex_match(keyword, conn_compl_kw);
 
-        auto numstart  = keyword.find_first_of({"_1234567889"});
-        if (numstart == std::string::npos) {
-            return ret;
-        }
-        auto numend = keyword.find(':');
-        if ( numend == std::string::npos || numend < numstart ) {
-            numend = keyword.size();
-        }
-        if ((numend - numstart) != 3) {
-            throw std::invalid_argument {
+        if (ret) {
+            auto numstart  = keyword.find_first_of({"_1234567889"});
+            if (numstart == std::string::npos) {
+                return ret;
+            }
+            auto numend = keyword.find(':');
+            if ( numend == std::string::npos || numend < numstart ) {
+                numend = keyword.size();
+            }
+            if ((numend - numstart) != 3) {
+                throw std::invalid_argument {
                     fmt::format("Well completion quantity {} needs to have "
                                 "three consecutive characters that are "
                                 " a number or an underline character "
                                 " (e.g. WWCTL123:W1, WWCTL_23:W or WWCTL__3:W1)",
                                 keyword)};
+            }
         }
         return ret;
     }
