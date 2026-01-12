ceb1a0071239ffa4cedf4047f1b6ed17cf748798
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2026-01-12 12:59:53 +0100

    added: ignore list for cppcheck in static analysis
    avoid hardcoding

--- jenkins/static_analysis.sh
+++ jenkins/static_analysis.sh
@@ -3,6 +3,8 @@
 source ${TOOLCHAIN_DIR}/build-configurations-sca.sh
 source `dirname $0`/build-opm-module.sh
 
+CPPCHECK_IGNORE_LIST="-i$WORKSPACE/tests/material/test_fluidmatrixinteractions.cpp"
+
 $WORKSPACE/jenkins/build.sh
 
 run_static_analysis opm-common
