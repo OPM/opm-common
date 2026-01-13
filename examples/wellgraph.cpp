927f803453e5a7c94414d25983aa869e92c2a3f2
Kai Bao <kai.bao@sintef.no>
2026-01-13 14:48:21 +0100

    moving the group structure visulization to GroupStructureViz
    so we can use the function in the code.

--- examples/wellgraph.cpp
+++ examples/wellgraph.cpp
@@ -42,56 +42,12 @@
 #include <opm/common/OpmLog/StreamLog.hpp>
 #include <opm/common/OpmLog/LogUtil.hpp>
 
+#include <opm/utility/GroupStructureViz.hpp>
+
 
 inline void createDot(const Opm::Schedule& schedule, const std::string& casename)
 {
-    std::cout << "Writing " << casename << ".gv .... ";  std::cout.flush();
-    std::ofstream os(casename + ".gv");
-    os << "// This file was written by the 'wellgraph' utility from OPM.\n";
-    os << "// Find the source code at github.com/OPM.\n";
-    os << "// Convert output to PDF with 'dot -Tpdf " << casename << ".gv > " << casename << ".pdf'\n";
-    os << "strict digraph \"" << casename << "\"\n{\n";
-    const auto groupnames = schedule.groupNames();
-    const std::size_t last = schedule.size() - 1;
-    // Group -> Group relations.
-    for (const auto& gn : groupnames) {
-        const auto& g = schedule.getGroup(gn, last);
-        const auto& children = g.groups();
-        if (!children.empty()) {
-            os << "    \"" << gn << "\" -> {";
-            for (const auto& child : children) {
-                os << " \"" << child << '"';
-            }
-            os << " }\n";
-        }
-    }
-    // Group -> Well relations.
-    os << "    node [shape=box]\n";
-    for (const auto& gn : groupnames) {
-        const auto& g = schedule.getGroup(gn, last);
-        const auto& children = g.wells();
-        if (!children.empty()) {
-            os << "    \"" << gn << "\" -> {";
-            for (const auto& child : children) {
-                os << " \"" << child << '"';
-            }
-            os << " }\n";
-        }
-    }
-    // Color wells by injector or producer.
-    for (const auto& w : schedule.getWellsatEnd()) {
-        os << "    \"" << w.name() << '"';
-        if (w.isProducer() && w.isInjector()) {
-            os << " [color=purple]\n";
-        } else if (w.isProducer()) {
-            os << " [color=red]\n";
-        } else {
-            os << " [color=blue]\n";
-        }
-    }
-    os << "}\n";
-    std::cout << "complete." << std::endl;
-    std::cout << "Convert output to PDF with 'dot -Tpdf " << casename << ".gv > " << casename << ".pdf'\n" << std::endl;
+    Opm::writeGroupStructure(schedule, casename);
 }
 
 
