cf5e458aa83bb5d12a1af50bec850358de796af3
Kai Bao <kai.bao@sintef.no>
2026-01-19 10:53:16 +0100

    moving the newtork visuzation to separate function
    so that the simulator can call it.

--- examples/networkgraph2.cpp
+++ examples/networkgraph2.cpp
@@ -44,75 +44,12 @@
 #include <opm/common/OpmLog/StreamLog.hpp>
 #include <opm/common/OpmLog/LogUtil.hpp>
 
+#include <opm/utility/NetworkViz.hpp>
+
 
 inline void createDot(const Opm::Schedule& schedule, const std::string& casename)
 {
-    std::cout << "Writing " << casename << ".gv .... ";  std::cout.flush();
-    std::ofstream os(casename + ".gv");
-    os << "// This file was written by the 'networkgraph2' utility from OPM.\n";
-    os << "// Find the source code at github.com/OPM.\n";
-    os << "// Convert output to PDF with 'dot -Tpdf " << casename << ".gv > " << casename << ".pdf'\n";
-    os << "strict digraph \"" << casename << "\"\n{\n";
-    const std::size_t last = schedule.size() - 1;
-    // by default, it is the last report step, while it can be any report step
-    const auto& network = schedule[last].network();
-
-    for (const auto& branch : network.branches()) {
-        os << "    \"" << branch->uptree_node() << "\" -> \"" << branch->downtree_node() << "\"";
-        if (branch->vfp_table().has_value()) {
-            os << " [label=\"" << branch->vfp_table().value() << "\"]";
-        }
-        os << ";\n";
-    }
-
-    // Highlight root nodes
-    for (const auto& root : network.roots()) {
-        const auto& root_node = root.get();
-        os << "    \"" << root_node.name() << "\" [shape=doubleoctagon";
-        if (root_node.terminal_pressure().has_value()) {
-            if (root_node.terminal_pressure().has_value()) {
-                // TODO: we should be able to get the Unit conversion here
-                os << ", label=\"" << root_node.name() << " : " << root_node.terminal_pressure().value()/1.e5 << "bars\"";
-            }
-        }
-        os << "];\n";
-    }
-
-    // Highlight leaf nodes
-    for (const auto& leaf : network.leaf_nodes()) {
-        os << "    \"" << leaf << "\" [shape=oval];\n";
-    }
-
-    // Group -> Well relations.
-    os << "    node [shape=box]\n";
-    for (const auto& leaf : network.leaf_nodes()) {
-        const auto& leaf_group = schedule.getGroup(leaf, last);
-        const auto& wells = leaf_group.wells();
-        if (!wells.empty()) {
-            os << "    \"" << leaf << "\" -> {";
-            for (const auto& child : wells) {
-                os << " \"" << child << '"';
-            }
-            os << " }\n";
-        }
-        if (!wells.empty()) {
-            for (const auto& child : wells) {
-                const auto& w = schedule.getWell(child, last);
-                os << "    \"" << w.name() << '"';
-                if (w.isProducer() && w.isInjector()) {
-                    os << " [color=purple]\n";
-                } else if (w.isProducer()) {
-                    os << " [color=red]\n";
-                } else {
-                    os << " [color=blue]\n";
-                }
-            }
-        }
-    }
-    os << "}\n";
-
-    std::cout << "complete." << std::endl;
-    std::cout << "Convert output to PDF with 'dot -Tpdf " << casename << ".gv > " << casename << ".pdf'\n" << std::endl;
+    Opm::writeNetworkStructure(schedule, casename);
 }
 
 
@@ -161,4 +98,3 @@ int main(int argc, char** argv)
         std::cout << "\n\n***** Exiting due to errors." << std::endl;
     }
 }
-
