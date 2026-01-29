5ac245b94dac076f49cc72cb134943c7d688c8b7
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2026-01-29 10:41:37 +0100

    UseFastBuilds: convert to function

--- cmake/Modules/UseFastBuilds.cmake
+++ cmake/Modules/UseFastBuilds.cmake
@@ -1,10 +1,17 @@
 # - Try to build faster depending on the compiler
 
 # faster builds by using a pipe instead of temp files
-include (AddOptions)
 include (UseCompVer)
 is_compiler_gcc_compatible ()
 
-if (CXX_COMPAT_GCC)
-	add_options (ALL_LANGUAGES ALL_BUILDS "-pipe")
-endif ()
+option(USE_FAST_BUILD "Enable flags for faster builds?" ON)
+
+function(use_fast_build)
+  cmake_parse_arguments(PARAM "" "TARGET" "" ${ARGN})
+  if(NOT PARAM_TARGET)
+    message(FATAL_ERROR "Function needs a TARGET parameter")
+  endif()
+  if (CXX_COMPAT_GCC AND USE_FAST_BUILD)
+    target_compile_options(${PARAM_TARGET} PRIVATE -pipe)
+  endif ()
+endfunction()
