3da36b456cc25a72f75940771499f11d9ad9caf7
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2026-03-02 14:01:43 +0100

    tab scrub

--- opm-common-prereqs.cmake
+++ opm-common-prereqs.cmake
@@ -1,27 +1,27 @@
-# -*- mode: cmake; tab-width: 2; indent-tabs-mode: t; truncate-lines: t; compile-command: "cmake -Wdev" -*-
-# vim: set filetype=cmake autoindent tabstop=2 shiftwidth=2 noexpandtab softtabstop=2 nowrap:
+# -*- mode: cmake; tab-width: 2; indent-tabs-mode: nil; truncate-lines: t; compile-command: "cmake -Wdev" -*-
+# vim: set filetype=cmake autoindent tabstop=2 shiftwidth=2 expandtab softtabstop=2 nowrap:
 
 # defines that must be present in config.h for our headers
 set (opm-common_CONFIG_VAR
-	HAVE_OPENMP
-	HAVE_TYPE_TRAITS
-	HAVE_VALGRIND
-	HAVE_FINAL
-	HAVE_ECL_INPUT
-	HAVE_CXA_DEMANGLE
-	HAVE_FNMATCH_H
-	HAVE_DUNE_COMMON
-	)
+  HAVE_OPENMP
+  HAVE_TYPE_TRAITS
+  HAVE_VALGRIND
+  HAVE_FINAL
+  HAVE_ECL_INPUT
+  HAVE_CXA_DEMANGLE
+  HAVE_FNMATCH_H
+  HAVE_DUNE_COMMON
+  )
 
 # dependencies
 set (opm-common_DEPS
-	# valgrind client requests
-	"Valgrind"
+  # valgrind client requests
+  "Valgrind"
 )
 
 # CMake 3.30.0 requires to find Boost in CONFIG mode
 if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.30.0)
-	set(_Boost_CONFIG_MODE CONFIG)
+  set(_Boost_CONFIG_MODE CONFIG)
 endif()
 
 list(APPEND opm-common_DEPS
