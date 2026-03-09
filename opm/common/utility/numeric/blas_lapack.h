91435c47b24a8ff706d00c23181e8a7eedc90f9f
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2026-03-09 13:59:10 +0100

    UseFortranWrapper: remove
    the modules using fortran functions (e.g. blas) should
    use the cmake native FortranCInterface directly

--- opm/common/utility/numeric/blas_lapack.h
+++ opm/common/utility/numeric/blas_lapack.h
@@ -20,9 +20,15 @@
 #ifndef OPM_BLAS_LAPACK_HEADER_INCLUDED
 #define OPM_BLAS_LAPACK_HEADER_INCLUDED
 
+#if ! __has_include(<FCMacros.h>)
+#error "Need FCMacros.h to be generated through FortranCInterface"
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#include <FCMacros.h>
 
 #if defined(MATLAB_MEX_FILE) && MATLAB_MEX_FILE
 #include <mex.h>
@@ -36,109 +42,108 @@ extern "C" {
 
 
 /* C <- a1*op(A)*op(B) + a2*C  where  op(X) in {X, X.'} */
-void dgemm_(const char *transA  , const char *transB   ,
-            const MAT_SIZE_T*  m, const MAT_SIZE_T* n  , const MAT_SIZE_T* k  ,
-            const double*     a1, const double*     A  , const MAT_SIZE_T* ldA,
-            const double*      B, const MAT_SIZE_T* ldB,
-            const double*     a2,       double*     C  , const MAT_SIZE_T* ldC);
+void FC_GLOBAL(dgemm,DGEMM)(const char *transA  , const char *transB   ,
+                            const MAT_SIZE_T*  m, const MAT_SIZE_T* n  , const MAT_SIZE_T* k  ,
+                            const double*     a1, const double*     A  , const MAT_SIZE_T* ldA,
+                            const double*      B, const MAT_SIZE_T* ldB,
+                            const double*     a2,       double*     C  , const MAT_SIZE_T* ldC);
 
 
 /* C <- a1*A*A' + a2*C   *or*   C <- a1*A'*A + a2*C */
-void dsyrk_(const char       *uplo, const char       *trans,
-            const MAT_SIZE_T *n   , const MAT_SIZE_T *k    ,
-            const double     *a1  , const double     *A    , const MAT_SIZE_T *ldA,
-            const double     *a2  ,       double     *C    , const MAT_SIZE_T *ldC);
+void FC_GLOBAL(dsyrk,DSYRK)(const char       *uplo, const char       *trans,
+                            const MAT_SIZE_T *n   , const MAT_SIZE_T *k    ,
+                            const double     *a1  , const double     *A    , const MAT_SIZE_T *ldA,
+                            const double     *a2  ,       double     *C    , const MAT_SIZE_T *ldC);
 
 
-void dgeqrf_(const MAT_SIZE_T *m    , const MAT_SIZE_T *n   ,
-                   double     *A    , const MAT_SIZE_T *ld  ,
-                   double     *tau  ,       double     *work,
-             const MAT_SIZE_T *lwork,       MAT_SIZE_T *info);
+void FC_GLOBAL(dgeqrf,DGERF)(const MAT_SIZE_T *m    , const MAT_SIZE_T *n   ,
+                             double     *A    , const MAT_SIZE_T *ld  ,
+                             double     *tau  ,       double     *work,
+                             const MAT_SIZE_T *lwork,       MAT_SIZE_T *info);
 
 
-void dorgqr_(const MAT_SIZE_T *m   , const MAT_SIZE_T *n    , const MAT_SIZE_T *k  ,
-                   double     *A   , const MAT_SIZE_T *ld   , const double     *tau,
-                   double     *work, const MAT_SIZE_T *lwork,       MAT_SIZE_T *info);
+void FC_GLOBAL(dorgqr,DORGQR)(const MAT_SIZE_T *m   , const MAT_SIZE_T *n    , const MAT_SIZE_T *k  ,
+                              double     *A   , const MAT_SIZE_T *ld   , const double     *tau,
+                              double     *work, const MAT_SIZE_T *lwork,       MAT_SIZE_T *info);
 
 /* A <- LU(A) */
-void dgetrf_(const MAT_SIZE_T *m   , const MAT_SIZE_T *n ,
-             double           *A   , const MAT_SIZE_T *ld,
-             MAT_SIZE_T       *ipiv, MAT_SIZE_T       *info);
+void FC_GLOBAL(dgetrf,DGETRF)(const MAT_SIZE_T *m   , const MAT_SIZE_T *n ,
+                              double           *A   , const MAT_SIZE_T *ld,
+                              MAT_SIZE_T       *ipiv, MAT_SIZE_T       *info);
 
 /* B <- A \ B, when A is LU(A) from dgetrf() */
-void dgetrs_(const char       *trans, const MAT_SIZE_T *n,
-             const MAT_SIZE_T *nrhs ,
-             const double     *A    , const MAT_SIZE_T *lda,
-             const MAT_SIZE_T *ipiv , double           *B,
-             const MAT_SIZE_T *ldb  , MAT_SIZE_T       *info);
+void FC_GLOBAL(dgetrs,DGETRS)(const char       *trans, const MAT_SIZE_T *n,
+                              const MAT_SIZE_T *nrhs ,
+                              const double     *A    , const MAT_SIZE_T *lda,
+                              const MAT_SIZE_T *ipiv , double           *B,
+                              const MAT_SIZE_T *ldb  , MAT_SIZE_T       *info);
 
 /* B <- A \ B, tridiagonal A with bands DL, D, DU */
-void dgtsv_(const MAT_SIZE_T *n    ,
-            const MAT_SIZE_T *nrhs ,
-                  double     *DL   ,
-                  double     *D    ,
-                  double     *DU   ,
-                  double     *B    ,
-            const MAT_SIZE_T *ldb  ,
-            MAT_SIZE_T       *info);
+void FC_GLOBAL(dgtsv,DGTSV)(const MAT_SIZE_T *n    ,
+                            const MAT_SIZE_T *nrhs ,
+                            double     *DL   ,
+                            double     *D    ,
+                            double     *DU   ,
+                            double     *B    ,
+                            const MAT_SIZE_T *ldb  ,
+                            MAT_SIZE_T       *info);
 
 /* B <- A \ B, band matrix A stored in AB with kl subdiagonals, ku superdiagonals */
-void dgbsv_(const MAT_SIZE_T *n    ,
-            const MAT_SIZE_T *kl   ,
-            const MAT_SIZE_T *ku   ,
-            const MAT_SIZE_T *nrhs ,
-            double     *AB   ,
-            const MAT_SIZE_T *ldab ,
-            MAT_SIZE_T *ipiv ,
-            double     *B    ,
-            const MAT_SIZE_T *ldb  ,
-            MAT_SIZE_T       *info);
+void FC_GLOBAL(dgbsv,DGBSV)(const MAT_SIZE_T *n    ,
+                            const MAT_SIZE_T *kl   ,
+                            const MAT_SIZE_T *ku   ,
+                            const MAT_SIZE_T *nrhs ,
+                            double     *AB   ,
+                            const MAT_SIZE_T *ldab ,
+                            MAT_SIZE_T *ipiv ,
+                            double     *B    ,
+                            const MAT_SIZE_T *ldb  ,
+                            MAT_SIZE_T       *info);
 
 /* B <- A \ B, general solver */
-void dgesv_(const MAT_SIZE_T *n,
-            const MAT_SIZE_T *nrhs ,
-            double     *A   ,
-            const MAT_SIZE_T *lda ,
-            MAT_SIZE_T *piv ,
-            double     *B    ,
-            const MAT_SIZE_T *ldb  ,
-            MAT_SIZE_T *info);
+void FC_GLOBAL(dgesv,DGESV)(const MAT_SIZE_T *n,
+                            const MAT_SIZE_T *nrhs ,
+                            double     *A   ,
+                            const MAT_SIZE_T *lda ,
+                            MAT_SIZE_T *piv ,
+                            double     *B    ,
+                            const MAT_SIZE_T *ldb  ,
+                            MAT_SIZE_T *info);
 
 /* A <- chol(A) */
-void dpotrf_(const char *uplo, const MAT_SIZE_T *n,
-             double     *A   , const MAT_SIZE_T *lda,
-             MAT_SIZE_T *info);
+void FC_GLOBAL(dpotrf,DPOTRF)(const char *uplo, const MAT_SIZE_T *n,
+                              double     *A   , const MAT_SIZE_T *lda,
+                              MAT_SIZE_T *info);
 
 /* B <- (A \ (A' \ B)), when A=DPOTRF(A_orig) */
-void dpotrs_(const char *uplo, const MAT_SIZE_T *n  , const MAT_SIZE_T *nrhs,
-             double     *A   , const MAT_SIZE_T *lda,
-             double     *B   , const MAT_SIZE_T *ldb,       MAT_SIZE_T *info);
+void FC_GLOBAL(dpotrs,DPOTRS)(const char *uplo, const MAT_SIZE_T *n  , const MAT_SIZE_T *nrhs,
+                              double     *A   , const MAT_SIZE_T *lda,
+                              double     *B   , const MAT_SIZE_T *ldb,       MAT_SIZE_T *info);
 
 /* A <- chol(A), packed format. */
-void dpptrf_(const char *uplo, const MAT_SIZE_T *n,
-             double     *Ap  ,       MAT_SIZE_T *info);
+void FC_GLOBAL(dpptrf,DPOTRF)(const char *uplo, const MAT_SIZE_T *n,
+                              double     *Ap  ,       MAT_SIZE_T *info);
 
 /* A <- (A \ (A' \ eye(n)) when A=DPPTRF(A_orig) (packed format). */
-void dpptri_(const char *uplo, const MAT_SIZE_T *n,
-             double     *Ap  ,       MAT_SIZE_T *info);
+void FC_GLOBAL(dpptri,DPPTRI)(const char *uplo, const MAT_SIZE_T *n,
+                              double     *Ap  ,       MAT_SIZE_T *info);
 
 /* y <- a1*op(A)*x + a2*y */
-void dgemv_(const char       *trans,
-            const MAT_SIZE_T *m    , const MAT_SIZE_T *n,
-            const double     *a1   , const double     *A, const MAT_SIZE_T *ldA ,
-                                     const double     *x, const MAT_SIZE_T *incX,
-            const double     *a2   ,       double     *y, const MAT_SIZE_T *incY);
+void FC_GLOBAL(dgemv,DGEMV)(const char       *trans,
+                            const MAT_SIZE_T *m    , const MAT_SIZE_T *n,
+                            const double     *a1   , const double     *A, const MAT_SIZE_T *ldA ,
+                            const double     *x, const MAT_SIZE_T *incX,
+                            const double     *a2   ,       double     *y, const MAT_SIZE_T *incY);
 
 
 /* y <- a*x + y */
-void daxpy_(const MAT_SIZE_T *n, const double *a,
-            const double *x, const MAT_SIZE_T *incx,
-                  double *y, const MAT_SIZE_T *incy);
+void FC_GLOBAL(daxpy,DAXPY)(const MAT_SIZE_T *n, const double *a,
+                            const double *x, const MAT_SIZE_T *incx,
+                            double *y, const MAT_SIZE_T *incy);
 
 /* s <- x' * y */
-double ddot_(const MAT_SIZE_T *n, const double *x, const MAT_SIZE_T *incx,
-                                  const double *y, const MAT_SIZE_T *incy);
-
+double FC_GLOBAL(ddot,DDOT)(const MAT_SIZE_T *n, const double *x, const MAT_SIZE_T *incx,
+                            const double *y, const MAT_SIZE_T *incy);
 
 
 #ifdef __cplusplus
