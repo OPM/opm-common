02af07edcdd0f1c74fd1fcd2d1019443f82fb7b5
Svenn Tveit <svtv@norceresearch.no>
2026-05-18 10:57:07 +0200

    Container for multicomponent salt

---
+++ opm/material/components/CaIon.hpp
@@ -0,0 +1,67 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*
+  Copyright 2026 NORCE.
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+
+  Consult the COPYING file in the top-level source directory of this
+  module for the precise wording of the license and the list of
+  copyright holders.
+*/
+/*!
+ * \file
+ * \copydoc Opm::CaIon
+ */
+#ifndef OPM_CAION_HPP
+#define OPM_CAION_HPP
+
+#include <opm/material/components/Component.hpp>
+
+namespace Opm
+{
+
+/*!
+ * \ingroup Components
+ *
+ * \brief Properties of Ca2+ (calcium) ion
+ *
+ * \tparam Scalar The type used for scalar values
+ */
+template <class Scalar>
+class CaIon : public Component<Scalar, CaIon<Scalar> >
+{
+public:
+    /*!
+     * \copydoc Component::name
+     */
+    static std::string_view name()
+    {
+        return "Ca2+";
+    }
+
+    /*!
+     * \copydoc Component::molarMass
+     */
+    static Scalar molarMass()
+    {
+        return 40.078e-3;
+    }
+};
+
+}
+
+#endif //OPM_CAION_HPP
