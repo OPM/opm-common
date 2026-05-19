973c93fcaaefa2aa49e3c74d7d4ab9eaf73f41a9
Svenn Tveit <svtv@norceresearch.no>
2026-05-19 10:33:34 +0200

    CO2STORE PVT with multicomponent salt
    - Multicomponent salt models activated with SALTMC in RUNSPEC
    - Option in SALTMC to enable Laliberte-Cooper pure water density
    - Use H2O class pure water density as default
    - SaltArray used for all salt deck inputs
    - Models that require one salinity input will be provided with sum
    - Spycher&Pruess 2005 solubility now correctly use sum of ion molalities
    - OBS: Multicomp. salt (SALTMC) does not work with GPU
    - SALTMC does not combine with BRINE option

--- opm/material/components/NaIon.hpp
+++ opm/material/components/NaIon.hpp
@@ -25,6 +25,7 @@
 #ifndef OPM_NAION_HPP
 #define OPM_NAION_HPP
 
+#include <opm/common/utility/gpuDecorators.hpp>
 #include <opm/material/components/Component.hpp>
 
 namespace Opm
@@ -52,7 +53,7 @@ public:
     /*!
      * \copydoc Component::molarMass
      */
-    static Scalar molarMass()
+    OPM_HOST_DEVICE static Scalar molarMass()
     {
         return 22.9898e-3;
     }
